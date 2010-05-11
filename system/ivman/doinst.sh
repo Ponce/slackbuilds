cdbf629d170bcb7b0033e019655869332b151cf8
Guillermo Bonvehi <gbonvehi@gmail.com>
2010-05-11 19:46:30 +0200

    system/ivman: Updated for version 0.6.14

--- system/ivman/doinst.sh
+++ system/ivman/doinst.sh
@@ -2,19 +2,19 @@
 
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/ivman/IvmConfigActions.xml
-config etc/ivman/IvmConfigProperties.xml
-config etc/ivman/IvmConfigBase.xml
-config etc/ivman/IvmConfigConditions.xml
+config etc/ivman/IvmConfigBase.xml.new
+config etc/ivman/IvmConfigActions.xml.new
+config etc/ivman/IvmConfigProperties.xml.new
+config etc/ivman/IvmConfigConditions.xml.new
 
