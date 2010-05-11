f64823ff4bed529c42fa7bfa22149b25c224b125
Nishant Limbachia <nishant@mnspace.net>
2010-05-11 22:26:33 +0200

    system/clamsmtp: Updated for version 1.10

--- system/clamsmtp/doinst.sh
+++ system/clamsmtp/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -13,13 +11,12 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.clamsmtpd.new:
-if [ -e etc/rc.d/rc.clamsmtpd ]; then
-  cp -a etc/rc.d/rc.clamsmtpd etc/rc.d/rc.clamsmtpd.new.incoming
+# Keep same perms on rc.heimdal:
+if [ -e etc/rc.d/rc.clamsmtpd]; then
+  cp -a etc/rc.d/rc.clamsmtpdetc/rc.d/rc.clamsmtpd.new.incoming
   cat etc/rc.d/rc.clamsmtpd.new > etc/rc.d/rc.clamsmtpd.new.incoming
   mv etc/rc.d/rc.clamsmtpd.new.incoming etc/rc.d/rc.clamsmtpd.new
 fi
 
-config etc/rc.d/rc.clamsmtpd.new
+config etc/rc.d/rc.clamsmtpd.new 
 config etc/clamsmtpd.conf.new
-
