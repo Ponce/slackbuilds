d4a07bf33efbef9413ec8c63be4256fd294df1a9
Alan Hicks <alan@lizella.net>
2010-05-11 22:25:39 +0200

    network/postfix: Updated for version 2.5.5

--- network/postfix/doinst.sh
+++ network/postfix/doinst.sh
@@ -2,11 +2,11 @@
 
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
@@ -33,3 +33,6 @@ config etc/rc.d/rc.postfix.new
 
 # This will set the permissions on all postfix files correctly
 postfix set-permissions
+
+# Symlinks added by makepkg(8)
+
