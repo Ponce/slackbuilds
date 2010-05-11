898b3ce8c9b4dedc1830ab83cf139d619ab8fcc7
Nishant Limbachia <nishant@mnspace.net>
2010-05-11 20:01:56 +0200

    system/clamsmtp: Added to 12.0 repository

---
+++ system/clamsmtp/doinst.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.clamsmtpd.new:
+if [ -e etc/rc.d/rc.clamsmtpd ]; then
+  cp -a etc/rc.d/rc.clamsmtpd etc/rc.d/rc.clamsmtpd.new.incoming
+  cat etc/rc.d/rc.clamsmtpd.new > etc/rc.d/rc.clamsmtpd.new.incoming
+  mv etc/rc.d/rc.clamsmtpd.new.incoming etc/rc.d/rc.clamsmtpd.new
+fi
+
+config etc/rc.d/rc.clamsmtpd.new
+config etc/clamsmtpd.conf.new
+
