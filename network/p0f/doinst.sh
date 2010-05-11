a9c294f0eab8cacb557047512ab271e0b2375517
David Spencer <nobbutl@yahoo.co.uk>
2010-05-11 20:01:41 +0200

    network/p0f: Added to 12.0 repository

---
+++ network/p0f/doinst.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/p0f/p0f.fp.new
+config etc/p0f/p0fa.fp.new
+config etc/p0f/p0fo.fp.new
+config etc/p0f/p0fr.fp.new
+
