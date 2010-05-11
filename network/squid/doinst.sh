2bc0efd73ea73d28b0c180711de99249c8609878
David Somero <dsomero@hotmail.com>
2010-05-11 20:01:44 +0200

    network/squid: Added to 12.0 repository

---
+++ network/squid/doinst.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.squid.new:
+if [ -e etc/rc.d/rc.squid ]; then
+  cp -a etc/rc.d/rc.squid etc/rc.d/rc.squid.new.incoming
+  cat etc/rc.d/rc.squid.new > etc/rc.d/rc.squid.new.incoming
+  mv etc/rc.d/rc.squid.new.incoming etc/rc.d/rc.squid.new
+else
+  # Install executable otherwise - irrelevant unless user starts in rc.local
+  chmod 0755 etc/rc.d/rc.squid.new
+fi
+
+config etc/rc.d/rc.squid.new
+config etc/squid/mime.conf.new
+config etc/squid/squid.conf.new
+config etc/squid/cachemgr.conf.new
+
