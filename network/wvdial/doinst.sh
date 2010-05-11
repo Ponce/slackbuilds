6bcbf9f38260f529b65261f04b6cdf3ca74e2b55
Vincent Batts <vbatts@batts.mine.nu>
2010-05-11 22:55:05 +0200

    network/wvdial: Added to 12.1 repository

---
+++ network/wvdial/doinst.sh
@@ -0,0 +1,17 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/wvdial.conf.new
+config etc/ppp/peers/wvdial-pipe.new
+config etc/ppp/peers/wvdial.new
+
