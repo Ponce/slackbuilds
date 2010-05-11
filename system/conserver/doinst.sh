e5f1fc75e36b4e043b9a3aeb81c1c6dd590fad69
Menno E. Duursma <druiloor@zonnet.nl>
2010-05-11 19:46:24 +0200

    system/conserver: Updated for version 8.1.16

--- system/conserver/doinst.sh
+++ system/conserver/doinst.sh
@@ -1,20 +1,27 @@
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+# Keep same perms on rc.conserver:
+if [ -e etc/rc.d/rc.conserver ]; then
+  cp -a etc/rc.d/rc.conserver etc/rc.d/rc.conserver.new.incoming
+  cat etc/rc.d/rc.conserver.new > etc/rc.d/rc.conserver.new.incoming
+  mv etc/rc.d/rc.conserver.new.incoming etc/rc.d/rc.conserver.new
+fi
+
+config etc/rc.d/rc.conserver.new
 config etc/conserver.cf.new
 config etc/conserver.passwd.new
 config etc/console.cf.new
-config etc/rc.d/rc.conserver.new
 
 # Make sure there is a service mapping for conserver
 if ! grep -q "^conserver" etc/services ; then
