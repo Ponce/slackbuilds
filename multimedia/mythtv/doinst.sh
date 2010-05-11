aceffd1739d64109b74a9a38af1bd017957f7c63
David Somero <dsomero@hotmail.com>
2010-05-11 20:01:23 +0200

    multimedia/mythtv: Added to 12.0 repository

---
+++ multimedia/mythtv/doinst.sh
@@ -0,0 +1,29 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then 
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.mythbackend.new:
+if [ -e etc/rc.d/rc.mythbackend ]; then
+  cp -a etc/rc.d/rc.mythbackend etc/rc.d/rc.mythbackend.new.incoming
+  cat etc/rc.d/rc.mythbackend.new > etc/rc.d/rc.mythbackend.new.incoming
+  mv etc/rc.d/rc.mythbackend.new.incoming etc/rc.d/rc.mythbackend.new
+fi
+
+config etc/rc.d/rc.mythbackend.new
+
+echo
+echo "Extra configuration needed..."
+echo 
+echo "A mythtv mysql database needs to be setup and"
+echo "mythtv-setup has to be run; see the README"
+echo
+
