4fd7a6d9d4346a0ea09895a4fd991b4f68541825
Ricardson Williams <ricardsonwilliams@yahoo.com.br>
2010-05-11 20:02:02 +0200

    system/monit: Added to 12.0 repository

---
+++ system/monit/doinst.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.monit.new:
+if [ -e etc/rc.d/rc.monit ]; then
+  cp -a etc/rc.d/rc.monit etc/rc.d/rc.monit.new.incoming
+  cat etc/rc.d/rc.monit.new > etc/rc.d/rc.monit.new.incoming
+  mv etc/rc.d/rc.monit.new.incoming etc/rc.d/rc.monit.new
+fi
+
+config etc/rc.d/rc.monit.new
+
