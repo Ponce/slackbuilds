70855a90948fdcb2203d45863af1afa6e91d1f7f
Christophe Nguyen <cnguyenminhtan@free.fr>
2010-05-11 22:53:25 +0200

    desktop/un-fonts-core: Added to 12.1 repository

---
+++ desktop/un-fonts-core/doinst.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Update mkfontscale and mkfontdir:
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+# Update the X font indexes:
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
+
+# For some versions before Slackware 12.1
+if [ -x /usr/X11R6/bin/mkfontdir ]; then
+  ( cd /usr/X11R6/lib/X11/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+if [ -x /usr/X11R6/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
