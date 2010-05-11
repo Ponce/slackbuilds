93d0e96a0d534af95c5b766ec551a500dc4d6c46
Menno E. Duursma <druiloor@zonnet.nl>
2010-05-11 19:46:22 +0200

    system/cfengine: Updated for version 2.2.3

--- system/cfengine/doinst.sh
+++ system/cfengine/doinst.sh
@@ -1,19 +1,24 @@
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
    # toss the redundant copy
    rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Add a sample rc file for the admin to consider
-config etc/rc.d/rc.cfengine.new
+# Keep same perms on rc.cfengine:
+if [ -e etc/rc.d/rc.cfengine ]; then
+  cp -a etc/rc.d/rc.cfengine etc/rc.d/rc.cfengine.new.incoming
+  cat etc/rc.d/rc.cfengine.new > etc/rc.d/rc.cfengine.new.incoming
+  mv etc/rc.d/rc.cfengine.new.incoming etc/rc.d/rc.cfengine.new
+fi
 
+config etc/rc.d/rc.cfengine.new
 config var/cfengine/inputs/update.conf.new
 config var/cfengine/inputs/cfagent.conf.new
 config var/cfengine/inputs/cfservd.conf.new
