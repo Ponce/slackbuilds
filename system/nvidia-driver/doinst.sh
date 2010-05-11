682760848256a56de42b9e10c4929feba888fcdf
Heinz Wiesinger <HMWiesinger@gmx.at>
2010-05-11 20:02:03 +0200

    system/nvidia-driver: Added to 12.0 repository

---
+++ system/nvidia-driver/doinst.sh
@@ -0,0 +1,28 @@
+
+if [ -x usr/bin/update-desktop-database ]; then
+  ./usr/bin/update-desktop-database ./usr/share/applications >/dev/null 2>&1
+fi
+
+if ! [ -e usr/lib/xorg/modules/libwfb.so ]; then
+  ( cd usr/lib/xorg/modules ; ln -s libnvidia-wfb.so.1 libwfb.so )
+fi
+
+( cd usr/lib/tls ; rm -rf libnvidia-tls.so.1 )
+( cd usr/lib/tls ; ln -sf tls/libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
+( cd usr/lib/xorg/modules ; rm -rf libnvidia-wfb.so.1 )
+( cd usr/lib/xorg/modules ; ln -sf libnvidia-wfb.so.PKGVERSION libnvidia-wfb.so.1 )
+( cd usr/lib ; rm -rf libnvidia-cfg.so.1 )
+( cd usr/lib ; ln -sf libnvidia-cfg.so.PKGVERSION libnvidia-cfg.so.1 )
+( cd usr/lib ; rm -rf libnvidia-tls.so.1 )
+( cd usr/lib ; ln -sf libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
+( cd usr/lib ; rm -rf libXvMCNVIDIA_dynamic.so.1 )
+( cd usr/lib ; ln -sf libXvMCNVIDIA.so.PKGVERSION libXvMCNVIDIA_dynamic.so.1 )
+( cd usr/lib ; rm -rf libnvidia-cfg.so )
+( cd usr/lib ; ln -sf libnvidia-cfg.so.1 libnvidia-cfg.so )
+( cd usr/lib ; rm -rf libcuda.so.1 )
+( cd usr/lib ; ln -sf libcuda.so.PKGVERSION libcuda.so.1 )
+( cd usr/lib ; rm -rf libcuda.so )
+( cd usr/lib ; ln -sf libcuda.so.1 libcuda.so )
+
+/usr/sbin/nvidia-switch --nvidia
+
