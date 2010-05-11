0cbdb460b9714b854b8aeb4ab02d0baf55201b7b
core <eroc@linuxmail.org>
2010-05-11 20:02:09 +0200

    system/terminus-font: Added to 12.0 repository

---
+++ system/terminus-font/doinst.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Update mkfontscale and mkfontdir:
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/local
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+# Update the X font indexes:
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
+
+# For some versions before Slackware 12.0
+if [ -x /usr/X11R6/bin/mkfontdir ]; then
+  ( cd /usr/X11R6/lib/X11/fonts/local
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+if [ -x /usr/X11R6/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
