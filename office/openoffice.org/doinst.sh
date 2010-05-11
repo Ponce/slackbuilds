c35e6fbc3ac90cf92a75de773a558a0112551ad2
Robby Workman <rw@rlworkman.net>
2010-05-11 22:26:11 +0200

    office/openoffice.org: Updated for version 3.0.0

--- office/openoffice.org/doinst.sh
+++ office/openoffice.org/doinst.sh
@@ -5,3 +5,12 @@ fi
 if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then 
+  for theme in gnome locolor hicolor ; do
+    if [ -e usr/share/icons/$theme/icon-theme.cache ]; then 
+      /usr/bin/gtk-update-icon-cache usr/share/icons/$theme >/dev/null 2>&1
+    fi
+  done
+fi
+
