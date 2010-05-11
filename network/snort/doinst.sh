6a3bb08a09dcad867ccfe36ee1c3e739ec68d4ab
Alan_Hicks <alan@lizella.net>
2010-05-11 15:01:35 +0200

    network/snort: Initial import

---
+++ network/snort/doinst.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/snort/snort.conf.new
+config etc/snort/reference.config.new
+config etc/snort/threshold.conf.new
+
