2f04f4ee24e14e523c1c31ed4decb0240376725b
core <eroc@linuxmail.org>
2010-05-11 20:01:15 +0200

    multimedia/ffmpeg: Added to 12.0 repository

---
+++ multimedia/ffmpeg/ffmpeg-mksrctarball.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# This script helps you to download and compress the
+# sourcecode for ffmpeg from its svn server.
+
+set -e
+
+PRGNAM=ffmpeg
+VERSION=$(date +"%Y%m%d")
+SVNSERVER=svn://svn.mplayerhq.hu
+
+echo "--> Downloading sourcecode from $SVNSERVER"
+svn checkout $SVNSERVER/$PRGNAM/trunk $PRGNAM-$VERSION 2>&1 | tee svn-$PRGNAM-$VERSION.log
+
+echo "--> Making the sourcecode tarball: $PRGNAM-$VERSION.tar.bz2 "
+tar -c $PRGNAM-$VERSION/ | bzip2 > $PRGNAM-$VERSION.tar.bz2
+
+echo "--> Erasing the sourcecode directory: $PRGNAM-$VERSION/"
+rm -rf $PRGNAM-$VERSION/
+
+echo "--> Sourcecode tarball for $PRGNAM: $PRGNAM-$VERSION.tar.bz2"
