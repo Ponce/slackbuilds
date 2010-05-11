20f2d0a33aff6d2564546c0892c59934873ab231
Erik Hanson <erik@slackbuilds.org>
2010-05-11 19:44:18 +0200

    development/medit: Updated for version 0.8.5

--- development/medit/doinst.sh
+++ development/medit/doinst.sh
@@ -1,3 +1,12 @@
 if [ -x usr/bin/update-desktop-database ]; then
-	usr/bin/update-desktop-database &> /dev/null
+  usr/bin/update-desktop-database > /dev/null 2>&1
 fi
+
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -x usr/bin/gtk-update-icon-cache ]; then
+  gtk-update-icon-cache -f -t usr/share/icons/hicolor > /dev/null 2>&1
+fi
+
