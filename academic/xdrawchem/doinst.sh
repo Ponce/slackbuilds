0778f1b46abce2f348a69d8bfae638ae71070a30
Robby Workman <rw@rlworkman.net>
2010-05-11 19:43:43 +0200

    academic/xdrawchem: Updated for version 1.9.9

--- academic/xdrawchem/doinst.sh
+++ academic/xdrawchem/doinst.sh
@@ -1,4 +1,3 @@
-
 if [ -x usr/bin/update-desktop-database ]; then
   ./usr/bin/update-desktop-database ./usr/share/applications >/dev/null 2>&1
 fi
@@ -6,4 +5,3 @@ fi
 if [ -x usr/bin/update-mime-database ]; then
   ./usr/bin/update-mime-database ./usr/share/mime >/dev/null 2>&1
 fi
-
