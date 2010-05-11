66e3f7d0bdcb60b0ba015ba3959124a80a69a99a
David Somero <dsomero@hotmail.com>
2010-05-11 22:54:38 +0200

    network/avahi: Added to 12.1 repository

---
+++ network/avahi/doinst.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.avahidaemon.new:
+if [ -e etc/rc.d/rc.avahidaemon ]; then
+  cp -a etc/rc.d/rc.avahidaemon etc/rc.d/rc.avahidaemon.new.incoming
+  cat etc/rc.d/rc.avahidaemon.new > etc/rc.d/rc.avahidaemon.new.incoming
+  mv etc/rc.d/rc.avahidaemon.new.incoming etc/rc.d/rc.avahidaemon.new
+fi
+
+# Keep same perms on rc.avahidnsconfd.new:
+if [ -e etc/rc.d/rc.avahidnsconfd ]; then
+  cp -a etc/rc.d/rc.avahidnsconfd etc/rc.d/rc.avahidnsconfd.new.incoming
+  cat etc/rc.d/rc.avahidnsconfd.new > etc/rc.d/rc.avahidnsconfd.new.incoming
+  mv etc/rc.d/rc.avahidnsconfd.new.incoming etc/rc.d/rc.avahidnsconfd.new
+fi
+
+config etc/rc.d/rc.avahidaemon.new
+config etc/rc.d/rc.avahidnsconfd.new
+config etc/avahi/avahi-daemon.conf.new
+config etc/dbus-1/system.d/avahi-dbus.conf.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
