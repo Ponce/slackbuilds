cf3f72ac05bf8525e1fc80e2828af775698fba1a
David Somero <dsomero@hotmail.com>
2010-05-11 22:54:29 +0200

    misc/qstat: Added to 12.1 repository

---
+++ misc/qstat/doinst.sh
@@ -0,0 +1,15 @@
+# Handle configuration files
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+# List of configuration files (they should end in .new)
+
+config etc/qstat.cfg.new
