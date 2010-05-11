9c5cee6a07a862d5cf9848dbd9eb2303e0f70e07
Martin Lefebvre <dadexter@gmail.com>
2010-05-11 19:44:18 +0200

    development/mono: Updated for version 1.2.4

--- development/mono/doinst.sh
+++ development/mono/doinst.sh
@@ -1,11 +1,11 @@
 
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
@@ -19,3 +19,4 @@ config etc/mono/2.0/web.config.new
 config etc/mono/2.0/DefaultWsdlHelpGenerator.aspx.new
 config etc/mono/config.new
 config etc/mono/browscap.ini.new
+
