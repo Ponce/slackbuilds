f7fdac81525a319d4afadb831f164162f5c7566c
Menno Duursma <druiloor@zonnet.nl>
2010-05-13 00:25:36 +0200

    development/splint: Updated for version 3.1.2

--- development/splint/doinst.sh
+++
@@ -1,7 +0,0 @@
-# If we do not have a lint already:
-if ! command -v lint 1> /dev/null 2> /dev/null ; then
-  # Make this the default
-  ( cd /usr/bin ; ln -sf splint lint )
-  ( cd /usr/man/man1 ; ln -sf splint.1.gz lint.1.gz )
-fi
-
