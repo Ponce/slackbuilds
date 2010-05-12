505448c69c363ce87911caafca73f70dfb37ccfb
Heinz Wiesinger <pprkut@slackbuilds.org>
2010-05-12 11:47:46 +0200

    development/mod_perl: Removed from 12.2 repository

--- development/mod_perl/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -r etc/httpd/mod_perl.conf ]; then
-  cat etc/httpd/mod_perl.conf.example > etc/httpd/mod_perl.conf
-elif [ "$(cat etc/httpd/mod_perl.conf 2> /dev/null)" = "" ]; then
-  cat etc/httpd/mod_perl.conf.example > etc/httpd/mod_perl.conf
-fi
-
