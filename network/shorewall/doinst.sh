ab1d59ca5d1f0e84c2689b272068b2097b2746ff
ArTourter <artourter@gmail.com>
2010-05-13 01:00:22 +0200

    network/shorewall: Added to 13.0 repository

---
+++ network/shorewall/doinst.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# vim: et ts=2 sw=2 
+
+config() {
+  NEW="$1"
+  OLD="${NEW%*.new}"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.firewall.new
+preserve_perms etc/rc.d/rc.shorewall.new
+
