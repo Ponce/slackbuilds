88b000b53fad146da4a1fa04e7f215cb4190a376
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2010-05-13 00:25:02 +0200

    development/netbeans: Updated for version 6.7.1

--- development/netbeans/doinst.sh
+++ development/netbeans/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -13,14 +11,21 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config opt/netbeans/etc/netbeans.conf.new
-config opt/netbeans/etc/netbeans.clusters.new
-
 if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications 2>/dev/null
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
 if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi  
+fi
+
+config opt/netbeans/etc/netbeans.conf.new
+config opt/netbeans/etc/netbeans.clusters.new
+config opt/netbeans/etc/netbeans.import.new
 
