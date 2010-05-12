a4f48c6e2036f6636a786f85bbbd41f023230042
Erik Hanson <erik@slackbuilds.org>
2010-05-12 23:33:36 +0200

    system/gnome-vfs: Added to 12.2 repository

---
+++ system/gnome-vfs/doinst.sh
@@ -0,0 +1,14 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install desktop_default_applications.schemas
+schema_install desktop_gnome_url_handlers.schemas
+schema_install system_dns_sd.schemas
+schema_install system_http_proxy.schemas
+schema_install system_smb.schemas
+
