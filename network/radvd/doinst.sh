6ba5faeff93c5be66c8b9d766bb0ebb9e9007ff2
Sean Donner <sean.donner@gmail.com>
2010-05-13 00:38:08 +0200

    network/radvd: Updated for version 1.5

--- network/radvd/doinst.sh
+++ network/radvd/doinst.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
@@ -19,5 +18,13 @@ if [ -e etc/rc.d/rc.radvd ]; then
   mv etc/rc.d/rc.radvd.new.incoming etc/rc.d/rc.radvd.new
 fi
 
+# Keep same perms on radvd.conf.new:
+if [ -e etc/radvd.conf ]; then
+  cp -a etc/radvd.conf etc/radvd.conf.new.incoming
+  cat etc/radvd.conf.new > etc/radvd.conf.new.incoming
+  mv etc/radvd.conf.new.incoming etc/radvd.conf.new
+fi
+
 config etc/rc.d/rc.radvd.new
 config etc/radvd.conf.new
+
