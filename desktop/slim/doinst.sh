bbe2cab46caaabd380fbb4598bfa6086049594f0
Frank Caraballo <fecaraballo{at}gmail{dot}com>
2010-05-12 17:38:05 +0200

    desktop/slim: Updated for version 1.3.1

--- desktop/slim/doinst.sh
+++ desktop/slim/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -13,13 +11,5 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.slim.new:
-if [ -e etc/rc.d/rc.slim ]; then
-  cp -a etc/rc.d/rc.slim etc/rc.d/rc.slim.new.incoming
-  cat etc/rc.d/rc.slim.new > etc/rc.d/rc.slim.new.incoming
-  mv etc/rc.d/rc.slim.new.incoming etc/rc.d/rc.slim.new
-fi
-
-config etc/rc.d/rc.slim.new
 config etc/slim.conf.new
 
