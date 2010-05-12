8d1d8aa9a5801d5e80fb6e1a2d0fcadc8db90865
Mauro Giachero <mauro dot giachero at gmail dot com>
2010-05-13 00:42:15 +0200

    system/vice: Updated for version 2.1

--- system/vice/doinst.sh
+++ system/vice/doinst.sh
@@ -1,3 +1,16 @@
+# Update desktop icons
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+
+# Update the X font indexes:
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
+
