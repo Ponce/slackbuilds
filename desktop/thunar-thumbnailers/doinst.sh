db6eedae69f6ffff9eeeb26e517238fcde328bfb
Robby Workman <rw@rlworkman.net>
2010-05-13 00:23:15 +0200

    desktop/thunar-thumbnailers: Updated for version 0.4.1

--- desktop/thunar-thumbnailers/doinst.sh
+++ desktop/thunar-thumbnailers/doinst.sh
@@ -1,5 +1,4 @@
-
-if [ -x usr/bin/update-mime-database ]; then
-  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
