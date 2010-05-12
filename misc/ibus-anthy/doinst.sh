cffa06022cc9c0978104f4cf01cc98170bcafebb
Ozan Türkyılmaz <ozan.turkyilmaz@gmail.com>
2010-05-13 00:59:25 +0200

    misc/ibus-anthy: Added to 13.0 repository

---
+++ misc/ibus-anthy/doinst.sh
@@ -0,0 +1,6 @@
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
