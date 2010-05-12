bdc2f9ff500bf75ff33d972f12e3934821947b24
Heinz Wiesinger <pprkut@liwjatan.at>
2010-05-12 17:36:49 +0200

    audio/ardour: Updated for version 2.8

--- audio/ardour/doinst.sh
+++ audio/ardour/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -25,3 +23,4 @@ if [ -x /usr/bin/gtk-update-icon-cache ] \
   && [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
 fi
+
