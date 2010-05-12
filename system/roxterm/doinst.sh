029ae4f08f253e8d3bca1b6c72fce65387edfed3
Erik Hanson <erik@slackbuilds.org>
2010-05-12 17:45:56 +0200

    system/roxterm: Updated for version 1.14.2

--- system/roxterm/doinst.sh
+++ system/roxterm/doinst.sh
@@ -1,4 +1,4 @@
-if [ -x usr/bin/update-desktop-database ]; then
-    usr/bin/update-desktop-database -q usr/share/applications
+if [ -x /usr/bin/update-desktop-database ]; then
+    /usr/bin/update-desktop-database -q usr/share/applications
 fi
 
