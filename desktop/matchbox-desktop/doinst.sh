9d5a1edec28d9072508be7113a0acc41fbe645c8
Daniel de Kok <danieldk@pobox.com>
2010-05-13 00:22:49 +0200

    desktop/matchbox-desktop: Updated for version 0.9.1

--- desktop/matchbox-desktop/doinst.sh
+++ desktop/matchbox-desktop/doinst.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
-
-if [ -x usr/bin/update-desktop-database ]; then
-    ./usr/bin/update-desktop-database ./usr/share/applications > /dev/null 2>&1
-fi
-
-# Handle the incoming configuration files:
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
-config etc/mbdesktop_modules.new
+
+config etc/matchbox/mbdesktop_modules.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
