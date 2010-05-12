9200a8fd01a6b398f604f0fc58b8341372e631c3
Jim Capozzoli <saltmiser@gmail.com>
2010-05-12 17:36:47 +0200

    audio/abcde: Updated for version 2.3.99.6

--- audio/abcde/doinst.sh
+++ audio/abcde/doinst.sh
@@ -1,10 +1,10 @@
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
