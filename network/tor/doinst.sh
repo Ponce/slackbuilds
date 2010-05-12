3be86dfb0f559ae557987a0ba6148307fd6c183b
Erik Hanson <erik@slackbuilds.org>
2010-05-13 00:38:27 +0200

    network/tor: Updated for version 0.2.1.22

--- network/tor/doinst.sh
+++ network/tor/doinst.sh
@@ -1,24 +1,28 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.tor.new:
-if [ -e etc/rc.d/rc.tor ]; then
-  cp -a etc/rc.d/rc.tor etc/rc.d/rc.tor.new.incoming
-  cat etc/rc.d/rc.tor.new > etc/rc.d/rc.tor.new.incoming
-  mv etc/rc.d/rc.tor.new.incoming etc/rc.d/rc.tor.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.tor.new
+preserve_perms etc/rc.d/rc.tor.new
 config etc/tor/tor-tsocks.conf.new
 config etc/tor/torrc.new
+
