a1ad0b9a01be72a555c68de0463d810542dcdbe3
Heinz Wiesinger <pprkut@slackbuilds.org>
2010-05-13 00:07:31 +0200

    games/Phalanx: Removed from 13.0 repository

--- games/Phalanx/doinst.sh
+++
@@ -1,7 +0,0 @@
-# Don't clobber an existing /var/games/phalanx/learn.phalanx
-if [ ! -r var/games/phalanx/learn.phalanx ]; then
-  mv var/games/phalanx/learn.phalanx.new var/games/phalanx/learn.phalanx
-else
-  rm -f var/games/phalanx/learn.phalanx.new
-fi
-
