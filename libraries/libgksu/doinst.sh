3c09fc558d117a35729fe9c811f4c548a7dabf14
Stylianos Tsampas <ztsampas@otenet.gr>
2010-05-13 00:30:42 +0200

    libraries/libgksu: Updated for version 2.0.7

--- libraries/libgksu/doinst.sh
+++ libraries/libgksu/doinst.sh
@@ -3,11 +3,11 @@ chroot . gconftool-2 --makefile-install-rule \
     /etc/gconf/schemas/gksu.schemas \
     1>/dev/null 
 
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
-fi
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database &> /dev/null
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
 fi
 
