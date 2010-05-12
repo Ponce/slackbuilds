7c0fce924fb9e5005f5d8b499409e54c789614e1
Menno Duursma <druiloor@zonnet.nl>
2010-05-13 00:41:59 +0200

    system/tiger: Updated for version 3.2.3

--- system/tiger/doinst.sh
+++ system/tiger/doinst.sh
@@ -1,20 +1,17 @@
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
-
-# Try not to mess over any costum settings
 config etc/tiger/cronrc.new
 config etc/tiger/tigerrc.new
-config usr/share/tiger/initdefs.new
-config usr/share/tiger/check.tbl.new
-config usr/share/tiger/syslist.new
-config usr/share/tiger/config.new
+config etc/tiger/tiger.ignore.new
+config etc/cron.d/tiger.new
+
