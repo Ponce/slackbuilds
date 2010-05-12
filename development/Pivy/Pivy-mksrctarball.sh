cd6705175cb352300a1eb2cf15e0925ce0f3b625
Aleksandar Samardzic <asamardzic@gmail.com>
2010-05-12 17:38:13 +0200

    development/Pivy: Updated for version 20081227

---
+++ development/Pivy/Pivy-mksrctarball.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script helps you to download and compress the sourcecode for
+# Pivy from its svn server.
+
+set -e
+
+PRGNAM=Pivy
+VERSION=$(date +"%Y%m%d")
+SVNSERVER=https://svn.coin3d.org/repos/
+
+echo "--> Downloading sourcecode from $SVNSERVER"
+echo "t" | svn export $SVNSERVER/$PRGNAM/trunk $PRGNAM-$VERSION 2>&1 | tee svn-$PRGNAM-$VERSION.log
+
+echo "--> Making the sourcecode tarball: $PRGNAM-$VERSION.tar.bz2 "
+tar -c $PRGNAM-$VERSION/ | bzip2 > $PRGNAM-$VERSION.tar.bz2
+
+echo "--> Erasing the sourcecode directory: $PRGNAM-$VERSION/"
+rm -rf $PRGNAM-$VERSION/
+
+echo "--> Sourcecode tarball for $PRGNAM: $PRGNAM-$VERSION.tar.bz2"
