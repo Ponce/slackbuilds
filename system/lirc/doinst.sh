987400e2380babcbc118603fb2e7c98794d1d82a
Murat D. Kadirov <banderols@gmail.com>
2010-05-13 00:40:51 +0200

    system/lirc: Updated for version 0.8.6

--- system/lirc/doinst.sh
+++ system/lirc/doinst.sh
@@ -1,19 +1,18 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then 
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 config etc/lircd.conf.new
 config etc/lircmd.conf.new
 config etc/lircrc.new
+config etc/logrotate.d/lirc.new
 
-chroot . /sbin/depmod -ae @KERNEL@
-
+if [ -x sbin/depmod ]; then
+  chroot . /sbin/depmod -ae @KERNEL@ 1> /dev/null 2> /dev/null
+fi
