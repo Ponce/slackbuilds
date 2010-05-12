85baeb20e1ccc3ceb113320d5740f21fea338b9c
Johann Wilhelm <johann.wilhelm@9mal6.de>
2010-05-12 17:45:29 +0200

    system/lirc: Updated for version 0.8.4a

--- system/lirc/doinst.sh
+++ system/lirc/doinst.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then 
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
@@ -15,4 +14,6 @@ config() {
 config etc/lircd.conf.new
 config etc/lircmd.conf.new
 config etc/lircrc.new
+
+chroot . /sbin/depmod -ae @KERNEL@
 
