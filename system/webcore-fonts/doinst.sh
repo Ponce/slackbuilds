b7aeae22648b1902233a63210416d1b003abb145
Dugan Chen <dugan [underscore] c [at] fastmail [dot] fm>
2010-05-13 00:42:23 +0200

    system/webcore-fonts: Updated for version 3.0

--- system/webcore-fonts/doinst.sh
+++ system/webcore-fonts/doinst.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 # Update the X font indexes:
 if [ -x /usr/bin/mkfontdir -o -x /usr/X11R6/bin/mkfontdir ]; then
   ( cd /usr/share/fonts/TTF
@@ -9,3 +8,4 @@ fi
 if [ -x /usr/bin/fc-cache ]; then
   /usr/bin/fc-cache -f
 fi
+
