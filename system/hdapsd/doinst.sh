4db878070ac8636be9cad28bdeae6ecf168ff662
alkos333 <me@alkso333.net>
2010-05-12 23:33:37 +0200

    system/hdapsd: Added to 12.2 repository

---
+++ system/hdapsd/doinst.sh
@@ -0,0 +1,23 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+# Keep same perms on rc.hdapsd.new:
+if [ -e etc/rc.d/rc.hdapsd ]; then
+  cp -a etc/rc.d/rc.hdapsd etc/rc.d/rc.hdapsd.new.incoming
+  cat etc/rc.d/rc.hdapsd.new > etc/rc.d/rc.hdapsd.new.incoming
+  mv etc/rc.d/rc.hdapsd.new.incoming etc/rc.d/rc.hdapsd.new
+fi
+
+config /etc/rc.d/rc.hdapsd.new
+config /etc/hdapsd.conf.new
+
