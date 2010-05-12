594b847b5585209e16c8b19b429945175ccb5df6
B. Watson <yalhcru@gmail.com>
2010-05-13 00:39:38 +0200

    system/atarisio: Updated for version 20100113

--- system/atarisio/doinst.sh
+++ system/atarisio/doinst.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-chroot . /sbin/depmod -a
+if [ -x sbin/depmod ]; then
+	chroot . /sbin/depmod -a
+fi
 
 # Ensure that udevd knows what's up with the atarisio device(s)...
-# Redirect stderr to /dev/null to avoid Slack 12.2 warning:
-# Older (Slack 12.0/12.1) udevadm doesn't recognize the new form
-# (--reload-rules is an error). For now, I want the package to
-# work on at least 12.1 and 12.2.
+# If you're on Slack 12.1 or older, the --reload-rules needs to be
+# replaced with --reload_rules below.
 
 if [ -x sbin/udevadm ]; then
-	sbin/udevadm control --reload_rules 2>/dev/null
+	sbin/udevadm control --reload-rules 2>/dev/null
 fi
