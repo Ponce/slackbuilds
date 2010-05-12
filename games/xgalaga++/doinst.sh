c4c4bba93578054da4dc87e3815d0895e0304bc8
Heinz Wiesinger <pprkut@slackbuilds.org>
2010-05-13 00:07:42 +0200

    games/xgalaga++: Removed from 13.0 repository

--- games/xgalaga++/doinst.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -e var/games/xgalaga++/xgalaga++.scores ]; then
-  mv var/games/xgalaga++/xgalaga++.scores.new var/games/xgalaga++/xgalaga++.scores
-fi
-rm -f var/games/xgalaga++/xgalaga++.scores.new
-
