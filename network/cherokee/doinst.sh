fef1426727f109d6a7383982f947d554af87d272
Pablo Hernan Saro <pablosaro@gmail.com>
2010-05-13 00:35:56 +0200

    network/cherokee: Updated for version 0.99.24

--- network/cherokee/doinst.sh
+++ network/cherokee/doinst.sh
@@ -9,9 +9,15 @@ config() {
 }
 
 if [ -e etc/rc.d/rc.cherokee ]; then
-  cp -a etc/rc.d/rc.cherokee etc/rc.d/rc.cherokee.new.incoming
-  cat etc/rc.d/rc.cherokee.new > etc/rc.d/rc.cherokee.new.incoming
-  mv etc/rc.d/rc.cherokee.new.incoming etc/rc.d/rc.cherokee.new
+	cp -a etc/rc.d/rc.cherokee etc/rc.d/rc.cherokee.new.incoming
+	cat etc/rc.d/rc.cherokee.new > etc/rc.d/rc.cherokee.new.incoming
+	mv etc/rc.d/rc.cherokee.new.incoming etc/rc.d/rc.cherokee.new
+fi
+
+if [ -e etc/cherokee/cherokee.conf ]; then
+	cp -a etc/cherokee/cherokee.conf etc/cherokee/cherokee.conf.new.incoming
+	cat etc/cherokee/cherokee.conf.new > etc/cherokee/cherokee.conf.new.incoming
+	mv etc/cherokee/cherokee.conf.new.incoming etc/cherokee/cherokee.conf.new
 fi
 
 config etc/rc.d/rc.cherokee.new
