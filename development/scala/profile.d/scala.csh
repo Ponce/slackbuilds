f8b36fd3ee3331aa6884afae9547b5d75f481bdf
Matthias Diehn Ingesman <matthias@pf-c.dk>
2010-05-13 00:58:03 +0200

    development/scala: Added to 13.0 repository

---
+++ development/scala/profile.d/scala.csh
@@ -0,0 +1,4 @@
+#!/bin/csh
+setenv SCALA_HOME @LIBDIR@/scala
+setenv MANPATH ${MANPATH}:${SCALA_HOME}/man
+setenv PATH ${PATH}:${SCALA_HOME}/bin
---
+++ development/scala/profile.d/scala.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export SCALA_HOME=@LIBDIR@/scala
+export MANPATH="${MANPATH}:${SCALA_HOME}/man"
+export PATH="${PATH}:${SCALA_HOME}/bin"
