953c8fb3fc46f5eb175647fcbbfac643cbe869be
Heinz Wiesinger <pprkut@liwjatan.at>
2010-05-13 00:41:09 +0200

    system/nvidia-legacy173-driver: Updated for version 173.14.22

--- system/nvidia-legacy173-driver/doinst.sh
+++ system/nvidia-legacy173-driver/doinst.sh
@@ -2,25 +2,25 @@ if [ -x /usr/bin/update-desktop-database ]; then
   ./usr/bin/update-desktop-database -q usr/share/applications
 fi
 
-if ! [ -e usr/lib/xorg/modules/libwfb.so ]; then
-  ( cd usr/lib/xorg/modules ; ln -s libnvidia-wfb.so.1 libwfb.so )
+if ! [ -e usr/libLIBDIRSUFFIX/xorg/modules/libwfb.so ]; then
+  ( cd usr/libLIBDIRSUFFIX/xorg/modules ; ln -s libnvidia-wfb.so.1 libwfb.so )
 fi
 
-( cd usr/lib/tls ; rm -rf libnvidia-tls.so.1 )
-( cd usr/lib/tls ; ln -sf tls/libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
-( cd usr/lib/xorg/modules ; rm -rf libnvidia-wfb.so.1 )
-( cd usr/lib/xorg/modules ; ln -sf libnvidia-wfb.so.PKGVERSION libnvidia-wfb.so.1 )
-( cd usr/lib ; rm -rf libnvidia-cfg.so.1 )
-( cd usr/lib ; ln -sf libnvidia-cfg.so.PKGVERSION libnvidia-cfg.so.1 )
-( cd usr/lib ; rm -rf libnvidia-tls.so.1 )
-( cd usr/lib ; ln -sf libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
-( cd usr/lib ; rm -rf libXvMCNVIDIA_dynamic.so.1 )
-( cd usr/lib ; ln -sf libXvMCNVIDIA.so.PKGVERSION libXvMCNVIDIA_dynamic.so.1 )
-( cd usr/lib ; rm -rf libnvidia-cfg.so )
-( cd usr/lib ; ln -sf libnvidia-cfg.so.1 libnvidia-cfg.so )
-( cd usr/lib ; rm -rf libcuda.so.1 )
-( cd usr/lib ; ln -sf libcuda.so.PKGVERSION libcuda.so.1 )
-( cd usr/lib ; rm -rf libcuda.so )
-( cd usr/lib ; ln -sf libcuda.so.1 libcuda.so )
+( cd usr/libLIBDIRSUFFIX/tls ; rm -rf libnvidia-tls.so.1 )
+( cd usr/libLIBDIRSUFFIX/tls ; ln -sf libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
+( cd usr/libLIBDIRSUFFIX/xorg/modules ; rm -rf libnvidia-wfb.so.1 )
+( cd usr/libLIBDIRSUFFIX/xorg/modules ; ln -sf libnvidia-wfb.so.PKGVERSION libnvidia-wfb.so.1 )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libnvidia-cfg.so.1 )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libnvidia-cfg.so.PKGVERSION libnvidia-cfg.so.1 )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libnvidia-tls.so.1 )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libnvidia-tls.so.PKGVERSION libnvidia-tls.so.1 )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libXvMCNVIDIA_dynamic.so.1 )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libXvMCNVIDIA.so.PKGVERSION libXvMCNVIDIA_dynamic.so.1 )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libnvidia-cfg.so )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libnvidia-cfg.so.1 libnvidia-cfg.so )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libcuda.so.1 )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libcuda.so.PKGVERSION libcuda.so.1 )
+( cd usr/libLIBDIRSUFFIX ; rm -rf libcuda.so )
+( cd usr/libLIBDIRSUFFIX ; ln -sf libcuda.so.1 libcuda.so )
 
 /usr/sbin/nvidia-switch --install
