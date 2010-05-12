fa216410db68135df67deb45c7fabd0e05c906d2
Erik Hanson <erik@slackbuilds.org>
2010-05-12 17:39:19 +0200

    games/kobodeluxe: Updated for version 0.5.1

--- games/kobodeluxe/doinst.sh
+++ games/kobodeluxe/doinst.sh
@@ -1,3 +1,3 @@
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database usr/share/applications &> /dev/null
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database usr/share/applications &> /dev/null
 fi
