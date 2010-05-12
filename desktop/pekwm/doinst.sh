13fda597655a5cae9ccff94ca73fc64302f9dbc2
Eugene Suter <easuter@gmail.com>
2010-05-13 00:23:04 +0200

    desktop/pekwm: Updated for version 0.1.11

---
+++ desktop/pekwm/doinst.sh
@@ -0,0 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/pekwm/config.new
+config etc/pekwm/start.new
+config etc/pekwm/autoproperties.new
+config etc/pekwm/mouse.new
+config etc/pekwm/vars.new
+config etc/pekwm/menu.new
+config etc/pekwm/keys.new
