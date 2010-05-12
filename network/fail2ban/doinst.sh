be48e858385bef134f4bcb7a55d1325f4b16f5e2
Nishant Limbachia <nishant@mnspace.net>
2010-05-13 00:36:23 +0200

    network/fail2ban: Updated for version 0.8.4

--- network/fail2ban/doinst.sh
+++ network/fail2ban/doinst.sh
@@ -20,4 +20,11 @@ fi
 
 config etc/rc.d/rc.fail2ban.new
 config etc/logrotate.d/fail2ban.new
-
+config etc/fail2ban/fail2ban.conf.new
+config etc/fail2ban/jail.conf.new
+for conf_file in etc/fail2ban/action.d/*.new; do
+  config $conf_file
+done
+for conf_file in etc/fail2ban/filter.d/*.new; do
+  config $conf_file
+done
