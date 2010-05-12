b3a4ac24499aef1caa034e6391f29b671ec7b021
Stylianos Tsampas <ztsampas@otenet.gr>
2010-05-13 00:40:27 +0200

    system/gksu: Updated for version 2.0.0

--- system/gksu/doinst.sh
+++ system/gksu/doinst.sh
@@ -1,8 +1,8 @@
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
-fi
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database &> /dev/null
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database /usr/share/mime &> /dev/null
 fi
 
