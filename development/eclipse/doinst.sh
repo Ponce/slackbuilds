8e5a81f1f03e9720661f9a23cbaa260646b3e4a6
Antonio Hernández Blas <hba.nihilismus@gmail.com>
2010-05-13 00:24:30 +0200

    development/eclipse: Updated for version 3.5.1

--- development/eclipse/doinst.sh
+++ development/eclipse/doinst.sh
@@ -12,13 +12,20 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi  
+fi
+
 config opt/eclipse/configuration/config.ini.new
 config opt/eclipse/eclipse.ini.new
 
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database -q usr/share/applications
-fi
-
-if [ -x usr/bin/update-mime-database ]; then
-  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
-fi
