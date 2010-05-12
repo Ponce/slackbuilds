b83a67ebf626bbe4bf6f9558db2ce44881e340ce
Michiel van Wessem <michiel@slackbuilds.org>
2010-05-12 17:44:25 +0200

    network/vnstat: Updated for version 1.7

--- network/vnstat/doinst.sh
+++ network/vnstat/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
@@ -13,5 +11,12 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/vnstat.conf.new
+# Keep same perms on rc.vnstat.new:
+if [ -e etc/rc.d/rc.vnstat ]; then
+  cp -a etc/rc.d/rc.vnstat etc/rc.d/rc.vnstat.new.incoming
+  cat etc/rc.d/rc.vnstat.new > etc/rc.d/rc.vnstat.new.incoming
+  mv etc/rc.d/rc.vnstat.new.incoming etc/rc.d/rc.vnstat.new
+fi
 
+config etc/rc.d/rc.vnstat.new
+config etc/vnstat.conf.new
