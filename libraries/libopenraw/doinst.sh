fc4191c267018099130985abf6c895ed83cf8881
Michiel van Wessem <michiel@slackbuilds.org>
2010-05-13 00:31:30 +0200

    libraries/libopenraw: Updated for version 0.0.8

---
+++ libraries/libopenraw/doinst.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/$PRGNAM.conf.new
+
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database -q usr/share/applications
+fi
+
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
