0e64bd0f789a3ce1119dc0b3476f8b1feb057ab1
Erik Hanson <erik@slackbuilds.org>
2010-05-13 00:26:59 +0200

    games/qmc2: Updated for version 0.2.b13

--- games/qmc2/doinst.sh
+++ games/qmc2/doinst.sh
@@ -1,3 +1,19 @@
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications &> /dev/null
 fi
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/qmc2/qmc2.ini.new
+
