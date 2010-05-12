369589a48d98d9cf0d1e4f6f22545c6e772d732d
B. Watson <yalhcru@gmail.com>
2010-05-13 00:26:32 +0200

    games/koules: Updated for version 1.4

--- games/koules/doinst.sh
+++ games/koules/doinst.sh
@@ -1,3 +1,3 @@
 if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications &> /dev/null
+  /usr/bin/update-desktop-database -q usr/share/applications 1> /dev/null 2>/dev/null
 fi
