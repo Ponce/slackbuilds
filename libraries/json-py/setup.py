4e44e15c0a4f5dcde23c49743422a1f8ba16f177
Marco Bonetti <sid77@slackware.it>
2010-05-12 23:30:23 +0200

    libraries/json-py: Added to 12.2 repository

---
+++ libraries/json-py/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+import json
+
+setup(name="json",
+      version="3.4",
+      description="JSON (http://json.org) reader and writer.",
+      author="Patrick D. Logan",
+      author_email="patrickdlogan@stardecisions.com",
+      url = "http://sourceforge.net/projects/json-py/",
+      py_modules=['json']
+     )
+
