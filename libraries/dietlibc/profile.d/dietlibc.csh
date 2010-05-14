d22501b876eeb6e154be3330316604801f6f3e07
markus reichelt <slackbuilds@mareichelt.de>
2010-05-14 01:06:11 -0500

    libraries/dietlibc: Added (small libc implementation).

---
+++ libraries/dietlibc/profile.d/dietlibc.csh
@@ -0,0 +1,3 @@
+#!/bin/csh
+setenv PATH ${PATH}:/opt/diet/bin
+setenv MANPATH ${MANPATH}:/opt/diet/man
---
+++ libraries/dietlibc/profile.d/dietlibc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH="${PATH}:/opt/diet/bin"
+export MANPATH="${MANPATH}:/opt/diet/man"
