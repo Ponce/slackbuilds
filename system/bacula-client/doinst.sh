53952012d1c465875ac024e1cc24a4af9dcb8e7e
mario <mario@slackverse.org>
2010-06-16 06:25:31 -0500

    system/bacula-client: Updated for version 5.0.2.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- system/bacula-client/doinst.sh
+++ system/bacula-client/doinst.sh
@@ -11,14 +11,17 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.bacula.new:
-if [ -e etc/rc.d/rc.bacula ]; then
-  cp -a etc/rc.d/rc.bacula etc/rc.d/rc.bacula.new.incoming
-  cat etc/rc.d/rc.bacula.new > etc/rc.d/rc.bacula.new.incoming
-  mv etc/rc.d/rc.bacula.new.incoming etc/rc.d/rc.bacula.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
+preserve_perms etc/rc.d/rc.bacula.new
 config etc/bacula/bacula-fd.conf.new
 config etc/bacula/bconsole.conf.new
-config etc/rc.d/rc.bacula.new
-
