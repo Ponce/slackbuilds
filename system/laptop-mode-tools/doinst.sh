188355e7db3e109e638d4959c7fc422890f3d65d
V'yacheslav Stetskevych <slava18@gmail.com>
2010-07-02 07:54:20 -0500

    system/laptop-mode-tools: Added (laptop power saving package)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ system/laptop-mode-tools/doinst.sh
@@ -0,0 +1,32 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+for conf in etc/laptop-mode/*.conf.new etc/laptop-mode/conf.d/*.conf.new; do
+	config "$conf"
+done
+
+preserve_perms etc/rc.d/rc.laptop-mode.new
+
+killall -SIGHUP acpid 2>/dev/null
+killall -SIGHUP apmd 2>/dev/null
