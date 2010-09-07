a3dd5a4576f6be2b95c3bf4b005e2ac0608fa186
Manlio Modugno <manliomodugno -# #- gmail-# #- com>
2010-09-07 10:36:54 -0400

    system/apache-cassandra: Updated for version 0.6.5.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- system/apache-cassandra/doinst.sh
+++ system/apache-cassandra/doinst.sh
@@ -11,7 +11,7 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.tomcat.new:
+# Keep same perms on rc.cassandra.new:
 if [ -e etc/rc.d/rc.cassandra ]; then
   cp -a etc/rc.d/rc.cassandra etc/rc.d/rc.cassandra.new.incoming
   cat etc/rc.d/rc.cassandra.new > etc/rc.d/rc.cassandra.new.incoming
@@ -19,3 +19,21 @@ if [ -e etc/rc.d/rc.cassandra ]; then
 fi
 
 config etc/rc.d/rc.cassandra.new
+
+# Keep same perms on storage-conf.xml.new:
+if [ -e etc/apache-cassandra/storage-conf.xml ]; then
+  cp -a etc/apache-cassandra/storage-conf.xml etc/apache-cassandra/storage-conf.xml.new.incoming
+  cat etc/apache-cassandra/storage-conf.xml.new > etc/apache-cassandra/storage-conf.xml.new.incoming
+  mv etc/apache-cassandra/storage-conf.xml.new.incoming etc/apache-cassandra/storage-conf.xml.new
+fi
+
+config etc/apache-cassandra/storage-conf.xml.new
+
+# Keep same perms on cassandra.in.sh.new:
+if [ -e etc/apache-cassandra/cassandra.in.sh ]; then
+  cp -a etc/apache-cassandra/cassandra.in.sh etc/apache-cassandra/cassandra.in.sh.new.incoming
+  cat etc/apache-cassandra/cassandra.in.sh.new > etc/apache-cassandra/cassandra.in.sh.new.incoming
+  mv etc/apache-cassandra/cassandra.in.sh.new.incoming etc/apache-cassandra/cassandra.in.sh.new
+fi
+
+config etc/apache-cassandra/cassandra.in.sh.new
