7d5ffabc4dd36157ffce5ed8f57a38bfb4307350
Nilton Moura <nmoura@nmoura.eti.br>
2010-09-09 10:48:20 -0400

    network/keepalived: Added (HealthChecking for LVS & HA cluters)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

---
+++ network/keepalived/doinst.sh
@@ -0,0 +1,23 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+if [ -e etc/rc.d/rc.keepalived ]; then
+  cp -a etc/rc.d/rc.keepalived etc/rc.d/rc.keepalived.new.incoming
+  cat etc/rc.d/rc.keepalived.new > etc/rc.d/rc.keepalived.new.incoming
+  mv etc/rc.d/rc.keepalived.new.incoming etc/rc.d/rc.keepalived.new
+fi
+
+config etc/keepalived/keepalived.new
+config etc/keepalived/keepalived.conf.new
+config etc/rc.d/rc.keepalived.new
+
