959b26fabdc770a2454b560eb1f908693cdfea7d
David Spencer <baildon.research@googlemail.com>
2010-09-12 11:17:26 -0500

    graphics/darktable: Added (digital photography application)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ graphics/darktable/doinst.sh
@@ -0,0 +1,20 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install darktable.schemas
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
