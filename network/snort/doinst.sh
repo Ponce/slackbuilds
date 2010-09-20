75f2ad568a76b131b75575e8054e098dd819a01d
Niels Horn <niels.horn@gmail.com>
2010-09-20 19:23:25 -0400

    network/snort: Added (Intrusion Detection and Prevention System)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

---
+++ network/snort/doinst.sh
@@ -0,0 +1,33 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.snort.new
+config etc/snort/snort.conf.new
+config etc/snort/reference.config.new
+config etc/snort/threshold.conf.new
+config etc/snort/attribute_table.dtd.new
+config etc/snort/classification.config.new
+config etc/snort/gen-msg.map.new
+config etc/snort/sid-msg.map.new
+config etc/snort/unicode.map.new
+
