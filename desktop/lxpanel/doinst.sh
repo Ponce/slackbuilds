45004f6b09a941c80958701e2a1f03ab434e183f
Michael Wagner <lapinours@web.de>
2010-09-20 20:25:50 -0400

    desktop/lxpanel: Updated for version 0.5.6.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- desktop/lxpanel/doinst.sh
+++ desktop/lxpanel/doinst.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
-
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config usr/share/lxpanel/profile/default/panels/panel.new 
-config usr/share/lxpanel/profile/default/config.new 
+config usr/share/lxpanel/profile/default/panels/panel.new
+config usr/share/lxpanel/profile/default/config.new
