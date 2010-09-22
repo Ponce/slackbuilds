966d20d5bb36f212c7bdd444d9150a4022ec540e
Binh Nguyen <binhnguyen@fastmail.fm>
2010-09-22 17:47:37 -0500

    desktop/notification-daemon: Added (the name says it all)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ desktop/notification-daemon/doinst.sh
@@ -0,0 +1,18 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+schema_install notificaton-daemon.schemas
+
+chroot . /etc/rc.d/rc.messagebus reload
+
