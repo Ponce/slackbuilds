aaed14602bae4bad693cf41467faabce9f8d41a6
Ponce <matteo.bernardini@sns.it>
2010-10-17 09:28:14 -0400

    desktop/lxde-common: Fixed naming of config files.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- desktop/lxde-common/doinst.sh
+++ desktop/lxde-common/doinst.sh
@@ -1,23 +1,3 @@
-config() {
-  for infile in \$1; do
-    NEW="\$infile"
-    OLD="\`dirname \$NEW\`/\`basename \$NEW .new\`"
-    # If there's no config file by that name, mv it over:
-    if [ ! -r \$OLD ]; then
-      mv \$NEW \$OLD
-    elif [ "\`cat \$OLD | md5sum\`" = "\`cat \$NEW | md5sum\`" ]; then
-      # toss the redundant copy
-      rm \$NEW
-    fi
-    # Otherwise, we leave the .new copy for the admin to consider...
-  done
-}
-
-config etc/xdg/lxsession/LXDE/autostart.new
-config etc/xdg/lxsession/LXDE/desktop.conf.new
-config etc/xdg/pcmanfm/main.lxde.new
-config etc/xdg/pcmanfm/pcmanfm.conf.new
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications &> /dev/null
 fi
