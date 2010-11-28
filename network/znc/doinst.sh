3638387f02c31f93e79a9aec3158e65b33780247
Sean Donner <sean.donner@gmail.com>
2010-11-28 02:26:02 -0600

    network/znc: Updated for version 0.096.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- network/znc/doinst.sh
+++ network/znc/doinst.sh
@@ -1,22 +1,22 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
-
-# Keep same perms on rc.znc.new:
-if [ -e etc/rc.d/rc.znc ]; then
-  cp -a etc/rc.d/rc.znc etc/rc.d/rc.znc.new.incoming
-  cat etc/rc.d/rc.znc.new > etc/rc.d/rc.znc.new.incoming
-  mv etc/rc.d/rc.znc.new.incoming etc/rc.d/rc.znc.new
-fi
-
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 config etc/rc.d/rc.znc.new
+preserve_perms etc/rc.d/rc.znc.new 
 
