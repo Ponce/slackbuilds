40e678c3aea6d5b3e771dbb5600ce234df729fa7
Alan Hicks <alan@lizella.net>
2010-12-06 22:53:21 -0600

    network/dovecot: Updated for version 2.0.8.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- network/dovecot/doinst.sh
+++ network/dovecot/doinst.sh
@@ -10,13 +10,15 @@ config() {
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
-
-# Keep same perms on rc.dovecot.new:
-if [ -e etc/rc.d/rc.dovecot ]; then
-  cp -a etc/rc.d/rc.dovecot etc/rc.d/rc.dovecot.new.incoming
-  cat etc/rc.d/rc.dovecot.new > etc/rc.d/rc.dovecot.new.incoming
-  mv etc/rc.d/rc.dovecot.new.incoming etc/rc.d/rc.dovecot.new
-fi
-
-config etc/rc.d/rc.dovecot.new
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+preserve_perms etc/rc.d/rc.dovecot.new
 
