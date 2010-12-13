0d3df4c6f2282e4f00c2eaa2471751c5609691ae
Manuel Reimer <Manuel.Reimer@gmx.de>
2010-12-13 15:53:01 -0200

    misc/iscan-firmware: Added (firmware files for EPSON USB scanners)
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

---
+++ misc/iscan-firmware/doinst.sh
@@ -0,0 +1,5 @@
+echo
+echo Be sure to read /usr/doc/iscan-firmware-2.8.0.1/README.Slackware
+echo for instructions on adding support for loading firmware to your
+echo /etc/sane.d/snapscan.conf file.
+echo 
