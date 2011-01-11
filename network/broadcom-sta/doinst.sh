a2482f799fffd1c2735ff16ba5f0a4c66ff1f67f
David Matthew Jerry Koenig <koenigdavidmj@gmail.com>
2011-01-11 12:04:30 -0600

    network/broadcom-sta: Updated for version 5.100.82.38.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- network/broadcom-sta/doinst.sh
+++ network/broadcom-sta/doinst.sh
@@ -1,5 +1,5 @@
 # A good idea whenever kernel modules are added or changed:
 if [ -x sbin/depmod ]; then
-  /sbin/depmod -a @KERNEL@ 1> /dev/null 2> /dev/null
+  /sbin/depmod -a 1> /dev/null 2> /dev/null
 fi
 
