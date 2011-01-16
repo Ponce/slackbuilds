0a7698b8ef778b69ff69f400666185b026d0a429
M.Dinslage <daedra1980@gmail.com>
2011-01-16 00:11:34 -0600

    misc/conkyforecast: Reverted to version 2.12.
    Due to problems with 2.13, 2.14, and 2.15.
    Contact maintainer for more information.
    
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- misc/conkyforecast/doinst.sh
+++ misc/conkyforecast/doinst.sh
@@ -3,6 +3,9 @@ if [ -x /usr/bin/mkfontdir ]; then
   ( cd /usr/share/fonts/TTF
     mkfontscale .
     mkfontdir .
+    cd /usr/share/fonts/OTF
+    mkfontscale .
+    mkfontdir .
   )
 fi
 if [ -x /usr/bin/fc-cache ]; then
