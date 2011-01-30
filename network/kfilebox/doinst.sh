29a5d5e4b3549df93bcf69b7b6feab7fd12f0f4b
Alan Alberghini <414N@slacky.it>
2011-01-30 10:00:14 -0600

    network/kfilebox: Added (KDE Dropbox client)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ network/kfilebox/doinst.sh
@@ -0,0 +1,16 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -e usr/share/icons/oxygen/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/oxygen >/dev/null 2>&1
+  fi
+fi
+
