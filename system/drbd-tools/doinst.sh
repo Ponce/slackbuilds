cd50870a09e7591b461b533f37c5d43f4315ce97
dsomero <xgizzmo@slackbuilds.org>
2011-03-23 18:21:19 -0400

    system/drbd-tools: Handle .new files correctly.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- system/drbd-tools/doinst.sh
+++ system/drbd-tools/doinst.sh
@@ -11,13 +11,18 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.drbd.new:
-if [ -e etc/rc.d/rc.drbd ]; then
-  cp -a etc/rc.d/rc.drbd etc/rc.d/rc.drbd.new.incoming
-  cat etc/rc.d/rc.drbd.new > etc/rc.d/rc.drbd.new.incoming
-  mv etc/rc.d/rc.drbd.new.incoming etc/rc.d/rc.drbd.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.drbd.new
-config etc/drbd.conf.new
-config etc/drbd.d/global_common.conf.new
+preserve_perms etc/rc.d/rc.drbd.new
+preserve_perms etc/drbd.conf.new
+preserve_perms etc/drbd.d/global_common.conf.new
+
