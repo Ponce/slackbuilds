e821e5f6f8667c89f7910f88f904e3d103ba97a0
dsomero <xgizzmo@slackbuilds.org>
2011-03-23 18:19:25 -0400

    network/mod_geoip2: Handle .new files correctly.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- network/mod_geoip2/doinst.sh
+++ network/mod_geoip2/doinst.sh
@@ -11,5 +11,15 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/httpd/mod_geoip.conf.new
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
+preserve_perms etc/httpd/extra/mod_geoip.conf.new
