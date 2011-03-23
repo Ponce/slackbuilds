24a857c6cc4902982b68f70ba2c360c1d83d7061
dsomero <xgizzmo@slackbuilds.org>
2011-03-23 18:17:38 -0400

    network/haproxy: Handle .new files correctly.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- network/haproxy/doinst.sh
+++ network/haproxy/doinst.sh
@@ -11,11 +11,18 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.haproxy.new:
-if [ -e etc/rc.d/rc.haproxy ]; then
-  cp -a etc/rc.d/rc.haproxy etc/rc.d/rc.haproxy.new.incoming
-  cat etc/rc.d/rc.haproxy.new > etc/rc.d/rc.haproxy.new.incoming
-  mv etc/rc.d/rc.haproxy.new.incoming etc/rc.d/rc.haproxy.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.haproxy.new
+preserve_perms etc/rc.d/rc.haproxy.new
+config etc/haproxy/haproxy.cfg.new
+
+
