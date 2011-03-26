318a515e5186d92e92ae7cc9cc4a93345148deb9
Robby Workman <rworkman@slackbuilds.org>
2011-03-25 23:11:31 -0500

    misc/gnome-keyring: Updated for version 2.32.1.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- misc/gnome-keyring/doinst.sh
+++ misc/gnome-keyring/doinst.sh
@@ -1,5 +1,1 @@
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/gnome-keyring.schemas \
-    1>/dev/null 
-
+chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/
