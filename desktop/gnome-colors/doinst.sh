8711b2a6aeade52cbe7a777f51e2ccb7856a0213
Eugene Wissner <eugen@flevum.de>
2011-05-04 22:27:28 -0500

    desktop/gnome-colors: Misc tweaks ; email change
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- desktop/gnome-colors/doinst.sh
+++ desktop/gnome-colors/doinst.sh
@@ -1,5 +1,7 @@
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+for theme in brave carbonite colors-common dust human illustrious noble tribute wine wise ; do
+  if [ -e usr/share/icons/gnome-$theme/icon-theme.cache ]; then
+    if [ -x /usr/bin/gtk-update-icon-cache ]; then
+      /usr/bin/gtk-update-icon-cache usr/share/icons/gnome-$theme >/dev/null 2>&1
+    fi
   fi
-fi
+done
