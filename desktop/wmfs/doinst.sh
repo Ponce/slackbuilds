5f21d4dff2ed450ff124a25ce7487cd4ef8c02d6
Benjamin Trigona-Harany <bosth@alumni.sfu.ca>
2011-05-15 22:05:37 -0500

    desktop/wmfs: Added (dynamic tiling window manager)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ desktop/wmfs/doinst.sh
@@ -0,0 +1,10 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+config etc/wmfs/wmfsrc.new
