afa1c01d62ab2bb4a56c47947d74ee639b75fa45
mario <mario@slackverse.org>
2011-07-03 13:41:59 -0400

    development/freetds: Added (Tabular Data Stream protocol)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

---
+++ development/freetds/doinst.sh
@@ -0,0 +1,16 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/freetds/pool.conf.new
+config etc/freetds/freetds.conf.new
+config etc/freetds/locales.conf.new
