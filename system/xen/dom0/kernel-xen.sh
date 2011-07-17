29f2e42098ae40204d9009b56d3e1f4145bd5b30
mario <mario@slackverse.org>
2011-07-17 12:43:12 -0300

    system/xen: Added (the Xen virtualization hypervisor)
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

---
+++ system/xen/dom0/kernel-xen.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+# Xem dom0 kernel installation script
+
+# Written by Chris Abela <chris.abela@maltats.com>, 20100515
+# Updated by mario <mario@slackverse.org>, 2010-2011
+
+KERNEL=${KERNEL:-huge}
+VERSION=${VERSION:-2.6.34.7}
+
+# Rebased patches version
+SVERSION=${SVERSION:-2.6.34-6}
+
+# Xen version
+XVERSION=${XVERSION:-4.1.0}
+
+BOOTLOADER=${BOOTLOADER:-lilo}
+
+# Automatically determine the architecture we're building on:
+if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH=i486 ;;
+    x86_64) ARCH=x86_64 ;;
+    # Bail out on everything else:
+       *) echo "Unsupported architecture detected ($ARCH)"; exit ;;
+  esac
+fi
+
+CWD=$(pwd)
+XEN=${XEN:-/tmp/xen}
+PATCH=${PATCH:-/tmp/xen/patch-$VERSION}
+MODINST=${MODINST:-/tmp/xen/modules}
+
+set -e
+
+rm -rf $XEN
+mkdir -p $PATCH $MODINST
+cd $PATCH
+tar -xvf $CWD/xen-patches-$SVERSION.tar.bz2
+
+if [ ! -d /usr/src/linux-$VERSION ]; then
+  echo "Missing kernel source in /usr/src/linux-$VERSION"
+  echo "Get it from kernel.org and rerun this script."
+  exit
+fi
+
+# Prepare kernel source
+cd /usr/src
+rm -rf linux-$VERSION-xen xenlinux
+cp -a linux-$VERSION linux-$VERSION-xen
+ln -s linux-$VERSION-xen xenlinux
+
+cd linux-$VERSION-xen
+make clean
+for i in $PATCH/* ; do
+  echo Patching $i
+  patch -p1 -s < $i
+done
+
+# Copy the right config
+cat $CWD/config-${KERNEL}-${ARCH}-${VERSION}-xen > .config
+
+# If the user wants, we will run menuconfig
+if [ "$MENUCONFIG" = yes ]; then
+  unset junk
+  make menuconfig
+  while [ "$junk" != N ]; do
+    echo
+    echo "Do you want to run"
+    echo "# make menuconfig"
+    echo -n "again? (Y/N) "
+    read junk
+    if [ "$junk" = Y ]; then
+      make menuconfig
+    fi
+  done
+fi
+
+make vmlinux modules
+make modules_install INSTALL_MOD_PATH=$MODINST
+
+# We already have these
+rm -rf $MODINST/lib/firmware
+
+# Strip kernel symbols
+strip vmlinux -o vmlinux-stripped
+
+# For lilo we pack kernel up with mbootpack
+if [ "$BOOTLOADER" = lilo ]; then
+  gzip -d -c /boot/xen-${XVERSION}.gz > xen-${XVERSION}
+  mbootpack -o vmlinux-stripped-mboot -m vmlinux-stripped xen-${XVERSION}
+  gzip vmlinux-stripped-mboot -c > vmlinuz
+elif [ "$BOOTLOADER" = grub ]; then
+  gzip vmlinux-stripped -c > vmlinuz
+fi
+
+install -D -m 644 vmlinuz /boot/vmlinuz-$KERNEL-$VERSION-xen
+install -m 644 System.map /boot/System.map-$KERNEL-$VERSION-xen
+install -m 644 .config /boot/config-$KERNEL-$VERSION-xen
+
+cd /boot
+ln -s vmlinuz-$KERNEL-$VERSION-xen vmlinuz-xen
+ln -s System.map-$KERNEL-$VERSION-xen System.map-xen
+ln -s config-$KERNEL-$VERSION-xen config-xen
+
+cp -a $MODINST/lib/modules/$VERSION-xen /lib/modules
+
+cd /usr/src/linux-$VERSION-xen
+make clean
---
+++ system/xen/domU/domU.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# This Script builds a Slackware domU Xen Guest on a Slackware host,
+# Although it might work correctly, this script is intended as a template, so
+# simplicity is the priority here.
+# Written by Chris Abela <chris.abela@maltats.com>, 20100308
+# Updated by mario <mario@slackverse.org>, 2010-2011
+
+set -e
+
+# Build an image for the root file system and another for the swap
+# Default values : 8GB and 500MB resepectively.
+ROOT_MB=${ROOT_MB:-8000}
+SWAP_MB=${SWAP_MB:-500}
+dd if=/dev/zero of=slackware.img bs=1M count=0 seek=$ROOT_MB
+mkfs.ext4 -F slackware.img
+dd if=/dev/zero of=swap_file bs=1M count=0 seek=$SWAP_MB
+mkswap swap_file
+
+# Make a mountpoint for the root file system and mount it
+mkdir -p mnt
+mount -o loop slackware.img mnt
+
+# Make a mountpoint for proc and mount it
+mkdir -p mnt/proc
+mount --bind /proc mnt/proc
+
+##############################################################################
+#									     #
+# IMPORTANT : This assumes that you have mounted your Slackware DVD on	     #
+#		/media/SlackDVD						     #
+#									     #
+##############################################################################
+
+# This will install a domU with the listed packages
+for i in a ap d e f k l n t tcl; do
+  installpkg --root mnt/ /media/SlackDVD/slackware*/$i/*.t?z
+done
+chroot mnt /sbin/ldconfig
+
+# create fstab
+cat >mnt/etc/fstab <<EOF
+/dev/xvda2       swap             swap        defaults         0   0
+/dev/xvda1       /                ext4        defaults         1   1
+#/dev/cdrom      /mnt/cdrom       auto        noauto,owner,ro  0   0
+/dev/fd0         /mnt/floppy      auto        noauto,owner     0   0
+devpts           /dev/pts         devpts      gid=5,mode=620   0   0
+proc             /proc            proc        defaults         0   0
+tmpfs            /dev/shm         tmpfs       defaults         0   0
+EOF
+
+chroot mnt /usr/sbin/timeconfig		# Set the time
+chroot mnt /sbin/netconfig		# Set the network
+chroot mnt /usr/bin/passwd		# Set root's password
+
+# Before we could use xencons=tty and leave inittab and securetty files intact,
+# but that stopped working as of Xen-4.x, so this has to be fixed by adding hvc0.
+sed 's/^\(c[1-6]:123\)/#\1/' /etc/inittab
+echo -e '\nc1:12345:respawn:/sbin/agetty 38400 hvc0 linux' >> /etc/inittab
+echo -e '\nhvc0' >> /etc/securetty
+
+# This will save us an alarming (yet harmless) warning
+(cd mnt/lib/modules
+  if [ -d 2.6.37.6-smp ]; then
+    # for Slack32
+    ln -s 2.6.37.6-smp 2.6.34.7-xen
+    else
+    # for Slack64
+    ln -s 2.6.37.6 2.6.34.7-xen
+  fi
+)
+
+# unmount proc and the filesystem
+umount mnt/proc
+umount mnt
