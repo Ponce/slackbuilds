89ec349ce612284f8d6ce74b837a14b96e61966b
LukenShiro <lukenshiro@ngi.it>
2011-08-10 10:46:41 -0300

    network/spamassassin: Updated for version 3.3.2.
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

--- network/spamassassin/doinst.sh
+++ network/spamassassin/doinst.sh
@@ -11,22 +11,22 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same permissions on rc.spamd.new as an existing rc.spamd:
-if [ -e etc/rc.d/rc.spamd ]; then
-  cp -a etc/rc.d/rc.spamd etc/rc.d/rc.spamd.new.incoming
-  cat etc/rc.d/rc.spamd.new > etc/rc.d/rc.spamd.new.incoming
-  mv etc/rc.d/rc.spamd.new.incoming etc/rc.d/rc.spamd.new
-else
-  # If rc.spamd does not exist, make the new one executable by default, 
-  # which won't matter unless it's called from rc.local anyway
-  chmod 0755 etc/rc.d/rc.spamd.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
+preserve_perms etc/rc.d/rc.spamd.new
 config etc/mail/spamassassin/init.pre.new
 config etc/mail/spamassassin/local.cf.new
 config etc/mail/spamassassin/v310.pre.new
 config etc/mail/spamassassin/v312.pre.new
 config etc/mail/spamassassin/v320.pre.new
 config etc/mail/spamassassin/v330.pre.new
-config etc/rc.d/rc.spamd.new
 config etc/spamassassin.conf.new
--- network/spamassassin/sa-update.sh
+++ network/spamassassin/sa-update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/usr/bin/sa-update > /dev/null
+/usr/bin/sa-update > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    [ -x /etc/rc.d/rc.spamd ] && /etc/rc.d/rc.spamd restart > /dev/null
+    [ -x /etc/rc.d/rc.spamd ] && /etc/rc.d/rc.spamd restart > /dev/null 2>&1
 fi
 
