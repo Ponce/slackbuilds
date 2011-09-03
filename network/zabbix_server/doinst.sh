3260d33953153ebf6f7fbaa6412c25b957d96a0c
Niels Horn <niels.horn@gmail.com>
2011-09-03 09:21:15 -0400

    network/zabbix_server: Updated for version 1.8.6.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- network/zabbix_server/doinst.sh
+++ network/zabbix_server/doinst.sh
@@ -10,14 +10,18 @@ config() {
     # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.zabbix_server.new:
-if [ -e etc/rc.d/rc.zabbix_server ]; then
-  cp -a etc/rc.d/rc.zabbix_server etc/rc.d/rc.zabbix_server.new.incoming
-  cat etc/rc.d/rc.zabbix_server.new > etc/rc.d/rc.zabbix_server.new.incoming
-  mv etc/rc.d/rc.zabbix_server.new.incoming etc/rc.d/rc.zabbix_server.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.zabbix_server.new
+preserve_perms etc/rc.d/rc.zabbix_server.new
 config etc/zabbix/zabbix_server.conf.new
 config var/log/zabbix/zabbix_server.log.new
 rm -f var/log/zabbix/zabbix_server.log.new
