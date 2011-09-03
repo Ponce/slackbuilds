ec410d942c11c4cb2ef9b8a7c280f5b6736308ee
Niels Horn <niels.horn@gmail.com>
2011-09-03 09:18:14 -0400

    network/ddclient: Updated for version 3.8.1.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- network/ddclient/doinst.sh
+++ network/ddclient/doinst.sh
@@ -12,24 +12,22 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.ddclient.new:
-if [ -e etc/rc.d/rc.ddclient ]; then
-  cp -a etc/rc.d/rc.ddclient etc/rc.d/rc.ddclient.new.incoming
-  cat etc/rc.d/rc.ddclient.new > etc/rc.d/rc.ddclient.new.incoming
-  mv etc/rc.d/rc.ddclient.new.incoming etc/rc.d/rc.ddclient.new
-fi
-config etc/rc.d/rc.ddclient.new
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.ddclient.new
 
 # Keep same perms on ddclient.conf.new:
 #   Normally, we don't bother with this for config files, but this one
 #   should usually be readable only by root, so that's how we'll install
 #   it.  However, if the admin changes it, we don't want to undo that.
-if [ -e etc/ddclient/ddclient.conf ]; then
-  cp -a etc/ddclient/ddclient.conf etc/ddclient/ddclient.conf.new.incoming
-  cat etc/ddclient/ddclient.conf.new > etc/ddclient/ddclient.conf.new.incoming
-  mv etc/ddclient/ddclient.conf.new.incoming etc/ddclient/ddclient.conf.new
- else
-  chmod 0600 etc/ddclient/ddclient.conf.new
-fi
-config etc/ddclient/ddclient.conf.new
+preserve_perms etc/ddclient/ddclient.conf.new
 
