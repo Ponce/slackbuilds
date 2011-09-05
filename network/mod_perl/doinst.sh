05bc19a05fa55e1f76965ee1257d920cc1b4a85b
Heinz Wiesinger <pprkut@liwjatan.at>
2011-09-05 13:42:35 -0500

    network/mod_perl: Updated for version 2.0.5.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- network/mod_perl/doinst.sh
+++ network/mod_perl/doinst.sh
@@ -1,6 +1,6 @@
-if [ ! -r etc/httpd/mod_perl.conf ]; then
-  cat etc/httpd/mod_perl.conf.example > etc/httpd/mod_perl.conf
-elif [ "$(cat etc/httpd/mod_perl.conf 2> /dev/null)" = "" ]; then
-  cat etc/httpd/mod_perl.conf.example > etc/httpd/mod_perl.conf
+if [ ! -r etc/httpd/extra/mod_perl.conf ]; then
+  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
+elif [ "$(cat etc/httpd/extra/mod_perl.conf 2> /dev/null)" = "" ]; then
+  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
 fi
 
