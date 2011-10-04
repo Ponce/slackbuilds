f097f5393482b8fed448964fdcd3931fe8c4d0ac
Niels Horn <niels.horn@gmail.com>
2011-10-04 08:19:09 -0500

    system/gcdemu: Added (gtk applet for controlling CDEmu daemon)
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

---
+++ system/gcdemu/doinst.sh
@@ -0,0 +1,13 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install gcdemu.schemas
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
