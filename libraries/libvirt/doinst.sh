427f5dc1e8e25cece9b976ec481982eefb74bd5b
Michal Bialozor <bialyy@o2.pl>
2011-10-07 09:15:26 -0300

    libraries/libvirt: Updated for version 0.9.4.
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

--- libraries/libvirt/doinst.sh
+++ libraries/libvirt/doinst.sh
@@ -12,9 +12,27 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/libvirt/libvirtd.conf.new
 config etc/libvirt/qemu.conf.new
 config etc/libvirt/qemu/networks/default.xml.new
+config etc/libvirt/libvirtd.conf.new
+config etc/libvirt/nwfilter/qemu-announce-self.xml.new
+config etc/libvirt/nwfilter/no-arp-spoofing.xml.new
+config etc/libvirt/nwfilter/no-mac-spoofing.xml.new
+config etc/libvirt/nwfilter/allow-incoming-ipv4.xml.new
+config etc/libvirt/nwfilter/allow-dhcp-server.xml.new
+config etc/libvirt/nwfilter/allow-arp.xml.new
+config etc/libvirt/nwfilter/no-other-rarp-traffic.xml.new
+config etc/libvirt/nwfilter/clean-traffic.xml.new
+config etc/libvirt/nwfilter/qemu-announce-self-rarp.xml.new
+config etc/libvirt/nwfilter/no-mac-broadcast.xml.new
+config etc/libvirt/nwfilter/no-ip-spoofing.xml.new
+config etc/libvirt/nwfilter/allow-dhcp.xml.new
+config etc/libvirt/nwfilter/no-other-l2-traffic.xml.new
+config etc/libvirt/nwfilter/allow-ipv4.xml.new
+config etc/libvirt/nwfilter/no-ip-multicast.xml.new
+config etc/libvirt/lxc.conf.new
 config etc/logrotate.d/libvirtd.new
+config etc/logrotate.d/libvirtd.lxc.new
+config etc/logrotate.d/libvirtd.qemu.new
+config etc/logrotate.d/libvirtd.uml.new
 config etc/sasl2/libvirt.conf.new
-
