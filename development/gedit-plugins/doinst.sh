77b451c918e63445731f07720c2098df03b54f16
byteframe <byteframe@gmail.com>
2011-10-08 08:47:51 -0500

    development/gedit-plugins: Added (GNOME Text Editor Plugins)
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

---
+++ development/gedit-plugins/doinst.sh
@@ -0,0 +1,11 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install gedit-show-tabbar-plugin.schemas
+schema_install gedit-drawspaces.schemas
+
