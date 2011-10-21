fd5d2a3446e3ac8d5e2e711f03cecd218ef0bc79
Zoran Stojakovic <stojakovic.zoran@gmail.com>
2011-10-21 10:56:09 -0200

    system/virt-manager: Updated for version 0.9.0 (+new maintainer)
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

--- system/virt-manager/doinst.sh
+++ system/virt-manager/doinst.sh
@@ -1,5 +1,19 @@
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/virt-manager.schemas \
-    1>/dev/null 
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
 
+schema_install virt-manager.schemas
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
