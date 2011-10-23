4e19a2eb824353e0e397cbe4991d1c5b7215cc4a
crocket <crockabiscuit@gmail.com>
2011-10-23 13:11:53 -0500

    audio/pulseaudio: Updated for version 1.1.
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

--- audio/pulseaudio/doinst.sh
+++ audio/pulseaudio/doinst.sh
@@ -11,6 +11,19 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+
 config etc/pulse/client.conf.new
 config etc/pulse/default.pa.new
 config etc/pulse/daemon.conf.new
+preserve_perms etc/rc.d/rc.pulseaudio.new
