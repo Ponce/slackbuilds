e2fccbd2cc544680ecc6eafc0944eab31e8f29a3
Andrea De Pasquale <andrea@de-pasquale.name>
2011-11-06 13:13:38 -0600

    multimedia/vlc: Added (VLC media player)
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

---
+++ multimedia/vlc/doinst.sh
@@ -0,0 +1,17 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ ! -e usr/lib%LIBDIRSUFFIX%/vlc/plugins*.dat ] ; then
+  /usr/lib%LIBDIRSUFFIX%/vlc/vlc-cache-gen -f usr/lib%LIBDIRSUFFIX%/vlc
+fi
