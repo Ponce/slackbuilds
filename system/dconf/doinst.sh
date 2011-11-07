d10103611402fe742ed2ac84adb6c8499488e958
Robby Workman <rworkman@slackbuilds.org>
2011-11-06 22:10:00 -0600

    system/dconf: Create the gio module cache during postinstall
    Thanks to disturbed1 on LQ for the tip.
    
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- system/dconf/doinst.sh
+++ system/dconf/doinst.sh
@@ -3,3 +3,7 @@ if [ -x etc/rc.d/rc.messagebus ]; then
   chroot . /etc/rc.d/rc.messagebus reload
 fi
 
+# Try to run these.  If they fail, no biggie.
+chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/ 1> /dev/null 2> /dev/null
+chroot . /usr/bin/gio-querymodules @LIBDIR@/gio/modules/ 1> /dev/null 2> /dev/null
+
