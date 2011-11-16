da6365ea18c04c80148a378e2e56be10b6d948cd
Heinz Wiesinger <pprkut@slackbuilds.org>
2011-11-16 17:39:14 +0100

    games/vbam: Added icon-theme and desktop file updating to doinst.sh
    Signed-off-by: Heinz Wiesinger <pprkut@slackbuilds.org>

--- games/vbam/doinst.sh
+++ games/vbam/doinst.sh
@@ -11,5 +11,15 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
 config etc/vbam.cfg.new
 
