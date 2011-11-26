5bf14287189b807744d60b83755494c90495ea64
Fridrich von Stauffenberg <cancellor2@gmail.com>
2011-11-26 09:42:55 -0600

    academic/scilab: Added (numerical computing package)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ academic/scilab/profile.d/scilab.csh
@@ -0,0 +1,6 @@
+#!/bin/csh
+
+setenv PATH ${PATH}:/opt/scilab/bin
+
+setenv PKG_CONFIG_PATH ${PKG_CONFIG_PATH}:/opt/scilab/lib/pkgconfig
+
---
+++ academic/scilab/profile.d/scilab.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+PATH=${PATH}:/opt/scilab/bin
+export PATH
+
+PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/opt/scilab/lib/pkgconfig
+export PKG_CONFIG_PATH
+
