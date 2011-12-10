d25436bde7f1404e0d2c40763be03c696a8caa1b
Marco Bonetti <sid77@slackware.it>
2011-12-10 16:44:25 -0600

    desktop/wmii: Added (window manager improved 2)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ desktop/wmii/doinst.sh
@@ -0,0 +1,19 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/wmii/welcome.new
+config etc/wmii/wmiirc.new
+config etc/wmii/plan9port/wmiirc.new
+config etc/wmii/python/wmiirc.new
+config etc/wmii/ruby/wmiirc.new
+
