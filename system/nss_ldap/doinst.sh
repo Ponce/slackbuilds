0208d4932e2ad17940e48da746d2c8f14025d6c6
Christopher Walker <kris240376@gmail.com>
2011-12-15 00:32:16 -0600

    system/nss_ldap: Updated for version 265.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- system/nss_ldap/doinst.sh
+++ system/nss_ldap/doinst.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
+
 config etc/ldap.conf.new
 
