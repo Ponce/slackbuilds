ee0d9cbd32309d14b3a0368485b0353e60fc7fd6
vvoody <vvoodywang@gmail.com>
2011-12-15 11:04:15 -0600

    misc/fcitx: Updated for version 4.1.2 (+new maintainer)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ misc/fcitx/profile.d/fcitx.csh
@@ -0,0 +1,7 @@
+#!/bin/csh
+
+setenv XMODIFIERS "@im=fcitx"
+setenv XIM fcitx
+setenv XIM_PROGRAM fcitx
+setenv GTK_IM_MODULE fcitx
+setenv QT_IM_MODULE fcitx
--- misc/fcitx/profile.d/fcitx.sh
+++ misc/fcitx/profile.d/fcitx.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-export GTK_IM_MODULE=xim
+
 export XMODIFIERS="@im=fcitx"
 export XIM=fcitx
 export XIM_PROGRAM=fcitx
-
+export GTK_IM_MODULE=fcitx  #=xim
+export QT_IM_MODULE=fcitx   #=xim
