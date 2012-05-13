79e444d58e10561a144653909b1c6ffb51f1a30d
B. Watson <yalhcru@gmail.com>
2012-05-13 09:05:35 -0400

    games/ds-models: Added (3D model packs for gzdoom and skulltag)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

---
+++ games/ds-models/svn2targz.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PRGNAM=ds-models
+VERSION=14
+REPO=http://ds-models.googlecode.com/svn/trunk/
+
+rm -rf $PRGNAM-r$VERSION
+svn export -r$VERSION $REPO $PRGNAM-r$VERSION
+tar cvfz $PRGNAM-r$VERSION.tar.gz $PRGNAM-r$VERSION
