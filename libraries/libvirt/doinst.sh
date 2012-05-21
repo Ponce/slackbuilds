70bb8c7b6094a03295b3defac36de0562db2da78
Michal Bialozor <bialyy@o2.pl>
2012-05-21 14:16:37 -0400

    libraries/libvirt: Updated for version 0.9.12.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- libraries/libvirt/doinst.sh
+++ libraries/libvirt/doinst.sh
@@ -14,6 +14,7 @@ config() {
 
 config etc/libvirt/qemu.conf.new
 config etc/libvirt/qemu/networks/default.xml.new
+config etc/libvirt/libvirt.conf.new
 config etc/libvirt/libvirtd.conf.new
 config etc/libvirt/nwfilter/qemu-announce-self.xml.new
 config etc/libvirt/nwfilter/no-arp-spoofing.xml.new
@@ -21,6 +22,8 @@ config etc/libvirt/nwfilter/no-mac-spoofing.xml.new
 config etc/libvirt/nwfilter/allow-incoming-ipv4.xml.new
 config etc/libvirt/nwfilter/allow-dhcp-server.xml.new
 config etc/libvirt/nwfilter/allow-arp.xml.new
+config etc/libvirt/nwfilter/no-arp-ip-spoofing.xml.new
+config etc/libvirt/nwfilter/no-arp-mac-spoofing.xml.new
 config etc/libvirt/nwfilter/no-other-rarp-traffic.xml.new
 config etc/libvirt/nwfilter/clean-traffic.xml.new
 config etc/libvirt/nwfilter/qemu-announce-self-rarp.xml.new
@@ -36,3 +39,4 @@ config etc/logrotate.d/libvirtd.lxc.new
 config etc/logrotate.d/libvirtd.qemu.new
 config etc/logrotate.d/libvirtd.uml.new
 config etc/sasl2/libvirt.conf.new
+config etc/sysctl.d/libvirtd.new
