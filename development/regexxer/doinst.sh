b1aabf801d562c2f5ca8b6d6168e8526a3892200
Andrey Maraev <andrew.maraev60@yandex.ru>
2012-06-30 12:30:49 -0400

    development/regexxer: Added (nifty GUI search/replace tool)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

---
+++ development/regexxer/doinst.sh
@@ -0,0 +1,24 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install regexxer.schemas
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/glib-compile-schemas ]; then
+  /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/
+fi
+
