cb3928b6eda11c9eea0f2fc9e7e08234e3f5beb1
David Melik <dchmelik@gmail.com>
2012-08-10 17:24:05 -0500

    games/moria: Added (a roguelike game)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ games/moria/doinst.sh
@@ -0,0 +1,17 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config var/games/moria/scores.new
+config etc/moria-hours.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
