a5b81ed1812d0073cf16fddcdb5b5951e53b6614
Heinz Wiesinger <pprkut@slackbuilds.org>
2012-09-02 18:58:40 +0200

    perl/mod_perl: Removed.
    This is not compatible with httpd 2.4 yet.
    
    Signed-off-by: Heinz Wiesinger <pprkut@slackbuilds.org>

--- perl/mod_perl/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ ! -r etc/httpd/extra/mod_perl.conf ]; then
-  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
-elif [ "$(cat etc/httpd/extra/mod_perl.conf 2> /dev/null)" = "" ]; then
-  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
-fi
-
