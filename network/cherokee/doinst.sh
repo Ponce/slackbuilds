a51e47323f61855e557323b94a96e9ec37208c08
Matteo Bernardini <ponce@slackbuilds.org>
2012-09-08 09:10:52 +0200

    network/cherokee: Updated for version 1.2.101.
    Also forced /var/log/cherokee and run with a dedicated user,
    various cleanups and additions to the README.
    
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- network/cherokee/doinst.sh
+++ network/cherokee/doinst.sh
@@ -8,17 +8,16 @@ config() {
   fi
 }
 
-if [ -e etc/rc.d/rc.cherokee ]; then
-	cp -a etc/rc.d/rc.cherokee etc/rc.d/rc.cherokee.new.incoming
-	cat etc/rc.d/rc.cherokee.new > etc/rc.d/rc.cherokee.new.incoming
-	mv etc/rc.d/rc.cherokee.new.incoming etc/rc.d/rc.cherokee.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-if [ -e etc/cherokee/cherokee.conf ]; then
-	cp -a etc/cherokee/cherokee.conf etc/cherokee/cherokee.conf.new.incoming
-	cat etc/cherokee/cherokee.conf.new > etc/cherokee/cherokee.conf.new.incoming
-	mv etc/cherokee/cherokee.conf.new.incoming etc/cherokee/cherokee.conf.new
-fi
-
-config etc/rc.d/rc.cherokee.new
+preserve_perms etc/rc.d/rc.cherokee.new
 config etc/cherokee/cherokee.conf.new
