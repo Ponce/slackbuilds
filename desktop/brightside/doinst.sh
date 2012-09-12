bf73bbddf737631baf62b8d432b37b5a1eb48636
Erik Hanson <erik@slackbuilds.org>
2012-09-12 15:45:06 -0500

    desktop/brightside: Removed.
    I can't seem to fix this.
    
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

--- desktop/brightside/doinst.sh
+++
@@ -1,9 +0,0 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
-
-GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-chroot . gconftool-2 --makefile-install-rule \
-    /usr/share/gconf/schemas/brightside.schemas \
-    1>/dev/null
-
