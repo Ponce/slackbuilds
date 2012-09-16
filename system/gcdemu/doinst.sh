af8bfeef74fa4fcbf2ca929e1ed752bce776ce58
Niels Horn <niels.horn@gmail.com>
2012-09-15 22:17:26 -0300

    system/gcdemu: Updated for version 1.5.0.
    Signed-off-by: Niels Horn <niels.horn@slackbuilds.org>

--- system/gcdemu/doinst.sh
+++ system/gcdemu/doinst.sh
@@ -1,13 +1,9 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install gcdemu.schemas
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
 fi
