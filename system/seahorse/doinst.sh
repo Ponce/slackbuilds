1703497b56ac7ad4c537913be23abadbd4c7da89
Matteo Bernardini <ponce@slackbuilds.org>
2012-09-23 17:40:40 +0200

    system/seahorse: Updated for version 3.4.1.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- system/seahorse/doinst.sh
+++ system/seahorse/doinst.sh
@@ -1,13 +1,3 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install seahorse.schemas
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -17,4 +7,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
