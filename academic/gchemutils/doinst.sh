f37bbbc550f5394ccd333f4b0f38574b91cde231
dsomero <xgizzmo@slackbuilds.org>
2012-09-27 07:27:16 -0400

    academic/gchemutils: Fixed (handle gconf schema)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- academic/gchemutils/doinst.sh
+++ academic/gchemutils/doinst.sh
@@ -1,3 +1,11 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -14,3 +22,7 @@ fi
 
 chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/ 1> /dev/null 2> /dev/null
 
+schema_install gchemutils.schemas
+schema_install gcrystal.schemas
+schema_install gchempaint.schemas
+schema_install gchempaint-arrows.schemas
