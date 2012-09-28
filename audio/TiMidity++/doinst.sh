b9b587d751bcfb5fd2c5697a3d0e70164612a324
dsomero <xgizzmo@slackbuilds.org>
2012-09-27 20:12:53 -0400

    audio/TiMidity++: Fixed (Keep perms on rc.* file)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- audio/TiMidity++/doinst.sh
+++ audio/TiMidity++/doinst.sh
@@ -4,12 +4,24 @@ config() {
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 config etc/timidity.cfg.new
-config etc/rc.d/rc.timidity.new
+preserve_perms etc/rc.d/rc.timidity.new
 
