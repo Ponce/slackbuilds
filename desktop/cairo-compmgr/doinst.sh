e3b4759d26ebc8ab349fa31025264225630bad3e
dsomero <xgizzmo@slackbuilds.org>
2012-09-27 22:34:17 -0400

    desktop/cairo-compmgr: Fixed (Handle schemas correctly)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- desktop/cairo-compmgr/doinst.sh
+++ desktop/cairo-compmgr/doinst.sh
@@ -1,3 +1,27 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install ccm-screen.schemas
+schema_install ccm-shadow.schemas
+schema_install ccm-window-animation.schemas
+schema_install ccm-freeze.schemas
+schema_install ccm-vala-window-plugin.schemas
+schema_install ccm-fade.schemas
+schema_install ccm-menu-animation.schemas
+schema_install ccm-perf.schemas
+schema_install ccm-snapshot.schemas
+schema_install ccm-magnifier.schemas
+schema_install ccm-mosaic.schemas
+schema_install ccm-automate.schemas
+schema_install ccm-opacity.schemas
+schema_install ccm-decoration.schemas
+schema_install ccm-display.schemas
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
