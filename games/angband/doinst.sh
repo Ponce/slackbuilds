fc4f21fa6f4d86c6ddfa1797a0979a4742b05802
dsomero <xgizzmo@slackbuilds.org>
2012-09-29 17:00:17 -0400

    games/angband: Fixed (Handle desktop file)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- games/angband/doinst.sh
+++ games/angband/doinst.sh
@@ -26,4 +26,6 @@ config etc/angband/edit/store.txt.new
 config etc/angband/edit/terrain.txt.new
 config etc/angband/edit/vault.txt.new
 
-
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
