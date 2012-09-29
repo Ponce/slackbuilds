3032ce580f99e781f534ffa6cad45bd039950b58
dsomero <xgizzmo@slackbuilds.org>
2012-09-28 20:25:19 -0400

    desktop/icewm: Fixed (Don't clobber config files)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- desktop/icewm/doinst.sh
+++ desktop/icewm/doinst.sh
@@ -1,3 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/X11/xinit/xinitrc.icewm.new
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications 2>/dev/null
 fi
