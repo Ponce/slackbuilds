5d95a346042e321f2890a2f706ded2230513f928
dsomero <xgizzmo@slackbuilds.org>
2012-09-30 11:49:36 -0400

    system/apcupsd: Fixed (Handle desktop file)
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- system/apcupsd/doinst.sh
+++ system/apcupsd/doinst.sh
@@ -31,3 +31,6 @@ config etc/apcupsd/offbattery.new
 config etc/apcupsd/onbattery.new
 config etc/logrotate.d/apcupsd.new
 
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
