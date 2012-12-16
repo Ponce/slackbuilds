5348ef7f2af521d72f1ab2a78240964cb4e0815a
Dugan Chen <thedoogster [at] gmail [dot] com>
2012-12-16 17:43:47 -0500

    system/adobe-source-sans-pro-font: Only install OTF fonts.
    Signed-off-by: dsomero <xgizzmo@slackbuilds.org>

--- system/adobe-source-sans-pro-font/doinst.sh
+++ system/adobe-source-sans-pro-font/doinst.sh
@@ -1,16 +1,8 @@
-# Update mkfontscale and mkfontdir:
-if [ -x /usr/bin/mkfontdir ]; then
-  ( cd /usr/share/fonts/OTF
-    mkfontscale .
-    mkfontdir .
-  )
-  ( cd /usr/share/fonts/TTF
-    mkfontscale .
-    mkfontdir .
-  )
-fi
-
 # Update the X font indexes:
+if [ -x /usr/bin/mkfontdir ]; then
+  /usr/bin/mkfontscale /usr/share/fonts/OTF
+  /usr/bin/mkfontdir /usr/share/fonts/OTF
+fi
 if [ -x /usr/bin/fc-cache ]; then
   /usr/bin/fc-cache -f
 fi
