aefc350250d96dea417c40f2667075b7d5614aa9
Michal Bialozor <bialyy@o2.pl>
2012-12-23 09:22:02 +0100

    network/zabbix_agentd: Updated for version 1.8.15, cleanups.
    (switched to new zabbixagent user http://bit.ly/TeN72F --ponce)
    
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- network/zabbix_agentd/doinst.sh
+++ network/zabbix_agentd/doinst.sh
@@ -1,13 +1,13 @@
 config() {
-    NEW="$1"
-    OLD="$(dirname $NEW)/$(basename $NEW .new)"
-    # If there's no config file by that name, mv it over:
-    if [ ! -r $OLD ]; then
-        mv $NEW $OLD
-    elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
-        rm $NEW
-    fi
-    # Otherwise, we leave the .new copy for the admin to consider...
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 preserve_perms() {
@@ -25,4 +25,3 @@ preserve_perms etc/rc.d/rc.zabbix_agentd.new
 config etc/zabbix/zabbix_agentd.conf.new
 config var/log/zabbix/zabbix_agentd.log.new
 rm -f var/log/zabbix/zabbix_agentd.log.new
-
