7300e7bd4d79f84b00b0d31cb3fffc53e77f6299
Christophe Trussardi <chris@teria.org>
2013-01-02 21:12:20 +0100

    network/pure-ftpd: Added (FTP server).
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

---
+++ network/pure-ftpd/doinst.sh
@@ -0,0 +1,26 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.pure-ftpd.new
+config etc/pure-ftpd/pure-ftpd.conf.new
+config etc/pure-ftpd/pureftpd-ldap.conf.new
+config etc/pure-ftpd/pureftpd-mysql.conf.new
+config etc/pure-ftpd/pureftpd-pgsql.conf.new
+config etc/pure-ftpd/pureftpd.schema.new
