0ce556677919eac99d16740c81f84ba91cb45bda
Kyle Guinn <elyk03@gmail.com>
2013-03-03 12:56:40 -0600

    system/artwiz-aleczapka: Reworked (split cursor font out)
    The cursor font is not in a separate package.
    
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- system/artwiz-aleczapka/doinst.sh
+++ system/artwiz-aleczapka/doinst.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# Update mkfontscale and mkfontdir:
-if [ -x /usr/bin/mkfontdir ]; then
-  ( cd /usr/share/fonts/misc
-    mkfontscale .
-    mkfontdir .
-  )
-fi
 # Update the X font indexes:
+if [ -x /usr/bin/mkfontdir ]; then
+  /usr/bin/mkfontscale /usr/share/fonts/misc
+  /usr/bin/mkfontdir /usr/share/fonts/misc
+fi
 if [ -x /usr/bin/fc-cache ]; then
   /usr/bin/fc-cache -f
 fi
