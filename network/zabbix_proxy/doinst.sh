a86c8a0d1b7afe02cf14969fa3571c1f04222596
Michal Bialozor <bialyy@o2.pl>
2013-03-03 01:36:19 -0600

    network/zabbix_proxy: Updated for version 1.8.16.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- network/zabbix_proxy/doinst.sh
+++ network/zabbix_proxy/doinst.sh
@@ -4,7 +4,8 @@ config() {
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
