64117deeb91046e006d9340f5fe3c06484512748
Christopher Walker <kris240376@gmail.com>
2013-05-11 23:55:16 -0500

    network/postgrey: Added (Postfix Greylisting Policy Server)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ network/postgrey/doinst.sh
@@ -0,0 +1,25 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/postfix/postgrey_whitelist_clients.new
+config etc/postfix/postgrey_whitelist_recipients.new
+preserve_perms etc/rc.d/rc.postgrey.new
+
