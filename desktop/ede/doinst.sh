671f3b1661020901c29212ffe2884bce60e24428
Andrew Tkalia <anddt@yandex.ru>
2013-06-03 12:11:34 -0500

    desktop/ede: Added (Equinox Desktop Environment)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ desktop/ede/doinst.sh
@@ -0,0 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications 2>/dev/null
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
