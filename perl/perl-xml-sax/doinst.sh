3220b5bc8f74e21b872dd590abbabcc70384cfb0
Robby Workman <rworkman@slackbuilds.org>
2013-07-09 23:04:24 -0500

    perl/perl-xml-sax: Removed (included in linuxdoc-tools)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- perl/perl-xml-sax/doinst.sh
+++
@@ -1,7 +0,0 @@
-if [ -f @PERLPATH@/XML/SAX/ParserDetails.ini ]; then
-	echo "ParserDetails.ini file already installed.";
-else
- 	perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::SAX::PurePerl))->save_parsers()"
-	echo "Creating ParserDetails.ini..."
-fi
-
