29395d3359451c81b5c55cb8cd9a5ad8387a6050
Robby Workman <rworkman@slackbuilds.org>
2013-07-13 22:42:15 -0500

    office/texlive: Change the order of the commands in doinst.sh
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- office/texlive/doinst.sh
+++ office/texlive/doinst.sh
@@ -1,8 +1,4 @@
-# This one shouldn't be needed, but just in case...
 chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
-
-# This is to generate /usr/share/texmf-var/ stuff
 chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
-chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
 chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
 
