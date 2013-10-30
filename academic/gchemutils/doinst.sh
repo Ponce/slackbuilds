d21ec588133eb46fd0b961161a1d3dc7570f4240
Petar Petrov <ppetrov@paju.oulu.fi>
2013-10-30 14:32:30 -0500

    academic/gchemutils: Updated for version 0.14.3.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- academic/gchemutils/doinst.sh
+++ academic/gchemutils/doinst.sh
@@ -1,11 +1,3 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -19,10 +11,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
-chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/ 1> /dev/null 2> /dev/null
-
-schema_install gchemutils.schemas
-schema_install gcrystal.schemas
-schema_install gchempaint.schemas
-schema_install gchempaint-arrows.schemas
