6c053ada06ab60b17ad88ed584c098124328f1c7
Robby Workman <rworkman@slackbuilds.org>
2013-11-07 14:57:29 -0600

    office/texlive: Updated for version 20130530.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- office/texlive/doinst.sh
+++ office/texlive/doinst.sh
@@ -1,4 +1,3 @@
 chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
 chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
 chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
-
