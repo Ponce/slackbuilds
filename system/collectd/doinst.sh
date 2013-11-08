1b393452df0547e7ca75ff1e3e74484a82e6072e
Matteo Bernardini <ponce@slackbuilds.org>
2013-11-08 13:48:36 +0100

    system/collectd: Updated for version 5.4.0.
    Added an rc script, fix for gcc-4.8.x,
    disabled static stuff
    
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- system/collectd/doinst.sh
+++ system/collectd/doinst.sh
@@ -11,4 +11,16 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 config etc/collectd.conf.new
+preserve_perms etc/rc.d/rc.collectd.new
