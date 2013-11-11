07704a2f9403787b59ac0c0fa52a0af0053b5a39
Robby Workman <rworkman@slackbuilds.org>
2013-11-11 09:46:31 -0600

    office/auctex: Removed (no SBo maintainer)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- office/auctex/doinst.sh
+++
@@ -1,5 +0,0 @@
-if [ -x /usr/bin/install-info ] ; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/preview-latex.info.gz 2>/dev/null
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/auctex.info.gz 2>/dev/null
-fi
-
