67494a6421be6fb297098b95dd20bc58c00f1d1b
Kyle Guinn <elyk03@gmail.com>
2013-11-15 07:33:59 +0700

    network/liferea: Updated for version 1.10.2.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/liferea/doinst.sh
+++ network/liferea/doinst.sh
@@ -1,13 +1,3 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install liferea.schemas
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -15,5 +5,11 @@ fi
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
   fi
 fi
