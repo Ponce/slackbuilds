0a7821a63932ea26418fb083cfb0e84dd446afb7
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2013-11-28 23:54:23 +0700

    system/brasero: Updated for version 2.32.1.
    The old version does not compile with totem-pl-parser version in SBo
    Thanks to slacksam for reporting via LQ
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/brasero/doinst.sh
+++ system/brasero/doinst.sh
@@ -1,13 +1,3 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install brasero.schemas
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -22,3 +12,8 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   fi
 fi
 
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
+fi
