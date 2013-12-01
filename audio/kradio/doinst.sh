f337c8f4e9f0869b2fb7ecbf4fa1892ea054e3ff
Larry Hajali <larryhaja@gmail.com>
2013-12-01 17:47:49 +0700

    audio/kradio: Updated for version 4.0.7 + New Maintainer.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- audio/kradio/doinst.sh
+++ audio/kradio/doinst.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
 fi
@@ -9,4 +7,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache /usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
