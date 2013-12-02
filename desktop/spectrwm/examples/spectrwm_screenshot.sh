4b01b571b333562408b145256e7b3948441d8ddb
LEVAI Daniel <leva@ecentrum.hu>
2013-12-02 22:34:10 +0700

    desktop/spectrwm: Updated for version 2.4.0.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/spectrwm/examples/spectrwm_screenshot.sh
+++ desktop/spectrwm/examples/spectrwm_screenshot.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 
-SCROT=$(which scrot)
-[ -x "${SCROT}" ]  ||  exit 1
+CAPTURE_TOOL=/usr/bin/import
+if [ ! -x "${CAPTURE_TOOL}" ];then
+	echo "$0: couldn't run ${CAPTURE_TOOL}" >&2
+	exit 1
+fi
 
-screenshot() {
-	case $1 in
-	full)
-		"${SCROT}" --multidisp
-		;;
-	window)
+CAPTURE_PATH="${HOME}/spectrwm_capture_$(date +%FT%T).png"
+
+case "$1" in
+	'full')
+		"${CAPTURE_TOOL}" -window root png:"${CAPTURE_PATH}"
+	;;
+	'window')
 		sleep 0.5
-		"${SCROT}" --select
-		;;
+		"${CAPTURE_TOOL}" png:"${CAPTURE_PATH}"
+	;;
 	*)
-		;;
-	esac;
-}
-
-screenshot $1
+		echo "$0: $0 <-full | -window>" >&2
+	;;
+esac
