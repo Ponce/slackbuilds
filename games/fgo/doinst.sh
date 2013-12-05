ecb92e4043e563c51c8c6a092f1732ab4453aea1
Philip Lacroix <philnx@bluebottle.com>
2013-12-05 23:08:53 +0700

    games/fgo: Added (FlightGear Flight Simulator Front End).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ games/fgo/doinst.sh
@@ -0,0 +1,29 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+CONFIG=${CONFIG:-/opt/fgo/data/config}
+for lang in de en es fr it pl ; do
+  config $CONFIG/config_${lang}.new
+done
+config $CONFIG/presets.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
