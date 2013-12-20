3f804ccb5fa8214de3f6d1007f478c13d72e596f
Benjamin Trigona-Harany <slackbuilds@jaxartes.net>
2013-12-20 23:58:47 +0700

    desktop/vim-qt: Added (Qt gui for Vim).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ desktop/vim-qt/doinst.sh
@@ -0,0 +1,7 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
