17af9762056ddc8ab2346337426b47d7ebec4ccc
Markus Reichelt <slackbuilds@mareichelt.de>
2013-12-26 21:09:07 +0100

    system/mksh: Updated for version R48b.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- system/mksh/doinst.sh
+++ system/mksh/doinst.sh
@@ -1,3 +1,15 @@
 if ! grep -q '/bin/mksh' etc/shells ; then
  echo "/bin/mksh" >> etc/shells
 fi
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+config etc/skel/.mkshrc.new
