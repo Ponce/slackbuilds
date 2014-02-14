146191edd7d66882d2602c5a69291874ef6a67e2
mancha <mancha1@hush.com>
2014-02-14 10:33:59 +0700

    libraries/libgksu: Fixed helper path on x86_64 + Compress manual.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- libraries/libgksu/doinst.sh
+++ libraries/libgksu/doinst.sh
@@ -1,7 +1,7 @@
 GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
 chroot . gconftool-2 --makefile-install-rule \
     /etc/gconf/schemas/gksu.schemas \
-    1>/dev/null 
+    1>/dev/null 2>/dev/null
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database &> /dev/null
