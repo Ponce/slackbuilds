46d36adc2adf4b9de5a6617735a1c94567e4dd5f
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2014-03-04 10:43:48 -0600

    development/meld: Updated for version 1.8.4 + New Maintainer
    Signed-off-by: Erik Hanson <erik@slackbuilds.org>

--- development/meld/doinst.sh
+++ development/meld/doinst.sh
@@ -7,3 +7,7 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
