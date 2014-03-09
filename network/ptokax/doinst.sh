3af05a73db97e5fc081f91f9aba96cb589d69236
mara <mara@fail.pp.ua>
2014-03-09 23:31:08 +0700

    network/ptokax: Added (Direct Connect Peer-To-Peer sharing network).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ network/ptokax/doinst.sh
@@ -0,0 +1,32 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.ptokax.new
+config etc/ptokax/cfg/BanList.xml.new
+config etc/ptokax/cfg/Motd.txt.new
+config etc/ptokax/cfg/Profiles.xml.new
+config etc/ptokax/cfg/RegisteredUsers.xml.new
+config etc/ptokax/cfg/ReservedNicks.xml.new
+config etc/ptokax/cfg/Scripts.xml.new
+config etc/ptokax/cfg/Settings.xml.new
