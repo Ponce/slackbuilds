9fd36e098c94ba888812f2c9a56db1463b66025b
Ken Roberts <alisonken1@juno.com>
2014-04-15 23:03:06 +0700

    business/trytond: Updated for version 3.0.4.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- business/trytond/doinst.sh
+++ business/trytond/doinst.sh
@@ -11,12 +11,16 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.openerp-server.new:
-if [ -e etc/rc.d/rc.trytond ]; then
-  cp -a etc/rc.d/rc.trytond etc/rc.d/rc.trytond.new.incoming
-  cat etc/rc.d/rc.trytond.new > etc/rc.d/rc.trytond.new.incoming
-  mv etc/rc.d/rc.trytond.new.incoming etc/rc.d/rc.trytond.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.trytond.new
-config etc/trytond/trytond.conf.new
+preserve_perms etc/rc.d/rc.trytond.new
+config etc/trytond.conf.new
