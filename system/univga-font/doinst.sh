13701fa3a6ba025dbfbcee84a9b8709d9a5abba8
B. Watson <yalhcru@gmail.com>
2014-05-28 08:40:26 +0700

    system/univga-font: Added (unicode terminal font).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/univga-font/doinst.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Update fonts.{dir,scale,alias}
+if [ -x /usr/bin/mkfontdir -a -x /usr/bin/mkfontscale ]; then
+  ( cd usr/share/fonts/misc
+    /usr/bin/mkfontscale .
+    /usr/bin/mkfontdir .
+    if ! grep -q '^univga\>' fonts.alias; then
+      echo 'univga -bolkhov-vga-medium-r-normal--16-160-75-75-c-80-iso10646-1' >> fonts.alias
+    fi
+  )
+fi
+
+# Update the X font indexes:
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
