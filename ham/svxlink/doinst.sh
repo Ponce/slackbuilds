ee8de43cf3d0056ec019046c39719a6999b64fa2
Gustavo Conrad - LU8WFY <gusconrad@gmail.com>
2014-07-24 01:13:03 +0700

    ham/svxlink: Added (voice service system).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ ham/svxlink/doinst.sh
@@ -0,0 +1,24 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/svxlink/svxlink.conf.new
+config etc/svxlink/remotetrx.conf.new
+config etc/svxlink/TclVoiceMail.conf.new
+config etc/svxlink/svxlink.d/ModuleDtmfRepeater.conf.new
+config etc/svxlink/svxlink.d/ModuleEchoLink.conf.new
+config etc/svxlink/svxlink.d/ModuleHelp.conf.new
+config etc/svxlink/svxlink.d/ModuleMetarInfo.conf.new
+config etc/svxlink/svxlink.d/ModuleParrot.conf.new
+config etc/svxlink/svxlink.d/ModulePropagationMonitor.conf.new
+config etc/svxlink/svxlink.d/ModuleSelCallEnc.conf.new
+config etc/svxlink/svxlink.d/ModuleTclVoiceMail.conf.new
