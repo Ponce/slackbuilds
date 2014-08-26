3114a6842f0ec052e496a4843853cfd31f8e9408
B. Watson <yalhcru@gmail.com>
2014-08-26 12:19:19 +0200

    games/hatari: Updated for version 1.8.0.
    Better desktop integration, script cleanups
    
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- games/hatari/doinst.sh
+++ games/hatari/doinst.sh
@@ -1,3 +1,7 @@
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -6,4 +10,9 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
+fi
+
+# if there's no tos.img symlink, take over with emutos.img
+if [ ! -r usr/share/hatari/tos.img ]; then
+  ( cd usr/share/hatari ; ln -sf emutos.img tos.img )
 fi
