0d6b9605160df988f967ecf2c5e60755db57bddf
David Spencer <baildon.research@googlemail.com>
2014-10-25 16:49:24 +0100

    gis/foxtrotgps: Updated for version 1.2.0.
    Signed-off-by: David Spencer <baildon.research@googlemail.com>

--- gis/foxtrotgps/doinst.sh
+++ gis/foxtrotgps/doinst.sh
@@ -11,3 +11,7 @@ schema_install apps_foxtrotgps.schemas
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+
+if [ -x /usr/bin/install-info ]; then
+  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/blah.gz 2> /dev/null
+fi
