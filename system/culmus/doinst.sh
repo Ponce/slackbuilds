80587f16a16eb1bed2bc377c0822aac08d69b49b
Robby Workman <rworkman@slackbuilds.org>
2014-12-07 01:12:02 -0600

    system/culmus: Fix font installation and doinst.sh paths
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- system/culmus/doinst.sh
+++ system/culmus/doinst.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-fc-cache -f
-cd /usr/share/fonts/TTF
-/usr/bin/mkfontscale
-/usr/bin/mkfontdir
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
 
-cd /usr/share/fonts/Type1
-/usr/bin/mkfontscale
-/usr/bin/mkfontdir
+# Update the X font indexes:
+if [ -x /usr/bin/mkfontdir -o -x /usr/X11R6/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
+  ( cd /usr/share/fonts/Type1
+    mkfontscale .
+    mkfontdir .
+  )
 
-( cd /etc/fonts/conf.d ; ln -sf ../conf.avail/61-culmus.conf 61-culmus.conf )
