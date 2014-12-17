acbaed2e23d9063fdc62b91ce436ed683a51af37
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2014-12-17 14:58:47 +0700

    network/suricata: Updated for version 2.0.5.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/suricata/doinst.sh
+++ network/suricata/doinst.sh
@@ -1,17 +1,14 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/suricata/classification.config.new
-config etc/suricata/reference.config.new
-config etc/suricata/suricata.yaml.new
-config etc/suricata/threshold.config.new
+CONFIGS="classification.config reference.config suricata.yaml threshold.config"
+for file in $CONFIGS; do
+    config etc/suricata/${file}.new
+done
