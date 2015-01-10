2b14f25b867c4ea071006bcc8354bf6a6bde904f
Gustavo Conrad <gusconrad@gmail.com>
2015-01-10 07:02:37 +0700

    ham/svxlink: Updated for version 14.08.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- ham/svxlink/doinst.sh
+++ ham/svxlink/doinst.sh
@@ -11,6 +11,17 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 config etc/svxlink/svxlink.conf.new
 config etc/svxlink/remotetrx.conf.new
 config etc/svxlink/TclVoiceMail.conf.new
@@ -22,3 +33,4 @@ config etc/svxlink/svxlink.d/ModuleParrot.conf.new
 config etc/svxlink/svxlink.d/ModulePropagationMonitor.conf.new
 config etc/svxlink/svxlink.d/ModuleSelCallEnc.conf.new
 config etc/svxlink/svxlink.d/ModuleTclVoiceMail.conf.new
+preserve_perms etc/rc.d/rc.svxlink.new
