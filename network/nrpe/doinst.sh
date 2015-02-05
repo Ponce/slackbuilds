845d1012a121d8dc759ddfabd8241b93c629ea0a
Mario Preksavec <mario@slackware.hr>
2015-02-05 04:57:18 +0100

    network/nrpe: Updated for version 2.15 + maintainer change

--- network/nrpe/doinst.sh
+++ network/nrpe/doinst.sh
@@ -11,13 +11,17 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.nrpe.new:
-if [ -e etc/rc.d/rc.nrpe ]; then
-  cp -a etc/rc.d/rc.nrpe etc/rc.d/rc.nrpe.new.incoming
-  cat etc/rc.d/rc.nrpe.new > etc/rc.d/rc.nrpe.new.incoming
-  mv etc/rc.d/rc.nrpe.new.incoming etc/rc.d/rc.nrpe.new
-fi
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
 
-config etc/rc.d/rc.nrpe.new
+preserve_perms etc/rc.d/rc.nrpe.new
 config etc/nagios/nrpe.cfg.new
 
