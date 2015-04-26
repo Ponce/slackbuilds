2e36ee2612aeba39608ad3ff5899049dee989492
Rudson Alves <rudsonalves@yahoo.com.br>
2015-04-26 09:05:30 +0700

    graphics/dvisvgm: Added (converts DVI to SVG).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ graphics/dvisvgm/doinst.sh
@@ -0,0 +1,9 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+schema_install blah.schemas
