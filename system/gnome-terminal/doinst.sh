d0dbe53dcae67a2dc1b89e8168070f3b5366ccc8
Thorn Inurcide <thorn-inurcide@hotmail.com>
2015-04-26 12:19:46 +0700

    system/gnome-terminal: Added (GNOME terminal).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/gnome-terminal/doinst.sh
@@ -0,0 +1,19 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
+fi
+
+if [ -x usr/bin/rarian-sk-update ]; then
+  usr/bin/rarian-sk-update 1> /dev/null 2> /dev/null
+fi
