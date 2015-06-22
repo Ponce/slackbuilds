43c725ceb0f7aed19cf038dc60aa390a696cb929
Klaatu <klaatu@member.fsf.org>
2015-06-22 18:30:24 +0700

    graphics/luxrender: Updated for version 1.4 + new maintainer.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- graphics/luxrender/doinst.sh
+++ graphics/luxrender/doinst.sh
@@ -1,3 +1,7 @@
+if [ -x /usr/bin/xdg-mime ]; then
+    /usr/bin/xdg-mime install /usr/doc/luxrender-1.4/mime-lux.xml
+fi
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
