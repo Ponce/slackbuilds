adc2bd6b95f744a4e3193e722c70620e92813a1d
Igor Drozdovsky <igor29768@gmail.com>
2015-11-11 23:20:59 +0700

    graphics/dvisvgm: Fix DOWNLOAD url + update script.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- graphics/dvisvgm/doinst.sh
+++
@@ -1,9 +0,0 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
-
-schema_install blah.schemas
