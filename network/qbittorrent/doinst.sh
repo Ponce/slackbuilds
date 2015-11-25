930330f99242481c2b07ed7d549caca9efe47dee
David Woodfall <dave@dawoodfall.net>
2015-11-25 18:12:26 +0700

    network/qbittorrent: Added qbittorrent-nox + an rc init script.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/qbittorrent/doinst.sh
+++ network/qbittorrent/doinst.sh
@@ -1,3 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/rc.d/rc.qbittorrent-nox.new
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
