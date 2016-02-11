f84cdc2c85d588fe06dabe8cfb8fe9825b733e04
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2016-02-12 01:44:04 +0700

    desktop/gnome-colors: Removed (per maintainer's request).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/gnome-colors/doinst.sh
+++
@@ -1,7 +0,0 @@
-for theme in brave carbonite colors-common dust human illustrious noble tribute wine wise ; do
-  if [ -e usr/share/icons/gnome-$theme/icon-theme.cache ]; then
-    if [ -x /usr/bin/gtk-update-icon-cache ]; then
-      /usr/bin/gtk-update-icon-cache usr/share/icons/gnome-$theme >/dev/null 2>&1
-    fi
-  fi
-done
