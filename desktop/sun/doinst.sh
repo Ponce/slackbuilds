3c1b1e5cbf50c4c796ff87087d2362a29510ec39
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2016-03-08 22:55:15 +0200

    desktop/sun: Updated for version 1.2.0.
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>

--- desktop/sun/doinst.sh
+++ desktop/sun/doinst.sh
@@ -8,18 +8,6 @@ config() {
   fi
 }
 
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
-
-preserve_perms etc/rc.d/rc.sun.new
 config etc/sun/sun.conf.new
 
 if [ -x /usr/bin/update-desktop-database ]; then
