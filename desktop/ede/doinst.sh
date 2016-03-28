9aabd52ef306ab9ff672fc28f8d9864d3090f4be
David Spencer <idlemoor@slackbuilds.org>
2016-03-28 15:48:53 +0100

    desktop/ede: Fixed doinst.sh.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- desktop/ede/doinst.sh
+++ desktop/ede/doinst.sh
@@ -1,3 +1,16 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications 2>/dev/null
 fi
