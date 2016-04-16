5c1f2de4fc19fd1b3df7a87efc2707d9051c3acc
David Spencer <baildon.research@googlemail.com>
2016-04-16 15:51:04 +0100

    network/mosaic-ck: Removed.
    Fails to build with libpng-1.6 or even libpng-1.5. No patches
    upstream. One of the other mosaic forks on Github would be a
    better replacement:
    github.com/yotann/ncsa-mosaic
    github.com/alandipert/ncsa-mosaic

--- network/mosaic-ck/doinst.sh
+++
@@ -1,9 +0,0 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
-
-# If there's no Mosaic link, take over:
-if [ ! -r usr/bin/Mosaic ]; then
-  ( cd usr/bin ; ln -sf mosaic-ck Mosaic )
-  ( cd usr/man/man1 ; ln -sf mosaic-ck.1.gz Mosaic.1.gz )
-fi
