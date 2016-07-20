066226567625557109826ca07218819ed74aaf2c
Johannes Schoepfer <slackbuilds[at]schoepfer[dot]info>
2016-07-20 05:01:34 -0500

    office/texlive-texmf-extra: Removed (replaced by texlive-extra)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- office/texlive-texmf-extra/doinst.sh
+++
@@ -1,4 +0,0 @@
-chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
-#printf "y\n" | chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
-chroot . /usr/bin/updmap-sys 1>/dev/null 2>/dev/null
-chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
