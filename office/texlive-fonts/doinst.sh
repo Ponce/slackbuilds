d288e54fa651d05e176f636f4b1092a5b5bd6c09
Johannes Schoepfer <slackbuilds[at]schoepfer[dot]info>
2016-07-20 04:58:52 -0500

    office/texlive-fonts: Added (extra fonts for TeXLive)
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

---
+++ office/texlive-fonts/doinst.sh
@@ -0,0 +1,5 @@
+[ -f usr/share/texmf-config/web2c/updmap.cfg ] && rm usr/share/texmf-config/web2c/updmap.cfg
+chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
+printf "y\n" | chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
+chroot . /usr/bin/updmap-sys 1>/dev/null 2>/dev/null
+chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
