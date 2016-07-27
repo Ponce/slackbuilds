5afe071b987c6706a2a9bf532e9d6f396cea3b95
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2016-07-27 10:31:55 +0100

    perl/mod_perl: Added (Perl Interpreter for Apache).
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ perl/mod_perl/doinst.sh
@@ -0,0 +1,5 @@
+if [ ! -r etc/httpd/extra/mod_perl.conf ]; then
+  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
+elif [ "$(cat etc/httpd/extra/mod_perl.conf 2> /dev/null)" = "" ]; then
+  cat etc/httpd/extra/mod_perl.conf.example > etc/httpd/extra/mod_perl.conf
+fi
