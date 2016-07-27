1c77e284c5a98d403179c73402ea0278cfcba85e
Giorgio Peron <giorgio.peron@gmail.com>
2016-07-27 22:23:01 +0100

    network/dokuwiki: Updated for version 20160626a.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- network/dokuwiki/doinst.sh
+++ network/dokuwiki/doinst.sh
@@ -1,4 +1,4 @@
-sed -i "s:#Include /etc/httpd/mod_php.conf:Include /etc/httpd/mod_php.conf:g" etc/httpd/httpd.conf 
+sed -i "s:#Include /etc/httpd/mod_php.conf:Include /etc/httpd/mod_php.conf:g" etc/httpd/httpd.conf
 grep -q -e 'Dokuwiki' etc/httpd/httpd.conf || cat >> etc/httpd/httpd.conf <<'EOF'
 # Dokuwiki
 Include /etc/httpd/extra/httpd-dokuwiki.conf
