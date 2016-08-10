25f95cf14706ee9a9ae54b752e3f00c74f3d7a3d
Strahil Yordanov <strahilski@yahoo.com>
2016-08-10 23:21:54 +0100

    development/openjdk8: Added (Open Implementation of JDK).
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ development/openjdk8/profile.d/openjdk8.csh
@@ -0,0 +1,4 @@
+#!/bin/csh
+setenv JAVA_HOME /usr/lib/openjdk8
+setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
+setenv PATH ${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
---
+++ development/openjdk8/profile.d/openjdk8.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export JAVA_HOME=/usr/lib/openjdk8
+export MANPATH="${MANPATH}:${JAVA_HOME}/man"
+export PATH="${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin"
