3be8811198ba45dd8f3b00f298a42cbd49f7104a
Eugene M <damagedone@gmx.com>
2016-08-24 06:42:23 +0700

    desktop/pekwm: Update script.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/pekwm/doinst.sh
+++ desktop/pekwm/doinst.sh
@@ -1,25 +1,3 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config etc/pekwm/autoproperties.new
-config etc/pekwm/autoproperties_typerules.new
-config etc/pekwm/config.new
-config etc/pekwm/config_system.new
-config etc/pekwm/keys.new
-config etc/pekwm/menu.new
-config etc/pekwm/mouse.new
-config etc/pekwm/mouse_click.new
-config etc/pekwm/mouse_sloppy.new
-config etc/pekwm/mouse_system.new
-config etc/pekwm/start.new
-config etc/pekwm/vars.new
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
