f002c4bdd38a57bb856cc03c8c721f62afd878e6
Michales Michaloudes <korgie@gmail.com>
2016-08-27 07:25:16 +0700

    desktop/florence: Added (virtual keyboard).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ desktop/florence/doinst.sh
@@ -0,0 +1,29 @@
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+## check SlackBuild for this. It's better aproach?
+schema_install org.florence.schemas
+
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
