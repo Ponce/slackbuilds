109b2f018af8e4c87e4c793c7c03dfab919fc450
Alexander Verbovetsky <alik@ejik.org>
2016-09-28 09:05:21 +0700

    network/r2e: Updated for version 2.71 + new maintainer.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/r2e/doinst.sh
+++ network/r2e/doinst.sh
@@ -1,9 +1,12 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  [ ! -r $OLD ] && mv $NEW $OLD
-  rm -f $NEW
-}
+if [ -d /etc/r2e ]; then
+  cat << EOF > /etc/r2e/README.config
+Starting version 2.71 to customize r2e please edit config.py in your
+user directory ~/.rss2email
 
-config etc/r2e/config.py.new
+File /etc/r2e/config.py is not used anymore.  The directory /etc/r2e
+can safely be removed.
 
+See /usr/doc/config.py.example for a full list of available
+configuration variables.
+EOF
+fi
