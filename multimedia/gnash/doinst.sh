15706dda080d0ab870caee55acd1d4fa09f65939
Kyle Guinn <elyk03@gmail.com>
2016-12-05 18:59:01 -0600

    multimedia/gnash: Updated build flags, HOMEPAGE, and DOWNLOAD.
    Signed-off-by: Kyle Guinn <elyk03@gmail.com>

--- multimedia/gnash/doinst.sh
+++ multimedia/gnash/doinst.sh
@@ -11,20 +11,9 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
-
-preserve_perms etc/gnashpluginrc.new
-preserve_perms etc/gnashthumbnailrc.new
-preserve_perms etc/gnashrc.new
+config etc/gnashpluginrc.new
+config etc/gnashthumbnailrc.new
+config etc/gnashrc.new
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
