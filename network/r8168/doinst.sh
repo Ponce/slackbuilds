cdbd2caec49ca242dff013b3555de73a39ca64bb
Hunter Sezen <ovariegata@yahoo.com>
2016-12-09 00:29:24 +0000

    network/r8168: Fixed doinst.sh.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- network/r8168/doinst.sh
+++ network/r8168/doinst.sh
@@ -1,3 +1,19 @@
-if [ -x /sbin/depmod ]; then
-  /sbin/depmod -a 1> /dev/null 2> /dev/null
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/modprobe.d/r8169_blacklist.conf.new
+
+# A good idea whenever kernel modules are added or changed:
+if [ -x sbin/depmod ]; then
+  chroot . /sbin/depmod -a @KERNEL@ 1> /dev/null 2> /dev/null
 fi
