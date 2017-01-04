81b95b67f8230e19858cefd378173360cedc9e31
Edinaldo P. Silva <edps.mundognu@gmail.com>
2017-01-04 17:19:47 -0600

    desktop/paper-icon-theme: Updated for version 1.4.0.
    Signed-off-by: Robby Workman <rworkman@slackbuilds.org>

--- desktop/paper-icon-theme/doinst.sh
+++ desktop/paper-icon-theme/doinst.sh
@@ -6,8 +6,15 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+if [ -e usr/share/icons/Paper/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache usr/share/icons/Paper>/dev/null 2>&1
   fi
 fi
+
+if [ -e usr/share/icons/Paper-Mono-Dark/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/Paper-Mono-Dark >/dev/null 2>&1
+  fi
+fi
+
