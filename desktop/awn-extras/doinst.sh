7fe95c027278218f35b07c4b70f3e655309ed8ed
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:44:36 +0700

    desktop/awn-extras: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/awn-extras/doinst.sh
+++ desktop/awn-extras/doinst.sh
@@ -38,6 +38,6 @@ schema_install awn-applet-dialect.schemas
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
