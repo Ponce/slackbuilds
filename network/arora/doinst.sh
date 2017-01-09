e14a4463381311b866f918899fb8d6b2cd8422b6
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:49:01 +0700

    network/arora: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/arora/doinst.sh
+++ network/arora/doinst.sh
@@ -3,7 +3,7 @@ if [ -x /usr/bin/update-desktop-database ]; then
 fi
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
 
