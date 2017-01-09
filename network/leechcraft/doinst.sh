3d89e8811d00d09c85edf08e2f9b203ebbd6b9fe
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:49:19 +0700

    network/leechcraft: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/leechcraft/doinst.sh
+++ network/leechcraft/doinst.sh
@@ -4,19 +4,19 @@ fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
 
 if [ -e usr/share/icons/Pevzi/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/Pevzi >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/Pevzi >/dev/null 2>&1
   fi
 fi
 
 if [ -e usr/share/icons/oxygen/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/oxygen >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/oxygen >/dev/null 2>&1
   fi
 fi
 
