bacfbca02da292799342929f8ad687b68f7de932
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:48:21 +0700

    misc/krename: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- misc/krename/doinst.sh
+++ misc/krename/doinst.sh
@@ -4,13 +4,13 @@ fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
 
 if [ -e usr/share/icons/locolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/locolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/locolor >/dev/null 2>&1
   fi
 fi
 
