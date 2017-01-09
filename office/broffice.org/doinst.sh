bcdf22735e3defedd47b68823c1a45158e1380c7
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:49:48 +0700

    office/broffice.org: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- office/broffice.org/doinst.sh
+++ office/broffice.org/doinst.sh
@@ -9,7 +9,7 @@ fi
 if [ -x /usr/bin/gtk-update-icon-cache ]; then 
   for theme in gnome locolor hicolor ; do
     if [ -e usr/share/icons/$theme/icon-theme.cache ]; then 
-      /usr/bin/gtk-update-icon-cache usr/share/icons/$theme >/dev/null 2>&1
+      /usr/bin/gtk-update-icon-cache -f usr/share/icons/$theme >/dev/null 2>&1
     fi
   done
 fi
