4ea1e8200925201e06b40fe3d2287280c4ee2af2
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2017-01-09 19:44:29 +0700

    business/skrooge: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- business/skrooge/doinst.sh
+++ business/skrooge/doinst.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -e usr/share/icons/oxygen/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache usr/share/icons/oxygen >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/oxygen >/dev/null 2>&1
   fi
 fi
 
