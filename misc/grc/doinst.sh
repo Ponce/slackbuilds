c2b699753b0b54302791a822751d6c62cf0febed
Edinaldo P. Silva <edps.mundognu@gmail.com>
2017-01-28 23:08:35 +0000

    misc/grc: Updated for version 1.10.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ misc/grc/doinst.sh
@@ -0,0 +1,17 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/grc.conf.new
+config etc/grc.bashrc.new
+config etc/grc.zsh.new
+config etc/grc.fish.new
