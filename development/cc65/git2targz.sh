6e64e0a6576815d705d82c729ee3660d46ae3a67
B. Watson <yalhcru@gmail.com>
2017-01-30 00:08:19 -0500

    development/cc65: Updated for version 2.15_20170126.

---
+++ development/cc65/git2targz.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Create source tarball from cc65 git repo, with generated version
+# number. We don't want to include the whole git history in the tarball,
+# but we do want to build the git hash into the binary (for --version),
+# so there's a bit of extra stuff here.
+
+# Note that this script doesn't need to be run as root. It does
+# need to be able to write to the current directory it's run from.
+
+PRGNAM=cc65
+CLONE_URL=https://github.com/$PRGNAM/$PRGNAM.git
+
+set -e
+
+GITDIR=$( mktemp -dt cc65.git.XXXXXX )
+rm -rf $GITDIR
+git clone $CLONE_URL $GITDIR
+
+CWD="$( pwd )"
+cd $GITDIR
+GIT_SHA=$( git rev-parse --short HEAD )
+sed -i "1iGIT_SHA=$GIT_SHA" src/Makefile
+
+# 6878ede and earlier commits are missing a \ in src/Makefile, which
+# causes the git hash *not* to be part of --version output. Fix, if
+# needed.
+sed -i '/-DLD65_LIB[^\\]*$/s,$, \\,' src/Makefile
+
+DATE=$( git log --date=format:%Y%m%d --format=%cd | head -1 )
+
+VERFILE=src/common/version.c
+MAJOR=$( sed -n 's,#define\s\+VER_MAJOR\s\+\([0-9]\+\)U.*,\1,p' $VERFILE )
+MINOR=$( sed -n 's,#define\s\+VER_MINOR\s\+\([0-9]\+\)U.*,\1,p' $VERFILE )
+
+VERSION=${MAJOR}.${MINOR}_$DATE
+
+rm -rf .git
+find . -name .gitignore -print0 | xargs -0 rm -f
+
+cd "$CWD"
+rm -rf $PRGNAM-$VERSION $PRGNAM-$VERSION.tar.xz
+mv $GITDIR $PRGNAM-$VERSION
+tar cvfJ $PRGNAM-$VERSION.tar.xz $PRGNAM-$VERSION
