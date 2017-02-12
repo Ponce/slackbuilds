a3dfdd867b1983993674aeb57dc5802ff47ca9b3
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-02-12 18:25:56 +0530

    system/openrc-services: Updated for version 20170217.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/openrc-services/doinst.sh
+++ system/openrc-services/doinst.sh
@@ -23,15 +23,16 @@ preserve_perms() {
   config $NEW
 }
 
+openrc_accessibility=(espeakup)
 openrc_base=(device-mapper dmcrypt lvm mdadm mdraid udev udev-settle udev-trigger)
 openrc_desktop=(acpid alsasound xdm gpm rfcomm wpa_supplicant)
-openrc_devel=(git-daemon mysqld postgresql svn distccd)
+openrc_devel=(git-daemon mysqld postgresql svn distccd redis)
 openrc_misc=(bitlbee cpupower connman ntpd ntp-client sntp rsyslog saned metalog pulseaudio syslog-ng sensord lircd irexec haveged salt-master salt-minion salt-syncdic hdparm clamd boinc atd libvirtd)
 openrc_net=(named dhcpd dhcrelay dhcrelay6 dnsmasq iptables ip6tables rpcbind nfs nfsclient openntpd slapd sshd openvpn quota rsyncd samba saslauthd squid transmission-daemon ufw vnstatd xinetd ypbind ypserv tor NetworkManager httpd syncthing)
 openrc_slack=(dcron sendmail snmpd snmptrapd sysklogd)
 openrc_video=(atieventsd vgl bumblebee)
 
-for file in "${openrc_base[@]}" "${openrc_desktop[@]}" "${openrc_devel[@]}" "${openrc_misc[@]}" "${openrc_net[@]}" "${openrc_slack[@]}" "${openrc_video[@]}"; do
+for file in "${openrc_accessibility[@]}" "${openrc_base[@]}" "${openrc_desktop[@]}" "${openrc_devel[@]}" "${openrc_misc[@]}" "${openrc_net[@]}" "${openrc_slack[@]}" "${openrc_video[@]}"; do
   config "etc/conf.d/${file}.new"
 done
 
