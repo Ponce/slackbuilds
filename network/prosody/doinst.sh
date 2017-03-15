789698d85456c6407c7c524b48d7815d9e29d1e5
Gerardo Zamudio <gerardo.zamudio@linux.com>
2017-03-15 22:44:46 +0000

    network/prosody: Updated for version 0.9.12.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- network/prosody/doinst.sh
+++ network/prosody/doinst.sh
@@ -8,17 +8,6 @@ config() {
   fi
 }
 
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
-
 config etc/prosody/prosody.cfg.lua.new
 config etc/prosody/migrator.cfg.lua.new
 config etc/prosody/certs/openssl.cnf.new
@@ -30,4 +19,8 @@ config etc/prosody/certs/localhost.cnf.new
 config etc/prosody/certs/example.com.cnf.new
 config etc/prosody/certs/localhost.crt.new
 
-preserve_perms etc/rc.d/rc.prosody.new
+if ! [ -L etc/rc.d/rc.prosody ] ;
+then
+  rm -f etc/rc.d/rc.prosody
+  ln -s /usr/bin/prosodyctl etc/rc.d/rc.prosody
+fi
