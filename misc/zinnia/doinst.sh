b512d1bd18947f6ccc6069085d81a24014cb37bf
B. Watson <yalhcru@gmail.com>
2017-03-21 01:02:34 -0400

    misc/zinnia: Remove unused doinst.sh, i486=>i586.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- misc/zinnia/doinst.sh
+++
@@ -1,4 +0,0 @@
-( cd usr/lib ; rm -rf libzinnia.so )
-( cd usr/lib ; ln -sf libzinnia.so.0.0.0 libzinnia.so )
-( cd usr/lib ; rm -rf libzinnia.so.0 )
-( cd usr/lib ; ln -sf libzinnia.so.0.0.0 libzinnia.so.0 )
