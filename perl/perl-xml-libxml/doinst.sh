b06d91d57893b945d6d4cb4296fe6206708f72db
B. Watson <yalhcru@gmail.com>
2017-03-21 21:33:15 -0400

    perl/perl-xml-libxml: Install doinst.sh.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- perl/perl-xml-libxml/doinst.sh
+++ perl/perl-xml-libxml/doinst.sh
@@ -1,6 +1,5 @@
 # Update SAX parsers
-chroot .  
-/usr/bin/perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::LibXML::SAX::Parser))->save_parsers()" 1>/dev/null
-/usr/bin/perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::LibXML::SAX))->save_parsers()" 1>/dev/null
-
-
+chroot . \
+  /usr/bin/perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::LibXML::SAX::Parser))->save_parsers()" 1>/dev/null
+chroot . \
+  /usr/bin/perl -MXML::SAX -e "XML::SAX->add_parser(q(XML::LibXML::SAX))->save_parsers()" 1>/dev/null
