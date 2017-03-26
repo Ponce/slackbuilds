df14bdf0c9dbd8a58e3ab6c00902a7eaf26b4cc8
B. Watson <yalhcru@gmail.com>
2017-03-26 08:02:44 -0400

    system/dina-font: New maintainer, rewrite script.
    Signed-off-by: B. Watson <yalhcru@gmail.com>

--- system/dina-font/doinst.sh
+++ system/dina-font/doinst.sh
@@ -1,10 +1,15 @@
-# Update the X font indexes:
 if [ -x /usr/bin/mkfontdir ]; then
-  ( cd /usr/share/fonts/misc
+  ( cd usr/share/fonts/misc
+    if ! grep -q ^Dina_ fonts.alias 2>/dev/null; then
+      echo 'Dina_6 -windows-dina-medium-r-normal--8-60-96-96-c-60-microsoft-cp1252' >> fonts.alias
+      echo 'Dina_8 -windows-dina-medium-r-normal--10-80-96-96-c-70-microsoft-cp1252' >> fonts.alias
+      echo 'Dina_9 -windows-dina-medium-r-normal--12-90-96-96-c-70-microsoft-cp1252' >> fonts.alias
+      echo 'Dina_10 -windows-dina-medium-r-normal--13-100-96-96-c-80-microsoft-cp1252' >> fonts.alias
+    fi
     mkfontscale .
     mkfontdir .
   )
 fi
-if [ -x /usr/bin/fc-cache ]; then
-  /usr/bin/fc-cache -f
+if [ -x usr/bin/fc-cache ]; then
+  usr/bin/fc-cache -f
 fi
