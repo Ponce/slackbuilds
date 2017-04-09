6a6631c09a0d37ca263d0615a1be0af48ca085c1
Andrew Clemons <andrew.clemons@gmail.com>
2017-04-09 15:19:37 +0100

    development/cargo-vendor: Updated for version 0.1.6.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- development/cargo-vendor/cargo-vendor-mkvendortarball.sh
+++ development/cargo-vendor/cargo-vendor-mkvendortarball.sh
@@ -25,7 +25,7 @@
 set -e
 
 PRGNAM=cargo-vendor
-VERSION=${VERSION:-0.1.5}
+VERSION=${VERSION:-0.1.6}
 
 rm -rf "$PRGNAM-$VERSION" "$PRGNAM-$VERSION-vendor.tar.gz"
 tar xvf "$PRGNAM-$VERSION.tar.gz"
