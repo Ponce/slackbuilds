1b3d3b91fb9420538eee1b022f90b535c9f146f0
Matteo Bernardini <ponce@slackbuilds.org>
2017-04-15 17:49:35 +0200

    git/doit.sh: Removed script added by mistake.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- doit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for FILE in `cat ~/FOOME.txt`; do \
-  sed -i "s/Ryan P\.C\. McQuen/orphaned - no maintainer/" "$FILE"
-  sed -i "s/ryanpcmcquen@member\.fsf\.org/nobody@nowhere/" "$FILE"
-  git add -A :/
-  git commit -sm "$(echo $FILE | cut -d'/' -f1-2): Orphan build."
-done
-
