fb1a9494e94dd510c971fa6f6c4fc3fd1a568ee5
Andrew Clemons <andrew.clemons@gmail.com>
2017-05-04 11:07:07 +0700

    development/cargo-vendor: Updated for version 0.1.8.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/cargo-vendor/cargo-vendor-mkvendortarball.sh
+++ development/cargo-vendor/cargo-vendor-mkvendortarball.sh
@@ -25,7 +25,7 @@
 set -e
 
 PRGNAM=cargo-vendor
-VERSION=${VERSION:-0.1.7}
+VERSION=${VERSION:-0.1.8}
 
 rm -rf "$PRGNAM-$VERSION" "$PRGNAM-$VERSION-vendor.tar.gz"
 tar xvf "$PRGNAM-$VERSION.tar.gz"
