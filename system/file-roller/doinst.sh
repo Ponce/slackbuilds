2893a524e3ff5c32d9b7eb688d89c8ae913fd11d
Frank Endres <frank.endres@sud-ouest.org>
2017-05-11 22:20:29 +0700

    system/file-roller: Updated for version 3.24.1 + new maintainer.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/file-roller/doinst.sh
+++ system/file-roller/doinst.sh
@@ -12,3 +12,6 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
+if [ -x /usr/bin/glib-compile-schemas ]; then
+	/usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas/ 2>/dev/null
+fi
