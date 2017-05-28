97bdf24d58cdd4e3883257a327c63f8c1909876a
Johannes Schoepfer <slackbuilds[at]schoepfer[dot]info>
2017-05-28 20:33:33 +0100

    network/radicale: Updated for version 1.1.3.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- network/radicale/doinst.sh
+++ network/radicale/doinst.sh
@@ -1,26 +1,27 @@
 config() {
-NEW="$1"
-OLD="$(dirname $NEW)/$(basename $NEW .new)"
-# If there's no config file by that name, mv it over:
-if [ ! -r $OLD ]; then
-	mv $NEW $OLD
-elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
-	rm $NEW
-fi
-# Otherwise, we leave the .new copy for the admin to consider...
-}
-preserve_perms () {
-NEW="$1"
-OLD="$(dirname $NEW)/$(basename $NEW .new)"
-if [ -e $OLD ]; then
-	cp -a $OLD ${NEW}.incoming
-	cat $NEW > ${NEW}.incoming
-	mv ${NEW}.incoming $NEW
-fi
-config $NEW
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-for i in etc/radicale/config.new etc/radicale/logging.new 
-do
-	preserve_perms $i
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+for i in etc/radicale/config.new etc/radicale/logging.new; do
+  preserve_perms $i
 done
