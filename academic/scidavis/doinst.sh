a01dc1fb22d2e8ceee0c2f60c6661657e39905c1
Fellype do Nascimento <fellype(at)gmail.com>
2017-06-02 01:19:59 +0100

    academic/scidavis: Updated for version 1.17.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ academic/scidavis/doinst.sh
@@ -0,0 +1,19 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -e usr/share/icons/locolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/locolor >/dev/null 2>&1
+  fi
+fi
