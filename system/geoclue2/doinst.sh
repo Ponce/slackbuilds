dd53f116b6304b978f6cdfbc1cd2c18446212b5e
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-14 18:09:49 +0100

    system/geoclue2: Added (location information service).
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ system/geoclue2/doinst.sh
@@ -0,0 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/geoclue/geoclue.conf.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
