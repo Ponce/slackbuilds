5cfdaa94ea6aee8456d376357f985441609fdfed
B. Watson <yalhcru@gmail.com>
2017-07-03 21:34:49 -0400

    development/cc65: Updated for version 2.16_20170630.
    Signed-off-by: B. Watson <yalhcru@gmail.com>

--- development/cc65/git2targz.sh
+++ development/cc65/git2targz.sh
@@ -38,6 +38,16 @@ VERSION=${MAJOR}.${MINOR}_$DATE
 rm -rf .git
 find . -name .gitignore -print0 | xargs -0 rm -f
 
+# DIRTY HACK ALERT:
+# -current's linuxdoc-tools hates upstream's sgml docs, and it's not
+# obvious what's wrong (bug/regression in linuxdoc-tools? 14.2's worked
+# fine). I'm not interested in trying to fix the problem because I
+# fucking hate XML, and I especially hate the mess that's the Slackware
+# linuxdoc-tools (28 source tarballs, interdependent). So I'll just
+# include pre-generated (on 14.2) HTML docs in my self-hosted source
+# tarball.
+make -C doc html
+
 cd "$CWD"
 rm -rf $PRGNAM-$VERSION $PRGNAM-$VERSION.tar.xz
 mv $GITDIR $PRGNAM-$VERSION
