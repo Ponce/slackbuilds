1a1d42064d30135422cc8f88180f0adbfc3c57ba
Andrew Clemons <andrew.clemons@gmail.com>
2017-08-09 23:34:06 +0100

    system/fwupd: Added (update firmware).
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ system/fwupd/doinst.sh
@@ -0,0 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config etc/fwupd.conf.new
+config etc/fwupd/remotes.d/fwupd.conf.new
+config etc/fwupd/remotes.d/lvfs-testing.conf.new
+config etc/fwupd/remotes.d/lvfs.conf.new
+config etc/fwupd/remotes.d/vendor.conf.new
+config etc/pki/fwupd-metadata/GPG-KEY-Linux-Vendor-Firmware-Service.new
+config etc/pki/fwupd/GPG-KEY-Hughski-Limited.new
+config etc/pki/fwupd/GPG-KEY-Linux-Vendor-Firmware-Service.new
