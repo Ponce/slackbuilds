7fdca74bdec2550428e2d180f9d8ce0cef0a1285
Heinz Wiesinger <pprkut@slackbuilds.org>
2017-08-30 21:38:19 +0200

    misc/ibus: Updated for version 1.5.16.
    Signed-off-by: Heinz Wiesinger <pprkut@slackbuilds.org>

--- misc/ibus/doinst.sh
+++ misc/ibus/doinst.sh
@@ -1,53 +1,20 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
-
-preserve_perms etc/profile.d/ibus.sh.new
-preserve_perms etc/profile.d/ibus.csh.new
-
-schema_install() {
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule "$1" 1>/dev/null
-}
-
-SCHEMA_FILE="etc/gconf/schemas/ibus.schemas"
-if [ -r "$SCHEMA_FILE" ]; then
-  schema_install "$SCHEMA_FILE"
-fi
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x usr/bin/update-gtk-immodules ]; then
+  chroot . /usr/bin/update-gtk-immodules --verbose 1>/dev/null
+fi
+
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
 fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
-fi
-
-# Run gtk-query-immodules so that "ibus" will appear under Input Method
-# when you right- click your mouse in a text box.
-if [ -x usr/bin/update-gtk-immodules ]; then
-  chroot . /usr/bin/update-gtk-immodules --verbose 1>/dev/null
 fi
 
