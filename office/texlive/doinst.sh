5c64d166b67d6ae0fa8d4587a35776d32d8f89db
Johannes Schoepfer <slackbuilds[at]schoepfer[dot]info>
2017-11-13 20:17:25 +0000

    office/texlive: Updated for version 2017.171108.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- office/texlive/doinst.sh
+++ office/texlive/doinst.sh
@@ -1,3 +1,4 @@
 chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
-printf "y\n" | chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
+printf "y\n" | chroot . /usr/bin/updmap-sys --syncwithtrees 1>/dev/null 2>/dev/null
 chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
+chroot . /usr/bin/mtxrun --generate 1>/dev/null 2>/dev/null
