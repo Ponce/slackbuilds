63dd2549911f0db4b12bbf86e2e9eef27d8813c5
Johannes Schoepfer <slackbuilds[at]schoepfer[dot]info>
2017-11-15 08:45:36 +0000

    office/texlive-extra: Updated for version 2017.171108.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- office/texlive-extra/doinst.sh
+++ office/texlive-extra/doinst.sh
@@ -1,4 +1,3 @@
-rm -f usr/share/texmf-config/web2c/updmap.cfg
 chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
-printf "y\n" | chroot . /usr/bin/updmap-sys --nohash --syncwithtrees 1>/dev/null 2>/dev/null
+printf "y\n" | chroot . /usr/bin/updmap-sys --syncwithtrees 1>/dev/null 2>/dev/null
 chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
