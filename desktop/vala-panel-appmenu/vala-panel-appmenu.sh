c61a88fde797d3fd9257595fea46324052c91ef3
Igor Krasylivskyi <tranzistor_2000 [at] ukr [dot] net>
2017-12-22 13:44:46 +0700

    desktop/vala-panel-appmenu: Added (Global Menu for Vala Panel).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ desktop/vala-panel-appmenu/vala-panel-appmenu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -n "$GTK_MODULES" ]; then
+    GTK_MODULES="${GTK_MODULES}:appmenu-gtk-module"
+else
+    GTK_MODULES="appmenu-gtk-module"
+fi
+
+if [ -z "$UBUNTU_MENUPROXY" ]; then
+    UBUNTU_MENUPROXY=1
+fi
+
+export GTK_MODULES
+export UBUNTU_MENUPROXY
