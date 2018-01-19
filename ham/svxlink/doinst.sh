877f40f72a361d923be6204a9bc390faff3825f5
Gustavo Conrad <gus3963@gmail.com>
2018-01-19 18:53:33 +0700

    ham/svxlink: Updated for version 17.12.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- ham/svxlink/doinst.sh
+++ ham/svxlink/doinst.sh
@@ -23,14 +23,17 @@ preserve_perms() {
 }
 
 config etc/svxlink/svxlink.conf.new
-config etc/svxlink/remotetrx.conf.new
 config etc/svxlink/TclVoiceMail.conf.new
-config etc/svxlink/svxlink.d/ModuleDtmfRepeater.conf.new
-config etc/svxlink/svxlink.d/ModuleEchoLink.conf.new
+config etc/svxlink/gpio.conf.new
+config etc/svxlink/remotetrx.conf.new
+config etc/svxlink/svxreflector.conf.new
+config etc/svxlink/svxlink.d/ModuleFrn.conf.new
 config etc/svxlink/svxlink.d/ModuleHelp.conf.new
+config etc/svxlink/svxlink.d/ModuleEchoLink.conf.new
+config etc/svxlink/svxlink.d/ModuleDtmfRepeater.conf.new
+config etc/svxlink/svxlink.d/ModuleTclVoiceMail.conf.new
+config etc/svxlink/svxlink.d/ModuleSelCallEnc.conf.new
+config etc/svxlink/svxlink.d/ModulePropagationMonitor.conf.new
 config etc/svxlink/svxlink.d/ModuleMetarInfo.conf.new
 config etc/svxlink/svxlink.d/ModuleParrot.conf.new
-config etc/svxlink/svxlink.d/ModulePropagationMonitor.conf.new
-config etc/svxlink/svxlink.d/ModuleSelCallEnc.conf.new
-config etc/svxlink/svxlink.d/ModuleTclVoiceMail.conf.new
 preserve_perms etc/rc.d/rc.svxlink.new
