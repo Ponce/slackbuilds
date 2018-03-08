451ae8400d2e5e365da89a52c166f86f80fb3a61
Mario Preksavec <mario@slackware.hr>
2018-03-08 10:55:49 +0100

    system/xen: Updated for version 4.10.0.
    Signed-off-by: Mario Preksavec <mario@slackware.hr>

--- system/xen/dom0/kernel-xen.sh
+++ system/xen/dom0/kernel-xen.sh
@@ -5,8 +5,8 @@
 # Written by Chris Abela <chris.abela@maltats.com>, 20100515
 # Modified by Mario Preksavec <mario@slackware.hr>
 
-KERNEL=${KERNEL:-4.4.88}
-XEN=${XEN:-4.9.0}
+KERNEL=${KERNEL:-4.4.118}
+XEN=${XEN:-4.10.0}
 BOOTLOADER=${BOOTLOADER:-lilo}
 
 ROOTMOD=${ROOTMOD:-ext4}
