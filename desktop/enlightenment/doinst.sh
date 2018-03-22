e841309ac64fa5352342cbecf896f6b4599476bb
Erik Falor <ewfalor@gmail.com>
2018-03-22 11:09:13 +0000

    desktop/enlightenment: Updated for version 0.22.2.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- desktop/enlightenment/doinst.sh
+++ desktop/enlightenment/doinst.sh
@@ -11,3 +11,11 @@ config() {
 config etc/enlightenment/sysactions.conf.new
 config etc/xdg/menus/e-applications.menu.new
 config etc/X11/xinit/xinitrc.enlightenment.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
