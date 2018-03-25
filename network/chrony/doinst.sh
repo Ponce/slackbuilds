34ddf9962a566af57ce230b4264750794f80f1b0
Richard Ellis <rellis@dp100.com>
2018-03-25 23:36:50 +0000

    network/chrony: Updated for version 3.2.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ network/chrony/doinst.sh
@@ -0,0 +1,23 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/chrony.conf.new
+preserve_perms etc/rc.d/rc.chrony.new
