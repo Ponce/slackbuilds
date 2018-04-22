2061b4112fdffd0b02f82b75e092141f55d5c570
Vladimir MyRequiem <mrvladislavovich@gmail.com>
2018-04-22 15:26:30 +0100

    system/spman: Added (Slackware package manager).
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

---
+++ system/spman/doinst.sh
@@ -0,0 +1,15 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+CONFIGS="blacklist repo-list spman.conf"
+
+for file in $CONFIGS; do
+    config etc/spman/${file}.new
+done
