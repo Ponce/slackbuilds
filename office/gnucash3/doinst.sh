ea2703dffdb00167bf5cdccdd4bad81a0689113e
Lenard Spencer <lspencer31@cfl.rr.com>
2018-05-03 23:44:44 +0700

    office/gnucash3: Updated for version 3.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- office/gnucash3/doinst.sh
+++ office/gnucash3/doinst.sh
@@ -12,9 +12,13 @@ config() {
 }
 
 config etc/gnucash/environment.new
-
+c
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
