17c7f340b1dc1d2da087076bc3acf7cc9bd9c6d4
Edinaldo P. Silva <edps.mundognu@gmail.com>
2018-05-25 20:54:53 +0100

    network/freerapid: Updated for version 0.9u4.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- network/freerapid/frd.sh
+++ network/freerapid/frd.sh
@@ -2,25 +2,25 @@
 
 # default option.
 if [ -x "`which java`" ]; then
-  cd /usr/share/freerapid-0.9.4/ && exec java -jar frd.jar "$@"
+  cd /usr/share/freerapid/ && exec java -jar frd.jar "$@"
 fi
 
 # less CPU usage.
 #if [ -x "`which java`" ]; then
-#  cd /usr/share/freerapid-0.9.4/ && exec java -Dsun.java2d.opengl=true -jar frd.jar "$@"
+#  cd /usr/share/freerapid/ && exec java -Dsun.java2d.opengl=true -jar frd.jar "$@"
 #fi
 
 # needed for some distros.
 #if [ -x "`which java`" ]; then
-#  cd /usr/share/freerapid-0.9.4/ && exec java -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
+#  cd /usr/share/freerapid/ && exec java -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
 #fi
 
 # better fonts appearance (smooth fonts).
 #if [ -x "`which java`" ]; then
-#  cd /usr/share/freerapid-0.9.4/ && exec java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
+#  cd /usr/share/freerapid/ && exec java -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
 #fi
 
 # all options combined.
 #if [ -x "`which java`" ]; then
-#  cd /usr/share/freerapid-0.9.4/ && exec java -Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
+#  cd /usr/share/freerapid/ && exec java -Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Djava.net.preferIPv4Stack=true -Xmx160m -jar frd.jar "$@"
 #fi
