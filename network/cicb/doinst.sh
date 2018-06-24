e27dc891e21a90e8a1587d8a3312b5d6316e9ee2
B. Watson <yalhcru@gmail.com>
2018-06-23 21:22:38 -0400

    network/cicb: Updated for version 5.2.0b1+git20170501.
    Signed-off-by: B. Watson <yalhcru@gmail.com>

---
+++ network/cicb/doinst.sh
@@ -0,0 +1,12 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config usr/share/cicb/system.icbrc.new
+config usr/share/cicb/icbserverdb.new
