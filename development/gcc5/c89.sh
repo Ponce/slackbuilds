c6e8f60eea992ccceda17e0f0b6264a6e3af2d8a
Matteo Bernardini <ponce@slackbuilds.org>
2018-06-27 10:12:08 +0200

    20180627.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

---
+++ development/gcc5/c89.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+fl="-std=c89"
+CC=${CC:-"gcc"}
+for opt; do
+  case "$opt" in
+    -ansi|-std=c89|-std=iso9899:1990) fl="";;
+    -std=*) echo "`basename $0` called with non ANSI/ISO C option $opt" >&2
+	    exit 1;;
+  esac
+done
+exec $CC $fl ${1+"$@"}
