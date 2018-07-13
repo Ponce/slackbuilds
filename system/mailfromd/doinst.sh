c26c9c5f7dd19ed2f92aa1ee06cfd461d31d77dc
Sergey Poznyakoff <gray@gnu.org>
2018-07-13 21:23:01 +0700

    system/mailfromd: Added (A General-Purpose Mail Filter).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/mailfromd/doinst.sh
@@ -0,0 +1,15 @@
+config() {
+    NEW="$1"
+    OLD=${NEW%%.new}
+    # If there's no config file by that name, mv it over:
+    if [ ! -r $OLD ]; then
+	mv $NEW $OLD
+    elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then
+	# toss the redundant copy
+        rm $NEW
+    fi
+    # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/mailfromd/mailfromd.mf.new
+config etc/rc.d/rc.mailfromd.new
