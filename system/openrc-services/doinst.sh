d5f6361657e44935644d4f5b1b12712187bcd689
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2018-09-09 11:48:54 +0530

    system/openrc-services: Updated for version 20180909.

--- system/openrc-services/doinst.sh
+++ system/openrc-services/doinst.sh
@@ -25,12 +25,12 @@ preserve_perms() {
 
 openrc_accessibility=(espeakup)
 openrc_base=(device-mapper dmcrypt lvm mdadm mdraid udev udev-settle udev-trigger)
-openrc_desktop=(acpid alsasound xdm gpm rfcomm wpa_supplicant)
-openrc_devel=(git-daemon mysqld postgresql svn distccd redis)
-openrc_misc=(bitlbee cpupower connman influxdb ntpd ntp-client sntp rsyslog saned metalog pulseaudio syslog-ng sensord lircd irexec haveged salt-master salt-minion salt-syncdic hdparm clamd boinc atd libvirtd)
-openrc_net=(named dhcpd dhcrelay dhcrelay6 dnsmasq iptables ip6tables rpcbind nfs nfsclient nginx openntpd slapd sshd openvpn quota rsyncd samba saslauthd squid transmission-daemon ufw vnstatd xinetd ypbind ypserv tor NetworkManager httpd syncthing)
+openrc_desktop=(acpid alsasound xdm gpm wpa_supplicant)
+openrc_devel=(git-daemon influxdb mysqld postgresql svn distccd redis)
+openrc_misc=(bitlbee cpupower connman ntpd ntp-client sntp rsyslog saned metalog pulseaudio syslog-ng sensord fancontrol lm_sensors lircd irexec haveged salt-master salt-minion salt-syncdic hdparm clamd boinc atd libvirtd)
+openrc_net=(named dhcpd dhcrelay dhcrelay6 dnsmasq iptables ip6tables rpcbind nfs nfsclient nginx openntpd slapd sshd openvpn quota rsyncd samba saslauthd squid transmission-daemon ufw vnstatd xinetd tor NetworkManager httpd syncthing)
 openrc_slack=(dcron sendmail snmpd snmptrapd sysklogd)
-openrc_video=(atieventsd vgl bumblebee)
+openrc_video=(vgl bumblebee)
 
 for file in "${openrc_accessibility[@]}" "${openrc_base[@]}" "${openrc_desktop[@]}" "${openrc_devel[@]}" "${openrc_misc[@]}" "${openrc_net[@]}" "${openrc_slack[@]}" "${openrc_video[@]}"; do
   config "etc/conf.d/${file}.new"
