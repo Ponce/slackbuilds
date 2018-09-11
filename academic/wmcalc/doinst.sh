fb4f25ce90c82c70dbcb03356b69deb7808e247e
B. Watson <yalhcru@gmail.com>
2018-09-11 15:46:31 -0400

    academic/wmcalc: Updated for version 0.7.
    Signed-off-by: B. Watson <yalhcru@gmail.com>

--- academic/wmcalc/doinst.sh
+++ academic/wmcalc/doinst.sh
@@ -1,3 +1,8 @@
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
 
 config() {
   NEW="$1"
