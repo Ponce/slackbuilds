8bb39ade035a0e1a085c424265914b42d4482bc7
Donald Cooley <chytraeu@sdf.org>
2018-11-24 02:03:20 +0000

    office/openoffice.org: Updated for version 4.1.6.
    Signed-off-by: David Spencer <idlemoor@slackbuilds.org>

--- office/openoffice.org/doinst.sh
+++ office/openoffice.org/doinst.sh
@@ -6,9 +6,9 @@ if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
-if [ -x /usr/bin/gtk-update-icon-cache ]; then 
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
   for theme in gnome locolor hicolor ; do
-    if [ -e usr/share/icons/$theme/icon-theme.cache ]; then 
+    if [ -e usr/share/icons/$theme/icon-theme.cache ]; then
       /usr/bin/gtk-update-icon-cache -f usr/share/icons/$theme >/dev/null 2>&1
     fi
   done
