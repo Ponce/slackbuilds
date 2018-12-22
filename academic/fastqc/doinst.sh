c438969e424f0e830c6bb2d58a93b3dc8d4672dd
Rob van Nues <sborg63@disroot.org>
2018-12-22 00:03:06 -0900

    academic/fastqc: Updated for version 0.11.8 + new maintainer.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- academic/fastqc/doinst.sh
+++ academic/fastqc/doinst.sh
@@ -1,3 +1,8 @@
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+
+( cd usr/bin ; rm -rf fastqc )
+( cd usr/bin ; ln -sf ../share/java/fastqc/fastqc fastqc )
+( cd usr/doc/fastqc-0.11.8 ; rm -rf Help )
+( cd usr/doc/fastqc-0.11.8 ; ln -sf ../../share/java/fastqc/Help Help )
