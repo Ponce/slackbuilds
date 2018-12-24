4002feeb7996e619913dc7e51568e51d20539d15
Corrado Franco <conraid (at) linux (dot) it>
2018-12-25 05:27:40 +0700

    network/neomutt: Added (Text-based Mail Client).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ network/neomutt/doinst.sh
@@ -0,0 +1,16 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+  mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+  rm $NEW
+  fi
+}
+
+config etc/neomutt/neomuttrc.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications > /dev/null 2>&1
+fi
+
