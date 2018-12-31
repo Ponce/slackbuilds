77b368a1dab5ba6ad46add7f54fc867f4d87957a
Sukma Wardana <sukma.wardana@mail.com>
2019-01-01 00:12:33 +0700

    development/netbeans: Updated for version 10.0.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/netbeans/doinst.sh
+++ development/netbeans/doinst.sh
@@ -12,7 +12,16 @@ if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
 config opt/netbeans/etc/netbeans.conf.new
 config opt/netbeans/etc/netbeans.clusters.new
 config opt/netbeans/etc/netbeans.import.new
-
