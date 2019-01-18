57addd2869ebec1fb6817689f1622ec4e2711994
Sukma Wardana <sukma.wardana@mail.com>
2019-01-19 04:25:54 +0700

    development/jbake: Added (Static Blog Generator).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/jbake/profile.d/jbake.csh
@@ -0,0 +1,3 @@
+#!/bin/csh
+setenv JBAKE_HOME /usr/share/jbake
+setenv PATH ${PATH}:${JBAKE_HOME}/bin
---
+++ development/jbake/profile.d/jbake.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export JBAKE_HOME=/usr/share/jbake
+export PATH="${PATH}:${JBAKE_HOME}/bin"
