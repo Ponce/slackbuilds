df85a3fa57c90ad5dfc8ee8869f9cc7351586a45
Christoph Willing <chris.willing@linux.com>
2019-06-02 21:25:56 +1000

    games/steam: Updated for version 1.0.0.61. New maintainer
    Signed-off-by: Christoph Willing <chris.willing@linux.com>

--- games/steam/doinst.sh
+++ games/steam/doinst.sh
@@ -1,3 +1,21 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-    /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+# Update the desktop database:
+if [ -x usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database usr/share/applications > /dev/null 2>&1
+fi
+
+# Update hicolor theme cache:
+if [ -d usr/share/icons/hicolor ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f -t usr/share/icons/hicolor 1> /dev/null 2> /dev/null
+  fi
+fi
+
+# Update the mime database:
+if [ -x usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+# Reload the udev rules to include our newly installed steam rules
+if [ -x /sbin/udevadm ]; then
+  /sbin/udevadm control --reload
 fi
