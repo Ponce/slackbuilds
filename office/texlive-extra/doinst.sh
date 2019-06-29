05eefea0784239b74dd5b9cb94c4127ccd26aefa
Johannes Schoepfer <slackbuilds@schoepfer.info>
2019-06-29 07:49:21 +0700

    office/texlive-extra: Updated for version 2019.190626.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- office/texlive-extra/doinst.sh
+++ office/texlive-extra/doinst.sh
@@ -1,3 +1,5 @@
 chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
 printf "y\n" | chroot . /usr/bin/updmap-sys --syncwithtrees 1>/dev/null 2>/dev/null
-chroot . /usr/bin/updmap-sys 1>/dev/null 2>/dev/null
+cp usr/share/texmf-dist/web2c/updmap.cfg.extra usr/share/texmf-dist/web2c/updmap.cfg.extra.tmp
+chroot . /usr/bin/updmap-sys --cnffile /usr/share/texmf-dist/web2c/updmap.cfg --cnffile /usr/share/texmf-dist/web2c/updmap.cfg.extra 1>/dev/null 2>/dev/null
+mv usr/share/texmf-dist/web2c/updmap.cfg.extra.tmp usr/share/texmf-dist/web2c/updmap.cfg.extra
