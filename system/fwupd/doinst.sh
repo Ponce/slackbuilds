df68478129934ac3132e680507ffa75c919d4295
Andrew Clemons <andrew.clemons@gmail.com>
2019-07-24 20:19:12 +1200

    system/fwupd: Updated for version 1.2.10.
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>

--- system/fwupd/doinst.sh
+++ system/fwupd/doinst.sh
@@ -8,18 +8,18 @@ config() {
   fi
 }
 
-config etc/fwupd/daemon.conf.new
-if [ -e etc/fwupd/uefi.conf ] ; then
-  config etc/fwupd/uefi.conf.new
-fi
-config etc/fwupd/remotes.d/fwupd.conf.new
-config etc/fwupd/remotes.d/fwupd-tests.conf.new
-config etc/fwupd/remotes.d/lvfs-testing.conf.new
-config etc/fwupd/remotes.d/lvfs.conf.new
-config etc/fwupd/remotes.d/vendor.conf.new
-
-config etc/pki/fwupd-metadata/GPG-KEY-Linux-Vendor-Firmware-Service.new
-config etc/pki/fwupd-metadata/LVFS-CA.pem.new
 config etc/pki/fwupd/GPG-KEY-Hughski-Limited.new
+config etc/pki/fwupd/GPG-KEY-Linux-Foundation-Firmware.new
 config etc/pki/fwupd/GPG-KEY-Linux-Vendor-Firmware-Service.new
 config etc/pki/fwupd/LVFS-CA.pem.new
+config etc/pki/fwupd-metadata/GPG-KEY-Linux-Foundation-Metadata.new
+config etc/pki/fwupd-metadata/GPG-KEY-Linux-Vendor-Firmware-Service.new
+config etc/pki/fwupd-metadata/LVFS-CA.pem.new
+config etc/fwupd/remotes.d/lvfs.conf.new
+config etc/fwupd/remotes.d/lvfs-testing.conf.new
+config etc/fwupd/remotes.d/vendor.conf.new
+config etc/fwupd/remotes.d/vendor-directory.conf.new
+config etc/fwupd/remotes.d/fwupd-tests.conf.new
+config etc/fwupd/daemon.conf.new
+config etc/fwupd/redfish.conf.new
+config etc/fwupd/uefi.conf.new
