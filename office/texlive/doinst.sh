6e2e8a576adb1511e142ed8c7f89067cd25ec3b8
Matteo Bernardini <ponce@slackbuilds.org>
2019-08-10 07:48:16 +0200

    20190810.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- office/texlive/doinst.sh
+++
@@ -1,5 +0,0 @@
-chroot . /usr/bin/mktexlsr 1>/dev/null 2>/dev/null
-printf "y\n" | chroot . /usr/bin/updmap-sys --syncwithtrees 1>/dev/null 2>/dev/null
-chroot . /usr/bin/updmap-sys 1>/dev/null 2>/dev/null
-chroot . /usr/bin/fmtutil-sys --all 1>/dev/null 2>/dev/null
-chroot . /usr/bin/mtxrun --generate 1>/dev/null 2>/dev/null
