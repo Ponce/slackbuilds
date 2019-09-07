5b6cec84dd08a6a1eb03f38e632d4995c6a35402
Matteo Bernardini <ponce@slackbuilds.org>
2019-09-07 18:19:29 +0200

    20190907.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- desktop/thunar-dropbox/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
-  fi
-fi
-
