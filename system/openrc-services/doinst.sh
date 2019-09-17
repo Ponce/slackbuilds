955e89a25283997c2bbb650d3843cc643edd6017
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2019-09-17 10:48:54 +0530

    system/openrc-services: Updated for version 20190917.

--- system/openrc-services/doinst.sh
+++ system/openrc-services/doinst.sh
@@ -23,22 +23,12 @@ preserve_perms() {
   config $NEW
 }
 
-openrc_accessibility=(espeakup)
-openrc_base=(device-mapper dmcrypt lvm mdadm mdraid udev udev-settle udev-trigger)
-openrc_desktop=(acpid alsasound xdm gpm wpa_supplicant)
-openrc_devel=(git-daemon influxdb mysqld postgresql svn distccd jenkins redis)
-openrc_misc=(bitlbee cpupower connman ntpd ntp-client sntp rsyslog saned metalog pulseaudio syslog-ng sensord fancontrol lm_sensors lircd irexec haveged salt-master salt-minion salt-syncdic hdparm clamd boinc atd libvirtd docker)
-openrc_net=(named dhcpd dhcrelay dhcrelay6 hostapd dnsmasq iptables ip6tables rpcbind nfs nfsclient nginx openntpd slapd sshd openvpn quota rsyncd samba saslauthd squid transmission-daemon ufw vnstatd xinetd tor NetworkManager httpd syncthing tftpd-hpa)
-openrc_slack=(dcron sendmail snmpd snmptrapd sysklogd fail2ban)
-openrc_video=(vgl bumblebee nvidia-persistenced)
-
-for file in "${openrc_accessibility[@]}" "${openrc_base[@]}" "${openrc_desktop[@]}" "${openrc_devel[@]}" "${openrc_misc[@]}" "${openrc_net[@]}" "${openrc_slack[@]}" "${openrc_video[@]}"; do
-  config "etc/openrc/conf.d/${file}.new"
+for file in etc/openrc/conf.d/*.new; do
+  config "${file}"
 done
 
-BACKUP_LOCAL=(rcM.start)
-for file in "${BACKUP_LOCAL[@]}"; do
-  preserve_perms "etc/openrc/local.d/${file}.new"
+for file in etc/openrc/local.d/*.new; do
+  preserve_perms "${file}"
 done
 
 # disable udev-postmount
