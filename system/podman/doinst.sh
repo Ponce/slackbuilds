901b4b2f8d539b1a828fa9238c88d3a589065a94
Vincent Batts <vbatts@hashbangbash.com>
2019-10-20 09:19:54 -0400

    system/podman: update to 1.6.2
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

---
+++ system/podman/doinst.sh
@@ -0,0 +1,18 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/containers/libpod.conf.new
+config etc/containers/registries.conf.new
+config etc/containers/mounts.conf.new
+config etc/containers/seccomp.json.new
+config etc/containers/policy.json.new
