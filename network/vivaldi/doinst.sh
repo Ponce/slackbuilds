3066c3735e298e53471350c9e25fcaea471cdfc2
Alexander Verbovetsky <alik@ejik.org>
2019-11-01 06:16:39 +0700

    network/vivaldi: Updated for version 2.9.1705.31.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/vivaldi/doinst.sh
+++ network/vivaldi/doinst.sh
@@ -7,3 +7,6 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+rm -f /opt/vivaldi/libffmpeg.so
+rm -f /opt/vivaldi/libwidevinecdm.so
