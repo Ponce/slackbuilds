ae5521051b4c262766a7add4008f95ead3649d27
Andrzej Telszewski <atelszewski@gmail.com>
2019-11-11 17:31:40 +0700

    development/STM32CubeIDE: Added (C/C++ development platform).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/STM32CubeIDE/doinst.sh
@@ -0,0 +1,28 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+config etc/udev/rules.d/49-stlinkv1.rules.new
+config etc/udev/rules.d/49-stlinkv2-1.rules.new
+config etc/udev/rules.d/49-stlinkv2.rules.new
+config etc/udev/rules.d/49-stlinkv3.rules.new
