7f63d6dc7baabc58e34acafc7341c0255c3dae18
Corrado Franco <conraid (at) linux (dot) it>
2019-12-09 11:28:51 +0700

    network/neomutt: Updated for version 20191207.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/neomutt/doinst.sh
+++ network/neomutt/doinst.sh
@@ -14,3 +14,9 @@ if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications > /dev/null 2>&1
 fi
 
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -t -f -q usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
