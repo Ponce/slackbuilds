681d03cb4ce4a6a4fce44889605d378451c865a0
Matteo Bernardini <ponce@slackbuilds.org>
2020-02-08 11:12:44 +0100

    20200208.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- network/midori/doinst.sh
+++ network/midori/doinst.sh
@@ -1,19 +1,3 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config etc/xdg/midori/search.new
-config etc/xdg/midori/extensions/adblock/config.new
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
