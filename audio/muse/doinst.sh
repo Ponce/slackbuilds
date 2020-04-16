1809d53b31e84cd6a5d3d495588bd415d5406d87
B. Watson <yalhcru@gmail.com>
2020-04-16 01:55:11 -0400

    audio/muse: Updated for version 3.1.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- audio/muse/doinst.sh
+++ audio/muse/doinst.sh
@@ -1,5 +1,3 @@
-setcap cap_ipc_lock,cap_sys_nice=ep /usr/bin/muse3 2>/dev/null
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
