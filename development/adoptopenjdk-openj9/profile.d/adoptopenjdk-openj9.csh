69f9aad8aceaaa990e22b8f2db1d3161ce3b9258
Sukma Wardana <sukma.wardana@mail.com>
2020-05-03 01:14:48 +0700

    development/adoptopenjdk-openj9: Added (Open Implementation of JDK).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/adoptopenjdk-openj9/profile.d/adoptopenjdk-openj9.csh
@@ -0,0 +1,4 @@
+#!/bin/csh
+setenv JAVA_HOME /usr/local/lib/jdk-@VERSION@
+setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
+setenv PATH ${PATH}:${JAVA_HOME}/bin
---
+++ development/adoptopenjdk-openj9/profile.d/adoptopenjdk-openj9.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export JAVA_HOME=/usr/local/lib/jdk-@VERSION@
+export MANPATH="${MANPATH}:${JAVA_HOME}/man"
+export PATH="${PATH}:${JAVA_HOME}/bin"
