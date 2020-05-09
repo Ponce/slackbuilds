8e29685ddaeb618d0e1e99ea6a6b0e1041c746ae
Gustavo Conrad <gus3963@gmail.com>
2020-05-09 18:37:35 +0700

    ham/ax25-apps: Added (AX.25 Packet radio support software).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ ham/ax25-apps/doinst.sh
@@ -0,0 +1,16 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/ax25/ax25ipd.conf.new
+config etc/ax25/ax25mond.conf.new
+config etc/ax25/ax25rtd.conf.new
