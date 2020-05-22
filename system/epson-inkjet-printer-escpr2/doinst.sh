492bf89a6e045dafaefd547be5ac8bb05cedc125
Tim Dickson <dickson.tim@googlemail.com>
2020-05-22 15:44:51 +0100

    system/epson-inkjet-printer-escpr2: Updated for version 1.1.12.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/epson-inkjet-printer-escpr2/doinst.sh
@@ -0,0 +1,6 @@
+echo
+echo "********************* NOTICE **********************"
+echo "Cups needs restarting for new drivers to be usable:"
+echo "/etc/rc.d/rc.cups restart"
+echo "***************************************************"
+echo
