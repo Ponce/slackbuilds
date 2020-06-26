e8104c1aab221cee84308b60a58bfbf65cd0c56e
B. Watson <yalhcru@gmail.com>
2020-06-26 14:56:49 +0100

    system/filerunner: Added (two-pane graphical file manager)
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/filerunner/doinst.sh
@@ -0,0 +1,14 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+# If there's no fr link, take over:
+if [ ! -r usr/bin/fr ]; then
+  ( cd usr/bin ; ln -sf filerunner fr )
+fi
