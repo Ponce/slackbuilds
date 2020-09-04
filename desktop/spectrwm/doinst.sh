4f78942d46b2136e743576645ade2181c0668b3b
Andrew Payne <phalange@komputermatrix.com>
2020-09-04 20:35:23 +0700

    desktop/spectrwm: Updated for version 3.4.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/spectrwm/doinst.sh
+++ desktop/spectrwm/doinst.sh
@@ -1,5 +1,19 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/X11/xinit/xinitrc.spectrwm.new
+config etc/spectrwm.conf.new
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
-
-[ -x /sbin/ldconfig ]  &&  /sbin/ldconfig 2>/dev/null
