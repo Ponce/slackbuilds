8adbacc0058d79d26ddf417d277a5756f5cb74a7
Tim Dickson <dickson.tim@googlemail.com>
2020-10-01 19:47:40 +0100

    system/epson-printer-utility: Added (epson printer monitor+tools)
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/epson-printer-utility/doinst.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#fix rc file on install, and register .desktop file
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/rc.d/rc.ecbd.new
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e /usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/kbuildsycoca4 ]; then
+  /usr/bin/kbuildsycoca4 >/dev/null 2>&1
+fi
+
+echo
+echo "********************* NOTICE *********************************"
+echo "ecbd needs to be running for the printer monitor to be usable:"
+echo "/etc/rc.d/rc.ecbd restart"
+echo "**************************************************************"
+echo
