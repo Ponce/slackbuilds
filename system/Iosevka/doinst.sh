ce962243ef29f480a8293365b3d4acafdabeb30d
Eugene M <damagedone@gmx.com>
2020-10-02 00:07:26 +0100

    system/Iosevka: Updated for version 3.6.1.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/Iosevka/doinst.sh
+++ system/Iosevka/doinst.sh
@@ -1,10 +1,10 @@
 # Update the X font indexes:
+
 if [ -x /usr/bin/mkfontdir ]; then
   ( cd /usr/share/fonts/TTF
     mkfontscale .
     mkfontdir .
   )
-fi
-if [ -x /usr/bin/fc-cache ]; then
-  /usr/bin/fc-cache -f
+
+  [ -x /usr/bin/fc-cache ] && /usr/bin/fc-cache -f
 fi
