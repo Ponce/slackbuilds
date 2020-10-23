4aac6f8b6688056cb1b43fb87b553469c0bc2208
Igor Alexandrov <igor.alexandrov@outlook.com>
2020-10-23 17:38:00 +0100

    development/eclipse-platform: Added (Eclipse IDE)
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/eclipse-platform/doinst.sh
@@ -0,0 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+config opt/eclipse-platform/configuration/config.ini.new
+config opt/eclipse-platform/eclipse.ini.new
