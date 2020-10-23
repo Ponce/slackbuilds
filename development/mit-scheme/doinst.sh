a2ac1ab84cf75e0e219a546fc30a1c288a0d350c
Jason Graham <jgraham@compukix.net>
2020-10-23 18:22:11 +0100

    development/mit-scheme: Updated for version 10.1.11.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/mit-scheme/doinst.sh
+++ development/mit-scheme/doinst.sh
@@ -1,3 +1,7 @@
-if [ -x /usr/bin/install-info ]; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/blah.gz 2> /dev/null
-fi
+info_install() {
+  INFO="$1"
+  if [ -x /usr/bin/install-info ]; then
+    chroot . /usr/bin/install-info --info-dir=/usr/info $INFO 2> /dev/null
+  fi
+}
+
