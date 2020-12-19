78086331e962f981b1be65177beab08a3b99852b
Matteo Bernardini <ponce@slackbuilds.org>
2020-12-19 10:03:37 +0100

    20201219.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- audio/pithos/doinst.sh
+++ audio/pithos/doinst.sh
@@ -8,3 +8,9 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   fi
 fi
 
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
+fi
+
