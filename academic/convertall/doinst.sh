bc11bcbf65f31f81fd67a5d88c3775f25d1ff2bc
John Berger <irgunii ampersand gmail . com>
2021-02-19 22:02:12 +0000

    academic/convertall: Updated for version 0.8.0.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- academic/convertall/doinst.sh
+++ academic/convertall/doinst.sh
@@ -9,6 +9,6 @@ fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
