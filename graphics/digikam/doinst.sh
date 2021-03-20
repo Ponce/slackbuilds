fa0050a884dbbdc100c32fe39e7cb5a847219ba3
Matteo Bernardini <ponce@slackbuilds.org>
2021-03-20 18:39:24 +0100

    20210320.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- graphics/digikam/doinst.sh
+++
@@ -1,9 +0,0 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications/kde4 >/dev/null 2>&1
-fi
-
-if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
-  fi
-fi
