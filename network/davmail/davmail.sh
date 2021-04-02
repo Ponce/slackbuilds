5e635b5de19ce525d5bde07f9d129f1a2ebc5d69
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2021-04-02 11:18:39 +0700

    network/davmail: Updated for version 5.5.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/davmail/davmail.sh
+++ network/davmail/davmail.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
 BASE=/usr/share/davmail
-for i in $BASE/lib/*; do export CLASSPATH=$CLASSPATH:$i; done
-exec java -Xmx512M -Dsun.net.inetaddr.ttl=60 -cp $BASE/davmail.jar:$CLASSPATH davmail.DavGateway $1
+
+if [ -n "$JAVA_HOME" ]; then
+  JAVA_CMD="$JAVA_HOME/bin/java"
+else
+  JAVA_CMD="$(which java)"
+fi
+
+for i in $BASE/lib/*; do
+  export CLASSPATH=$CLASSPATH:$i;
+done
+
+exec $JAVA_CMD -Xmx512M -Dsun.net.inetaddr.ttl=60 -cp $BASE/davmail.jar:$CLASSPATH davmail.DavGateway $1
