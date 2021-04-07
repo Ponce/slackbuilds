fc3c9447da6cded0b6741cf3e6c2386c16493440
Matteo Bernardini <ponce@slackbuilds.org>
2021-04-07 19:43:09 +0200

    20210407.1 global branch merge.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- accessibility/speech-dispatcher/doinst.sh
+++
@@ -1,9 +0,0 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    rm $NEW
-  fi
-}
