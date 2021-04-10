58616c52fbccab8e70c89cb4c9363029729023a8
Reza Talebi <reza.talebi.73@outlook.com>
2021-04-10 03:59:11 +0100

    network/anydesk: Updated for version 6.1.0.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/anydesk/doinst.sh
+++ network/anydesk/doinst.sh
@@ -1,3 +1,27 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
@@ -11,3 +35,5 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+preserve_perms etc/rc.d/rc.anydesk.new
