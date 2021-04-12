9533303d8ee58e0ff267f68c0932b6b63b0fe452
Kyle Guinn <elyk03@gmail.com>
2021-04-11 19:52:14 -0500

    multimedia/gnash: Updated for version 0.8.11dev_20190130.
    Signed-off-by: Kyle Guinn <elyk03@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- multimedia/gnash/doinst.sh
+++ multimedia/gnash/doinst.sh
@@ -11,9 +11,18 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
 config etc/gnashpluginrc.new
 config etc/gnashthumbnailrc.new
 config etc/gnashrc.new
+schema_install gnash.schemas
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
