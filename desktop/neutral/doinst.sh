1ea5b5653664ef96604b4b7acdf6682f11fef989
Robby Workman <rworkman@slackbuilds.org>
2021-04-21 21:10:45 -0500

    desktop/neutral: Removed (no maintainer)

--- desktop/neutral/doinst.sh
+++
@@ -1,6 +0,0 @@
-if [ -e usr/share/icons/neutral/icon-theme.cache ]; then
-  if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f usr/share/icons/neutral >/dev/null 2>&1
-  fi
-fi
-
