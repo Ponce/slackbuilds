84f266cc789a1fae74c10daf84a5798c25d9cab7
Gustavo B. Schenkel <gustavo.schenkel@gmail.com>
2021-08-19 02:39:41 -0300

    development/zulu-openjdk9: Removed since Azul stopped support it
    Signed-off-by: Gustavo B. Schenkel <gustavo.schenkel@gmail.com>
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/zulu-openjdk9/zulu-openjdk9.csh
+++
@@ -1,4 +0,0 @@
-#!/bin/csh
-setenv JAVA_HOME /usr/lib/zulu-openjdk9
-setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
-setenv PATH ${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
--- development/zulu-openjdk9/zulu-openjdk9.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=/usr/lib/zulu-openjdk9
-export MANPATH="${MANPATH}:${JAVA_HOME}/man"
-export PATH="${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin"
