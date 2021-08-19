1e6834720224221d30643cb6e2db904000ddc4bb
B. Watson <yalhcru@gmail.com>
2021-08-19 06:18:11 -0400

    system/vice: Updated for version 3.5.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/vice/doinst.sh
+++ system/vice/doinst.sh
@@ -1,15 +1,11 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+if [ -x /usr/bin/xdg-desktop-menu ]; then
+  /usr/bin/xdg-desktop-menu install \
+    usr/share/desktop-directories/vice-org-vice-org.directory \
+    usr/share/applications/vice-org-*.desktop
 fi
 
-if [ -x /usr/bin/mkfontdir ]; then
-  ( cd /usr/share/fonts/TTF
-    mkfontscale .
-    mkfontdir .
-  )
-fi
-if [ -x /usr/bin/fc-cache ]; then
-  /usr/bin/fc-cache -f
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
 chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/vice.info.gz
