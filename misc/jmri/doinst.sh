39430e28bf54005fd2cf2934b18264e78520871d
Andrew Clemons <andrew.clemons@gmail.com>
2021-08-22 21:28:47 +1200

    misc/jmri: Remove desktop file handling.
    There are no desktop files in this package.
    
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- misc/jmri/doinst.sh
+++ misc/jmri/doinst.sh
@@ -3,7 +3,3 @@ if [ ! -e var/log/jmri/messages.log ]; then
 else
   rm -f var/log/jmri/messages.log.new
 fi
-
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
-fi
