b170e5379baea4adb1096a5c8e032437940cc517
Heinz Wiesinger <pprkut@slackbuilds.org>
2022-01-30 17:51:18 +0100

    system/virtualbox-addons: Updated for version 6.1.32
    Signed-off-by: Heinz Wiesinger <pprkut@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/virtualbox-addons/plasma_disable_kscreen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-kwriteconfig5 --file kded5rc --group "Module-kscreen" --key autoload --type bool false
---
+++ system/virtualbox-addons/plasma_enable_kscreen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+kwriteconfig5 --file kded5rc --group "Module-kscreen" --key autoload --type bool true
