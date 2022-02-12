f045aa7c03f3b649b2a6b163bcee0b196e472b88
B. Watson <yalhcru@gmail.com>
2022-02-11 21:50:07 -0500

    system/clamsmtp: Fix 15.0 build.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/clamsmtp/doinst.sh
+++ system/clamsmtp/doinst.sh
@@ -1,17 +1,14 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-# Keep same perms on rc.heimdal:
+# Keep same perms on rc.clamsmtpd:
 if [ -e etc/rc.d/rc.clamsmtpd ]; then
   cp -a etc/rc.d/rc.clamsmtpd etc/rc.d/rc.clamsmtpd.new.incoming
   cat etc/rc.d/rc.clamsmtpd.new > etc/rc.d/rc.clamsmtpd.new.incoming
