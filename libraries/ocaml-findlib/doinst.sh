d7b1f822e73692f28be77975bcf7113836468df1
Matteo Bernardini <ponce@slackbuilds.org>
2022-02-24 22:20:59 +0100

    libraries/ocaml-findlib: Updated for version 1.9.1.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- libraries/ocaml-findlib/doinst.sh
+++ libraries/ocaml-findlib/doinst.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
 config() {
-  old="$1"
-  new="$old.new"
-  if [ ! -r $old ]; then
-    mv $new $old
-  elif [ "$(cat $old | md5sum)" = "$(cat $new | md5sum)" ]; then
-    rm $new
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
   fi
 }
-config etc/findlib.conf
+config etc/findlib.conf.new
 
 destdir=$(chroot . /usr/bin/ocamlfind printconf destdir)/stublibs
 ldconf=$(chroot . /usr/bin/ocamlfind printconf ldconf)
