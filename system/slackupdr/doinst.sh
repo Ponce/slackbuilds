bbc078dd5d88e346fb40413bcc266e20543332d0
Stu Miller <slackbuilds@go4it2day.com>
2022-04-06 09:13:48 +0700

    system/slackupdr: Added (slackpkg automation).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/slackupdr/doinst.sh
@@ -0,0 +1,16 @@
+# doinst.sh for slackupdr
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/slackpkg/slackupdr.conf.new
