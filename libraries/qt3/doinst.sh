a250a2c70a4d32225edb805229a01d3bb50587ef
B. Watson <yalhcru@gmail.com>
2022-04-19 21:09:15 -0400

    libraries/qt3: Fix docs, doinst, symlink.
    Signed-off-by: B. Watson <yalhcru@gmail.com>

--- libraries/qt3/doinst.sh
+++ libraries/qt3/doinst.sh
@@ -1,9 +1,6 @@
-## This was causing problems compiling KDE for unknown reasons.
-## So, we'll symlink the libraries into /opt/kde3 again...
 # Add QT library directory to /etc/ld.so.conf:
-if ! grep /opt/kde3/lib etc/ld.so.conf 1> /dev/null 2> /dev/null ; then
-  echo "/opt/kde3/lib" >> etc/ld.so.conf
-fi
-if [ -x /sbin/ldconfig ]; then
-  /sbin/ldconfig 2> /dev/null
+if [ -e etc/ld.so.conf ]; then
+  if ! grep /opt/kde3/lib etc/ld.so.conf 1> /dev/null 2> /dev/null ; then
+    echo "/opt/kde3/lib" >> etc/ld.so.conf
+  fi
 fi
