4ad297db9b83db88b966ea442d6afc419392bc66
Juan M. Lasca <juanmlasca@gmail.com>
2022-04-29 09:42:43 +0100

    development/gedit-plugins: Updated for version 42.0. New maintainer.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/gedit-plugins/doinst.sh
+++ development/gedit-plugins/doinst.sh
@@ -1,11 +1,7 @@
-schema_install() {
-  SCHEMA="$1"
-  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
-  chroot . gconftool-2 --makefile-install-rule \
-    /etc/gconf/schemas/$SCHEMA \
-    1>/dev/null
-}
 
-schema_install gedit-show-tabbar-plugin.schemas
-schema_install gedit-drawspaces.schemas
+if [ -e usr/share/glib-2.0/schemas ]; then
+  if [ -x /usr/bin/glib-compile-schemas ]; then
+    /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
+  fi
+fi
 
