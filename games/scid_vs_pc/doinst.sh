52b6caae486b4495880158d6ec661d7de4842ef3
B. Watson <yalhcru@gmail.com>
2022-05-02 02:47:49 -0400

    games/scid_vs_pc: Fix fonts, docs.
    Signed-off-by: B. Watson <yalhcru@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/scid_vs_pc/doinst.sh
+++ games/scid_vs_pc/doinst.sh
@@ -7,3 +7,7 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
