1c53f1629e3e8a5fd45a074bf839cdf7faa4c0b6
Ruari Oedegaard <sbo@ruari.com>
2022-05-20 23:52:53 +0100

    network/vivaldi: Updated for version 5.2.2623.48.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/vivaldi/doinst.sh
+++ network/vivaldi/doinst.sh
@@ -7,10 +7,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
-# Adding a suitable libffmpeg.so with support for H.264 & AAC directly into
-# the package makes redistribution of the output package difficult.
-# The following is used in postinstall for the official rpm and debs.
-# (Cleanup on uninstall will be handled by douninst.sh)
-nohup /opt/vivaldi/update-ffmpeg > /dev/null 2>&1 &
-
--- network/vivaldi/douninst.sh
+++ network/vivaldi/douninst.sh
@@ -1,9 +1,11 @@
-# Remove any lib fetched by update-ffmpeg, when doinst.sh was first run
-if ls /var/opt/vivaldi/media-codecs-*/libffmpeg.so >/dev/null 2>&1; then
-  rm /var/opt/vivaldi/media-codecs-*/libffmpeg.so
-fi
+# Remove any lib fetched by '/opt/vivaldi/update-ffmpeg' that was
+# installed system-wide into '/var/opt'
+rm -f /var/opt/vivaldi/media-codecs-*/libffmpeg.so
 if [ -d /var/opt/vivaldi ]; then
   # This removes directory trees that are empty or only populated by other
   # empty directories.
   find /var/opt/vivaldi -depth -type d -empty -exec rmdir {} \;
+  # '/var/opt' is not part of the default Slackware install, so we will
+  # remove it, if it is now empty (following the above).
+  rmdir --ignore-fail-on-non-empty /var/opt
 fi
