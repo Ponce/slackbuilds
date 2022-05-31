329feec04c0871ea6135132a55217c545e5a4454
Andrew Clemons <andrew.clemons@gmail.com>
2022-06-01 08:40:48 +1200

    network/zoom-linux: Silence warnings in doinst.sh.
    The file will not be found if we are installing with a different
    ROOT. Additionally, we do not use xdg-mime install in any other
    builds, relying instead solely on update-mime-database.
    
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/zoom-linux/doinst.sh
+++ network/zoom-linux/doinst.sh
@@ -1,7 +1,3 @@
-if [ -x /usr/bin/xdg-mime ]; then
-    /usr/bin/xdg-mime install /usr/share/mime/packages/zoom-linux.xml
-fi
-
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
