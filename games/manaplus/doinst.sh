ee2a63c509438c9b1b10b5256e6167cdc2a4ec45
slackjeff <slackjeff@riseup.net>
2022-07-01 18:33:07 +0100

    games/manaplus: Updated for version 2.1.3.17. New email.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/manaplus/doinst.sh
+++ games/manaplus/doinst.sh
@@ -1,3 +1,12 @@
 if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+    /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+fi
+
+# Fix sbopkglint
+#--- usr/share/icons should not contain files with executable permission:
+#-rwxr-xr-x 1 root root 8075 Jun 24 14:01 usr/share/icons/hicolor/scalable/apps/manaplus.svg
+chmod 744 $CWD/usr/share/icons/hicolor/scalable/apps/manaplus.svg
