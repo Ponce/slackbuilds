eebb40ac3947a5a39f6fa99fa38be614b35a5cdd
Alexander Verbovetsky <alik@ejik.org>
2022-07-15 20:16:16 +0100

    office/mtp2-fonts: Download URL changed, douninst.sh added.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- office/mtp2-fonts/douninst.sh
+++ office/mtp2-fonts/douninst.sh
@@ -1,3 +1,3 @@
 chroot . /usr/bin/mktexlsr >/dev/null 2>&1
-chroot . /usr/bin/updmap-sys --force --enable Map=mtpro2.map >/dev/null 2>&1
+chroot . /usr/bin/updmap-sys --force --disable mtpro2.map >/dev/null 2>&1
 chroot . /usr/bin/mktexlsr >/dev/null 2>&1
