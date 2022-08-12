6b65bd42d32fb0610bd79251822a7f0fa77cc86e
Bob Funk <bobfunk11@gmail.com>
2022-08-12 11:41:47 +0100

    desktop/gdm: Updated for version 42.0.
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/gdm/doinst.sh
+++ desktop/gdm/doinst.sh
@@ -1,3 +1,6 @@
+# Set the wayland-session script executable:
+( cd etc/gdm ; chmod 0755 wayland-session )
+
 if [ -e usr/share/glib-2.0/schemas ]; then
   if [ -x /usr/bin/glib-compile-schemas ]; then
     /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
