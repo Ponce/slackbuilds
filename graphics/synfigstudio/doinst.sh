b0265f592f0fbd6a4985a6d3f48cd2af3c09119a
Zhu Qun-Ying <zhu.qunying@gmail.com>
2022-08-21 00:08:05 +0100

    graphics/synfigstudio: Added (Professional vector animation program)
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ graphics/synfigstudio/doinst.sh
@@ -0,0 +1,3 @@
+/usr/bin/update-mime-database -n /usr/share/mime 1> /dev/null 2> /dev/null
+/usr/bin/gtk-update-icon-cache /usr/share/icons  1> /dev/null 2> /dev/null
+/usr/bin/update-desktop-database  1> /dev/null 2> /dev/null
