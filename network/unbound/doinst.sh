3a4f6aafbfb5ac842d45f753803bd423c62b2b5d
Badchay <badchay@protonmail.com>
2022-08-31 03:14:49 +0100

    network/unbound: Updated for version 1.16.2. New maintainer.
    Old maintainer is AWOL.
    
    Signed-off-by: Dave Woodfall <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/unbound/doinst.sh
+++ network/unbound/doinst.sh
@@ -24,3 +24,21 @@ preserve_perms() {
 
 preserve_perms etc/rc.d/rc.unbound.new
 config etc/unbound/unbound.conf.new
+
+# MD5SUM d837bf4c42abb7048c90d720a579f829 is a file hash from the previous initscript.
+
+if [ $(md5sum /etc/rc.d/rc.unbound | cut -f 1 -d " ") == "d837bf4c42abb7048c90d720a579f829" ]
+then
+  echo ""
+  echo "Warning! Red Hat style init script detected at /etc/rc.d/rc.unbound !"
+  echo "It's likely from your previous Unbound installation."
+  echo "The init script will probably work just fine but the script has since been rewritten"
+  echo "as of Unbound version 1.16.2 and it's no longer supported by this SlackBuild."
+  echo ""
+  echo "Simply run the following commands to install the new Unbound init script:"
+  echo "# cd /etc/rc.d && mv rc.unbound.new rc.unbound"
+  echo ""
+  echo "...or if you use slackpkg:"
+  echo "# slackpkg new-config"
+  echo ""
+fi
