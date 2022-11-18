8e22868acfb29c9079d8ccf789bdf7043a7f11c2
Bob Funk <bobfunk11@gmail.com>
2022-11-18 06:12:12 +0000

    desktop/gdm: Add wrapper script.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/gdm/doinst.sh
+++ desktop/gdm/doinst.sh
@@ -1,5 +1,18 @@
-# Set the wayland-session script executable:
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config etc/gdm/custom.conf.new
+
+# Set the wayland-session script and gdm wrapper executable:
 ( cd etc/gdm ; chmod 0755 wayland-session )
+( cd usr/sbin ; chmod 0755 gdm )
 
 if [ -e usr/share/glib-2.0/schemas ]; then
   if [ -x /usr/bin/glib-compile-schemas ]; then
