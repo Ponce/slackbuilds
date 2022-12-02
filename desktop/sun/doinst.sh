befa6e1d8aa70598e4e0bf8367c2a42601315921
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2022-12-02 20:53:40 +0200

    desktop/sun: Updated for version 1.3.6.
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/sun/doinst.sh
+++ desktop/sun/doinst.sh
@@ -8,7 +8,7 @@ config() {
   fi
 }
 
-config etc/sun/sun.conf.new
+config etc/sun/sun.toml.new
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
