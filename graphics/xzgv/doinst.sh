4b891516e3d90f0806ef009e48b423624cb4ee88
B. Watson <urchlay@slackware.uk>
2023-01-04 00:31:58 -0500

    graphics/xzgv: New maintainer.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- graphics/xzgv/doinst.sh
+++ graphics/xzgv/doinst.sh
@@ -1,3 +1,13 @@
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/install-info ]; then
+  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/xzgv.info.gz 2> /dev/null
+fi
