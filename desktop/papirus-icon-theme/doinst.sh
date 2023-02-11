e0d176f5033d2446965ecf574ad3cd6ce6156453
Jay Lanagan <j@lngn.net>
2023-02-11 10:05:45 +0000

    desktop/papirus-icon-theme: Add pre-made doinst.sh.gz.
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/papirus-icon-theme/doinst.sh
+++
@@ -1,8 +0,0 @@
-for theme in @THEMES@; do
-  if [ -e usr/share/icons/$theme/icon-theme.cache ]; then
-    if [ -x /usr/bin/gtk-update-icon-cache ]; then
-      /usr/bin/gtk-update-icon-cache -f usr/share/icons/$theme >/dev/null 2>&1
-    fi
-  fi
-done
-
