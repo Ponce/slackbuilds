7d0876d71fb1acd7999fcd9c61f95e28b87acd81
B. Watson <urchlay@slackware.uk>
2023-03-08 01:32:26 -0500

    games/xroar: Clean up douninst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/xroar/douninst.sh
+++ games/xroar/douninst.sh
@@ -1,17 +1,16 @@
-# 20211015 bkw: I was going to have a douninst.sh that runs
-# /usr/bin/install-info --delete /usr/info/xroar.info.gz /usr/info/dir
-# but this won't work because removepkg has already deleted
-# /usr/info/xroar.info.gz before it runs douninst.sh. And you can't
-# use install-info to remove stuff *by name* from /usr/info/dir;
-# the info file must actually exist.
-# So the script has to manually remove the entry from the info dir,
-# and the Emulators section if it's become empty.
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
 
-if [ -e usr/info/dir ]; then
-  # Remove the entry first:
-  sed -i '/^\* XRoar:/d' usr/info/dir
-  # If the Emulators section is empty now, remove it too:
-  if grep -A1 '^Emulators$' usr/info/dir | tail -1 | grep -q '^$'; then
-    sed -i '/^Emulators$/,+1d' usr/info/dir
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
+fi
+
+if [ -x /usr/bin/install-info -a -d usr/info ]; then
+  ( cd usr/info
+    rm -f dir
+    for i in *.info*; do /usr/bin/install-info $i dir 2>/dev/null; done
+  )
 fi
