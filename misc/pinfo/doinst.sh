562c2d88385114b783822a9ac8a721d74c7546e4
B. Watson <urchlay@slackware.uk>
2023-03-08 01:38:04 -0500

    misc/pinfo: Add douninst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- misc/pinfo/doinst.sh
+++ misc/pinfo/doinst.sh
@@ -1,18 +1,15 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 config etc/pinforc.new
 
 if [ -x /usr/bin/install-info ]; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/pinfo.info.gz 2> /dev/null
+  /usr/bin/install-info usr/info/pinfo.info.gz usr/info/dir
 fi
