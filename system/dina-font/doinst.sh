c18329b57362ecd52b5fc21491c1f61b951a9381
B. Watson <urchlay@slackware.uk>
2023-03-07 23:50:53 -0500

    system/dina-font: Add douninst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/dina-font/doinst.sh
+++ system/dina-font/doinst.sh
@@ -1,6 +1,6 @@
 if [ -x /usr/bin/mkfontdir ]; then
   ( cd usr/share/fonts/misc
-    if ! grep -q ^Dina_ fonts.alias 2>/dev/null; then
+    if ! grep -q '^Dina_\([689]\|10\) ' fonts.alias 2>/dev/null; then
       echo 'Dina_6 -windows-dina-medium-r-normal--8-60-96-96-c-60-microsoft-cp1252' >> fonts.alias
       echo 'Dina_8 -windows-dina-medium-r-normal--10-80-96-96-c-70-microsoft-cp1252' >> fonts.alias
       echo 'Dina_9 -windows-dina-medium-r-normal--12-90-96-96-c-70-microsoft-cp1252' >> fonts.alias
@@ -10,6 +10,9 @@ if [ -x /usr/bin/mkfontdir ]; then
     mkfontdir .
   )
 fi
+
+[ "$DISPLAY" != "" ] && xset fp rehash 2>/dev/null
+
 if [ -x usr/bin/fc-cache ]; then
   usr/bin/fc-cache -f
 fi
