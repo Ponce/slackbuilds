fca294ec70b70a8d7ef9b994d33ee684066809af
Giancarlo Dessì <slack@giand.it>
2023-04-08 00:48:23 +0100

    academic/gaiasky: Added (3D universe simulator for desktop and VR)
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ academic/gaiasky/doinst.sh
@@ -0,0 +1,7 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /sbin/setcap ]; then
+    /sbin/setcap cap_ipc_lock,cap_sys_nice=ep /opt/gaiasky/gaiasky
+fi
