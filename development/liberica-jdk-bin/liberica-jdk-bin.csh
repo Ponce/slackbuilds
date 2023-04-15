333fd6acc89f21381e1ea16d1615b329acf5ccb1
Alexander Verbovetsky <alik@ejik.org>
2023-04-15 02:13:48 +0100

    development/liberica-jdk-bin: Added (JDK distribution)
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/liberica-jdk-bin/liberica-jdk-bin.csh
@@ -0,0 +1,4 @@
+setenv JAVA_HOME /usr/lib
+setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
+setenv PATH ${PATH}:${JAVA_HOME}/bin
+setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:${JAVA_HOME}/lib/server
---
+++ development/liberica-jdk-bin/liberica-jdk-bin.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib
+export MANPATH="${MANPATH}:${JAVA_HOME}/man"
+export PATH="${PATH}:${JAVA_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${JAVA_HOME}/lib/server"
