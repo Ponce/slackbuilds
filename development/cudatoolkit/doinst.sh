a278e0b9c404af96c2cd9236feac89ef1eadb667
D Woodfall <dave@slackbuilds.org>
2023-05-17 00:39:43 +0100

    development/cudatoolkit: Fix ARCH sect. amd symlinks.
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/cudatoolkit/doinst.sh
+++ development/cudatoolkit/doinst.sh
@@ -1,3 +1,10 @@
-# install GCC 6 and link the compiler in cuda bin folder
-ln -sf usr/bin/gcc-6 usr/share/cuda/bin/gcc 
-ln -sf usr/bin/g++-6 usr/share/cuda/bin/g++
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
