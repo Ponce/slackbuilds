5a6e327dcf49237c7d28c51e71615ac96cd3e5f0
Alexander Verbovetsky <alik@ejik.org>
2023-05-26 20:40:19 +0100

    office/mtp2-fonts: Updated for version 2.1a.
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- office/mtp2-fonts/doinst.sh
+++
@@ -1,3 +0,0 @@
-chroot . /usr/bin/mktexlsr >/dev/null 2>&1
-chroot . /usr/bin/updmap-sys --force --enable Map=mtpro2.map >/dev/null 2>&1
-chroot . /usr/bin/mktexlsr >/dev/null 2>&1
--- office/mtp2-fonts/douninst.sh
+++
@@ -1,3 +0,0 @@
-chroot . /usr/bin/mktexlsr >/dev/null 2>&1
-chroot . /usr/bin/updmap-sys --force --disable mtpro2.map >/dev/null 2>&1
-chroot . /usr/bin/mktexlsr >/dev/null 2>&1
