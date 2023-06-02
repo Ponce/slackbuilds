354c377417f8e0839ea13f8481a8596a25837124
Christopher Luck <kermitdafrog8@gmail.com>
2023-06-02 21:19:04 +0100

    libraries/kddockwidgets: Added (KDAB's Dock Widget Framework for Qt)
    Signed-off-by: bedlam <dave@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ libraries/kddockwidgets/doinst.sh
@@ -0,0 +1,4 @@
+( cd usr/lib64 ; rm -rf libkddockwidgets.so )
+( cd usr/lib64 ; ln -sf libkddockwidgets.so.1.7 libkddockwidgets.so )
+( cd usr/lib64 ; rm -rf libkddockwidgets.so.1.7 )
+( cd usr/lib64 ; ln -sf libkddockwidgets.so.1.7.0 libkddockwidgets.so.1.7 )
