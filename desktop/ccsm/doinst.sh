0357aff5fda63ed98eebb52cf1af77d79b5f0613
B. Watson <urchlay@slackware.uk>
2023-06-17 16:43:42 -0400

    desktop/ccsm: Fix doinst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/ccsm/doinst.sh
+++ desktop/ccsm/doinst.sh
@@ -1,5 +1,7 @@
-if [ -x /usr/bin/gtk-update-icon-cache ]; then
-  /usr/bin/gtk-update-icon-cache -f -t usr/share/icons/hicolor >/dev/null 2>&1
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
 fi
 
 if [ -x /usr/bin/update-desktop-database ]; then
