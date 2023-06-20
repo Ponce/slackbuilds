dcbef36c9809dfb91f5b8378d1e5be46cb7dd9e7
B. Watson <urchlay@slackware.uk>
2023-06-20 03:36:59 -0400

    network/open-iscsi: Fix doinst, install headers -x.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/open-iscsi/doinst.sh
+++ network/open-iscsi/doinst.sh
@@ -1,16 +1,19 @@
+# 20230620 bkw: reverted this to the config() function from SBo
+# template. Please leave this as-is. It works, and it won't confuse
+# us (we have over 8000 scripts to look after...)
 
 config() {
-  NEW="${1}.new"
-  OLD="$1"
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(md5sum <$OLD)" = "$(md5sum <$NEW)" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/iscsi/iscsid.conf
-config etc/iscsi/initiatorname.iscsi
+config etc/iscsi/iscsid.conf.new
+config etc/iscsi/initiatorname.iscsi.new
