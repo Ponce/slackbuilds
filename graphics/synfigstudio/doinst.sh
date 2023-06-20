1759f88b9917b7fc4684caf3f52b84d2c043eebd
B. Watson <urchlay@slackware.uk>
2023-06-20 17:11:26 -0400

    graphics/synfigstudio: Fix doinst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- graphics/synfigstudio/doinst.sh
+++ graphics/synfigstudio/doinst.sh
@@ -1,3 +1,13 @@
-/usr/bin/update-mime-database -n /usr/share/mime 1> /dev/null 2> /dev/null
-/usr/bin/gtk-update-icon-cache /usr/share/icons  1> /dev/null 2> /dev/null
-/usr/bin/update-desktop-database  1> /dev/null 2> /dev/null
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
