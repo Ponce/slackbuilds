9b795af18c315d5b716f2a6b2bfa2f17ceda83bb
B. Watson <urchlay@slackware.uk>
2023-06-20 17:09:25 -0400

    games/steam: Fix doinst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/steam/doinst.sh
+++ games/steam/doinst.sh
@@ -4,9 +4,9 @@ if [ -x usr/bin/update-desktop-database ]; then
 fi
 
 # Update hicolor theme cache:
-if [ -d usr/share/icons/hicolor ]; then
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   if [ -x /usr/bin/gtk-update-icon-cache ]; then
-    /usr/bin/gtk-update-icon-cache -f -t usr/share/icons/hicolor 1> /dev/null 2> /dev/null
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
 
