d350d81a8c8002a775f77f83120309293faca267
B. Watson <urchlay@slackware.uk>
2023-06-20 03:40:48 -0400

    network/open-isns: Use template config().
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/open-isns/doinst.sh
+++ network/open-isns/doinst.sh
@@ -1,17 +1,18 @@
+# 20230620 bkw: Please stick with the config() from our template.
 
 config() {
-  NEW="${1}.new"
-  OLD="$1"
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(md5sum <$OLD)" = "$(md5sum <$NEW)" ]; then
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-config etc/isns/isnsd.conf
-config etc/isns/isnsdd.conf
-config etc/isns/isnsadm.conf
+config etc/isns/isnsd.conf.new
+config etc/isns/isnsdd.conf.new
+config etc/isns/isnsadm.conf.new
