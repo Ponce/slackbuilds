3e5b9d124cdcd90fca24533cbcc6621ee657dce8
Matteo Bernardini <ponce@slackbuilds.org>
2023-06-26 20:05:23 +0200

    system/adobe-source-code-pro-font: Updated for version 2.042.
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/adobe-source-code-pro-font/doinst.sh
+++ system/adobe-source-code-pro-font/doinst.sh
@@ -1,7 +1,7 @@
 # Update the X font indexes:
 if [ -x /usr/bin/mkfontdir ]; then
-  /usr/bin/mkfontscale /usr/share/fonts/OTF
-  /usr/bin/mkfontdir /usr/share/fonts/OTF
+  /usr/bin/mkfontscale usr/share/fonts/OTF
+  /usr/bin/mkfontdir usr/share/fonts/OTF
 fi
 if [ -x /usr/bin/fc-cache ]; then
   /usr/bin/fc-cache -f
