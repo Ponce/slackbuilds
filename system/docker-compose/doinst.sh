ee2e8fa0898b43339a7dffa757b555fad11d45a2
Ebben Aries <slackbuilds@dscp.org>
2023-07-31 08:24:19 +0700

    system/docker-compose: Added (Multi-container tool).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/docker-compose/doinst.sh
@@ -0,0 +1,8 @@
+# Install docker-compose to docker-cli global plugin directory
+PLUGIN_DIR="/usr/libexec/docker/cli-plugins"
+
+if [ ! -d "$PLUGIN_DIR" ]; then
+    mkdir -p $PLUGIN_DIR
+fi
+
+ln -s /usr/bin/docker-compose $PLUGIN_DIR/docker-compose
