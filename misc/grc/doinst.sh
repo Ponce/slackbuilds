293813b0209cdcdff47bdff81dc5f11a4a6b4e39
B. Watson <urchlay@slackware.uk>
2023-08-07 02:30:18 -0400

    misc/grc: Updated for version 1.13, new maintainer, support csh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- misc/grc/doinst.sh
+++ misc/grc/doinst.sh
@@ -1,17 +1,25 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/profile.d/grc.csh.new
+preserve_perms etc/profile.d/grc.sh.new
 config etc/grc.conf.new
-config etc/profile.d/grc.bashrc.new
-config etc/profile.d/grc.zsh.new
-config etc/profile.d/grc.fish.new
+config etc/default/grc.new
