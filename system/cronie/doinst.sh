dbd163adc6ec7d3065f2479e79a6e5c9857781a5
B. Watson <urchlay@slackware.uk>
2023-08-08 03:56:51 -0400

    system/cronie: Updated for version 1.6.1, new maintainer.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/cronie/doinst.sh
+++ system/cronie/doinst.sh
@@ -8,5 +8,18 @@ config() {
   fi
 }
 
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
 config var/spool/cron/root.new
 config etc/cron.deny.new
+config etc/default/crond.new
+preserve_perms etc/rc.d/rc.crond.new
