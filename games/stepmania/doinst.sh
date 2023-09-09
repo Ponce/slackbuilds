cf4e8bb2f6904c3b91840ab3ea02ca16e6d76dd8
Steven Voges <svoges.sbo@gmail.com>
2023-09-09 10:55:40 +0900

    games/stepmania: Added (dance and rhythm game).
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ games/stepmania/doinst.sh
@@ -0,0 +1,12 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+( cd usr/bin ; rm -rf stepmania )
+( cd usr/bin ; ln -sf /usr/share/stepmania-5.1/stepmania stepmania )
