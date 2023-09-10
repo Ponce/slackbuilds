9d98a6a1cef0090d41bd4d16b75e56846328ddd0
Ebben Aries <earies@dscp.org>
2023-09-09 17:25:18 -0700

    development/postman: Add desktop entry.
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/postman/doinst.sh
+++ development/postman/doinst.sh
@@ -1,2 +1,12 @@
 ( cd usr/bin ; rm -rf postman )
 ( cd usr/bin ; ln -sf /opt/postman/app/Postman postman )
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
