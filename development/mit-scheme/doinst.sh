8b8e17fb0a7ac6a34849a18e0d186b76d149362d
Lockywolf <for_sbo.mit-scheme_2023-11-27@lockywolf.net>
2023-12-01 08:26:29 +0700

    development/mit-scheme: Updated for version 12.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/mit-scheme/doinst.sh
+++ development/mit-scheme/doinst.sh
@@ -5,3 +5,17 @@ info_install() {
   fi
 }
 
+if [ -x /usr/bin/install-info ]; then
+  /usr/bin/install-info --info-dir=usr/info \
+                        /usr/info/mit-scheme-ffi.info.gz \
+                        /usr/info/mit-scheme-ref.info-1.gz \
+                        /usr/info/mit-scheme-ref.info-3.gz \
+                        /usr/info/mit-scheme-ref.info-5.gz \
+                        /usr/info/mit-scheme-sos.info.gz \
+                        /usr/info/mit-scheme-imail.info.gz \
+                        /usr/info/mit-scheme-ref.info-2.gz  \
+                        /usr/info/mit-scheme-ref.info-4.gz \
+                        /usr/info/mit-scheme-ref.info.gz  \
+                        /usr/info/mit-scheme-user.info.gz \
+                        1> /dev/null 2>&1
+fi
