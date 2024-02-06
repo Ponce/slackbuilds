ef19e20224b065de8fe9edb54e0e2bbe877b3d4c
Juan M. Lasca <juanmlasca@gmail.com>
2024-02-07 06:34:35 +0700

    development/cosmocc: Added (C library).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/cosmocc/scripts/register_ape.sh
@@ -0,0 +1,5 @@
+
+# register APE loader
+echo ':APE:M::MZqFpD::/usr/bin/ape:' > /proc/sys/fs/binfmt_misc/register
+echo ':APE-jart:M::jartsr::/usr/bin/ape:' > /proc/sys/fs/binfmt_misc/register
+
---
+++ development/cosmocc/scripts/unregister_ape.sh
@@ -0,0 +1,5 @@
+
+# unregister APE loader
+echo -1 > /proc/sys/fs/binfmt_misc/cli
+echo -1 > /proc/sys/fs/binfmt_misc/status
+
