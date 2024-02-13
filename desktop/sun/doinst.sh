33e7dbcd13e5707b813eb41abe0e545b2dbcdf22
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2024-02-13 22:16:30 +0200

    desktop/sun: Updated for version 1.6.0.
    Signed-off-by: Dimitris Zlatanidis <d.zlatanidis@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/sun/doinst.sh
+++ desktop/sun/doinst.sh
@@ -15,5 +15,5 @@ if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
 
-cmp etc/xdg/autostart/sun_daemon.desktop etc/xdg/autostart/sun_daemon.desktop.sample 2> /dev/null && \
-	  rm etc/xdg/autostart/sun_daemon.desktop.sample
+cmp etc/xdg/autostart/sun-daemon.desktop etc/xdg/autostart/sun-daemon.desktop.sample 2> /dev/null && \
+	  rm etc/xdg/autostart/sun-daemon.desktop.sample
