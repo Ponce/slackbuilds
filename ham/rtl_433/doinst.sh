2ba7a4254380c4a74f5475e2f37f1047d7c299c9
pyllyukko <pyllyukko@maimed.org>
2024-03-13 06:43:49 +0700

    ham/rtl_433: Added (decode radio transmissions).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ ham/rtl_433/doinst.sh
@@ -0,0 +1,11 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+shopt -s nullglob
+for i in etc/rtl_433/*.conf.new; do config "${i}"; done
