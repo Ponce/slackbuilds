f9af490972d0ccc9912ee7283309a3d7c3488421
zux <zux@pie-dabas.net>
2024-03-15 20:30:29 +0700

    network/zabbix_agent2: Added (new generation of Zabbix agent).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>

---
+++ network/zabbix_agent2/doinst.sh
@@ -0,0 +1,28 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.zabbix_agent2.new
+config etc/zabbix/zabbix_agent2.conf.new
+config var/log/zabbix/zabbix_agent2.log.new
+rm -f var/log/zabbix/zabbix_agent2.log.new
