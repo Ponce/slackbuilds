38fb196639b8f33c2a0b6a21812a2531b66d5acc
Alexander Verbovetsky <alik@ejik.org>
2024-04-12 22:53:22 +0900

    network/ejabberd-bin: Added (xmpp server).
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ network/ejabberd-bin/doinst.sh
@@ -0,0 +1,25 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.ejabberd.new
+config etc/ejabberd/ejabberd.yml.new
+config etc/ejabberd/ejabberdctl.cfg.new
+config etc/ejabberd/inetrc.new
