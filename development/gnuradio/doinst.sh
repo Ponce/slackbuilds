02c74ab03fcb91daa9a681415f97b070f949f204
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2024-04-14 10:31:33 +0700

    development/gnuradio: Update doinst.sh.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/gnuradio/doinst.sh
+++ development/gnuradio/doinst.sh
@@ -1,13 +1,9 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
 
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
