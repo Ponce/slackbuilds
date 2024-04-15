7c78ee423406e87878c5c01c6f60f613a8ae5b8b
Giancarlo Dessi <slack@giand.it>
2024-04-16 06:55:50 +0700

    academic/lammps: Updated for version 2023.08.02_update3.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ academic/lammps/doinst.sh
@@ -0,0 +1,12 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+config etc/profile.d/lammps.sh.new
+config etc/profile.d/lammps.csh.new
