5478b47bbf8c9a11a6367e54d5ab6b60e90d424d
Alexander Verbovetsky <alik@ejik.org>
2024-05-03 09:00:44 +0700

    development/liberica-jdk-bin: Update script.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/liberica-jdk-bin/liberica-jdk-bin.csh
+++
@@ -1,3 +0,0 @@
-setenv JAVA_HOME /usr/lib
-setenv PATH ${PATH}:${JAVA_HOME}/bin
-setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
--- development/liberica-jdk-bin/liberica-jdk-bin.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME=/usr/lib
-export PATH="${PATH}:${JAVA_HOME}/bin"
-export MANPATH="${MANPATH}:${JAVA_HOME}/man"
