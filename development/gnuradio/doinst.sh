1d0060b0c3bf56750d3bbcfc917fb1855b8af193
Mario Preksavec <mario@slackware.hr>
2024-07-02 02:37:18 +0200

    development/gnuradio: Updated for version 3.10.10.0, new maintainer.
    Signed-off-by: Mario Preksavec <mario@slackware.hr>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/gnuradio/doinst.sh
+++ development/gnuradio/doinst.sh
@@ -1,3 +1,13 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
