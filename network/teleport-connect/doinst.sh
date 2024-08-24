fc0114f4f0e3398275d16908e9f7b5ed34a90ea3
Rodrigo Lira <eurodrigolira@gmail.com>
2024-08-24 10:44:22 +0700

    network/teleport-connect: Added (Secure Access to Infrastructure).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ network/teleport-connect/doinst.sh
@@ -0,0 +1,12 @@
+( cd usr/bin ; rm -rf teleport-connect )
+( cd usr/bin ; ln -sf /opt/Teleport\ Connect/teleport-connect teleport-connect )
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
