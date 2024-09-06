4e97f057bb402ba41423b56abd3a85599cf58126
Andrew Clemons <andrew.clemons@gmail.com>
2024-09-06 21:48:40 +0900

    development/zulu-openjdk7: Allow profile script to override jdk.
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/zulu-openjdk7/zulu-openjdk7.csh
+++ development/zulu-openjdk7/zulu-openjdk7.csh
@@ -1,4 +1,4 @@
 #!/bin/csh
 setenv JAVA_HOME /usr/lib/zulu-openjdk7
-setenv MANPATH ${MANPATH}:${JAVA_HOME}/man
-setenv PATH ${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin
+setenv MANPATH ${JAVA_HOME}/man:${MANPATH}
+setenv PATH ${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin:${PATH}
--- development/zulu-openjdk7/zulu-openjdk7.sh
+++ development/zulu-openjdk7/zulu-openjdk7.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JAVA_HOME=/usr/lib/zulu-openjdk7
-export MANPATH="${MANPATH}:${JAVA_HOME}/man"
-export PATH="${PATH}:${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin"
+export MANPATH="${JAVA_HOME}/man:${MANPATH}"
+export PATH="${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin:${PATH}"
