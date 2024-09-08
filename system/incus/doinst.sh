bfe59f8fbf7011b38196073cc53f0675c68c2338
Christoph Willing <chris.willing@linux.com>
2024-09-08 17:03:23 +0700

    system/incus: Added (system container).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/incus/doinst.sh
@@ -0,0 +1,32 @@
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+preserve_perms etc/rc.d/rc.incusd.new
+preserve_perms etc/default/incus.new
+
+if [ -f etc/profile.d/bash_completion.sh ]; then
+  echo "Setting up bash completions for incus"
+  . etc/profile.d/bash_completion.sh
+fi
+
