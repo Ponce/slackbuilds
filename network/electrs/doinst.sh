98a805ed4e9542925da4366426fa6b18dc8bcb43
thnkman <thnkman@proton.me>
2024-09-09 10:38:31 +0200

    network/electrs: Rewrote init and fixed some mistakes in the build script.
    Shebang probably better off as /bin/sh.
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/electrs/doinst.sh
+++ network/electrs/doinst.sh
@@ -6,8 +6,7 @@ PRGNAM=electrs
 ELECTRS_UID=386
 ELECTRS_GID=386
 
-chown $ELECTRS_UID:$ELECTRS_GID etc/$PRGNAM/*
-
 setfacl -R -m u:$ELECTRS_UID:rwx etc/$PRGNAM
-setfacl -R -m u:$ELECTRS_UID:rwx var/run/$PRGNAM
+setfacl -R -m u:$ELECTRS_UID:rwx var/lib/$PRGNAM
 setfacl -R -m u:$ELECTRS_UID:rwx var/log/$PRGNAM
+setfacl -R -m u:$ELECTRS_UID:rwx var/run/$PRGNAM
