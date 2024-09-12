22f78d4437b61ba4793b0a4e5f23188f7d13cd3b
Rodrigo Lira <eurodrigolira@gmail.com>
2024-09-13 06:24:47 +0900

    network/grafana: Added (monitoring tool).
    Signed-off-by: Andrew Clemons <andrew.clemons@gmail.com>
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ network/grafana/doinst.sh
@@ -0,0 +1,25 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/grafana/grafana.ini.new
+config etc/default/grafana-server.new
+
+preserve_perms etc/rc.d/rc.grafana.new
