e7e555fe6dbb6905e5f1ca5402b9fd5e4ba36b39
B. Watson <urchlay@slackware.uk>
2024-09-25 15:22:03 -0400

    games/ltris: Updated for version 2.0.1.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/ltris/doinst.sh
+++ games/ltris/doinst.sh
@@ -7,3 +7,7 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
+
+# 20240925 bkw: don't blow away the saved high scores on package update.
+touch var/lib/lgames/ltris2.hscr
+chmod 666 var/lib/lgames/ltris2.hscr
