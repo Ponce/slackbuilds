77ad1035c4253f68f5dea3582474bd5b8af59d47
B. Watson <urchlay@slackware.uk>
2024-10-08 22:32:53 -0400

    games/tint: New maintainer, fixes.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/tint/doinst.sh
+++ games/tint/doinst.sh
@@ -1,7 +1,13 @@
-touch /var/games/tint.scores
-chown root:games /var/games/tint.scores
-chmod 0664 /var/games/tint.scores
+touch var/games/tint.scores
+chown root:games var/games/tint.scores
+chmod 0664 var/games/tint.scores
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
 fi
