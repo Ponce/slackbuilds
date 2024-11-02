9502ef5310f9892fbb3013bef6dfda964b8076e0
Matteo Bernardini <ponce@slackbuilds.org>
2024-11-02 16:51:06 +0100

    audio/wireplumber: Removed (added to Slackware).
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>

--- audio/wireplumber/doinst.sh
+++
@@ -1,5 +0,0 @@
-# Toss redundant sample files:
-for file in wireplumber.desktop ; do
-  cmp etc/xdg/autostart/${file} etc/xdg/autostart/${file}.sample 2> /dev/null && rm etc/xdg/autostart/${file}.sample
-done
-
