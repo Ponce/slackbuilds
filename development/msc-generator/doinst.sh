2822f662cc38a2dac1311fff860de3902b6c0357
Luigi Masini <luigimasini.lm@gmail.com>
2024-11-14 21:12:40 +0700

    development/msc-generator: Added (Draws signalling charts)
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/msc-generator/doinst.sh
@@ -0,0 +1,16 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -x /usr/bin/install-info ]; then
+  ( cd usr/info
+#    rm -f dir
+    for i in *.info*; do /usr/bin/install-info $i dir 2>/dev/null; done
+  )
+fi
