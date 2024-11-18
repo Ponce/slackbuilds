4361d3d6a3dc97dee8af2d638892ddb216344699
isaackwy <isaacyu@protonmail.com>
2024-11-17 19:38:32 -0800

    system/file-roller: Update for 43.1
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/file-roller/doinst.sh
+++ system/file-roller/doinst.sh
@@ -8,10 +8,6 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
   fi
 fi
 
-if [ -x /usr/bin/update-mime-database ]; then
-  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
-fi
-
 if [ -x /usr/bin/glib-compile-schemas ]; then
-	/usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas/ 2>/dev/null
+  /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas/ 2>/dev/null
 fi
