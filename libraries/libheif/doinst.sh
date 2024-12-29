beba381965c507644b0d57c092d9ca36287e72b2
Edward W. Koenig <kingbeowulf@linuxgalaxy.org>
2024-12-29 09:40:52 -0800

    libraries/libheif: Update doinst.sh for gdk-pixbuf loader
    Signed-off-by: Edward W. Koenig <kingbeowulf@linuxgalaxy.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- libraries/libheif/doinst.sh
+++ libraries/libheif/doinst.sh
@@ -1,3 +1,8 @@
 if [ -x /usr/bin/update-mime-database ]; then
   /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
+
+if [ -x /usr/bin/update-gdk-pixbuf-loaders ]; then
+  /usr/bin/update-gdk-pixbuf-loaders >/dev/null 2>&1
+fi
+
