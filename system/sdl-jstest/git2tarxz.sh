a212a1fa8b6700a6d87a780e67d88d49d8036379
B. Watson <urchlay@slackware.uk>
2025-01-13 15:52:12 -0500

    system/sdl-jstest: Updated for version 0.2.2+20240331_60f6e62.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/sdl-jstest/git2tarxz.sh
+++ system/sdl-jstest/git2tarxz.sh
@@ -2,8 +2,7 @@
 
 # Create source tarball from sdl-jstest git repo, with generated version
 # number. Since the build expects to have the git log available (for
-# embedding the git hash in --version output), don't rm -rf .git,
-# but do use --depth 1 when cloning.
+# embedding the git hash in --version output), don't rm -rf .git.
 
 # Note that this script doesn't need to be run as root. It does need to
 # be able to write to the current directory it's run from.
@@ -12,13 +11,13 @@
 # tarball of. With no arg, HEAD is used.
 
 PRGNAM=sdl-jstest
-CLONE_URL=https://gitlab.com/$PRGNAM/$PRGNAM.git
+CLONE_URL=https://github.com/Grumbel/sdl-jstest
 
 set -e
 
 GITDIR=$( mktemp -dt $PRGNAM.git.XXXXXX )
 rm -rf $GITDIR
-git clone --depth 1 $CLONE_URL $GITDIR
+git clone $CLONE_URL $GITDIR
 
 CWD="$( pwd )"
 cd $GITDIR
@@ -33,7 +32,9 @@ GIT_SHA=$( git rev-parse --short HEAD )
 
 DATE=$( git log --date=format:%Y%m%d --format=%cd | head -1 )
 
-VERSION=${DATE}_${GIT_SHA}
+V=$( git tag --sort=v:refname | tail -1 | sed 's,^v,,' )
+
+VERSION=${V}+${DATE}_${GIT_SHA}
 
 mv .git .keep.git
 find . -name .git\* -print0 | xargs -0 rm -rf
