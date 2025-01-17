ff0c2ca717992e667d2fe11a9d4b0c1bc76b3810
Zhu Qun-Ying <zhu.qunying@gmail.com>
2025-01-17 17:55:53 +0700

    libraries/aunit: Added (Ada Unit test framework).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ libraries/aunit/doinst.sh
@@ -0,0 +1,3 @@
+if [ -x /usr/bin/install-info ]; then
+	/usr/bin/install-info --info-dir=usr/info usr/info/aunit_cb.info.gz &> /dev/null
+fi
---
+++ libraries/aunit/douninst.sh
@@ -0,0 +1,3 @@
+if [ -x /usr/sbin/update-info-dir -a -d usr/info ]; then
+	/usr/sbin/update-info-dir
+fi
