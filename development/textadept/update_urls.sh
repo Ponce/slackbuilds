5b96dd5fbda789a5db6b64d4b99d1653e5675451
Arn0 <yth@ythogtha.org>
2025-02-11 22:41:08 +0100

    development/textadept: updated for version 12.6
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/textadept/update_urls.sh
+++ development/textadept/update_urls.sh
@@ -8,13 +8,8 @@ echo -ne "DOWNLOAD=\"$url"
 curl -s https://raw.githubusercontent.com/orbitalquark/textadept/textadept_${VERSION}/CMakeLists.txt > CMakeLists.txt
 md5=()
 files=()
-for pkg in $(grep "_url " CMakeLists.txt | cut -d\( -f2| cut -d_ -f1 | sort -u); do
-	ext=tgz
-	file=$(grep "(${pkg}_${ext} " CMakeLists.txt | head -1 | sed "s#.*${pkg}_${ext} \(.*\)[)]#\1#")
-	[ -z "$file" ] && ext=zip && file=$(grep "(${pkg}_${ext} " CMakeLists.txt | head -1 | sed "s#.*${pkg}_${ext} \(.*\)[)]#\1#")
-	url=$(grep "(${pkg}_url https" CMakeLists.txt | sed "s#.*${pkg}_url \(.*\)..${pkg}_${ext}..#\1${file}#")
-	[ -z "$url" ] && url=$(grep -Pzo "(?s)\(${pkg}_url\s*https\N*" CMakeLists.txt | tr -d '\n' | sed "s#.*${pkg}_url.*\(https.*\)..${pkg}_${ext}.*#\1${file}#")
-	#echo "$pkg.$ext[$file]: $url"
+for url in $(grep '^fetch(' CMakeLists.txt | cut -d\  -f2 | tr -d \) | sort -u); do
+	file=$(rev <<<$url | cut -d/ -f1 | rev)
 	md5=( "${md5[@]}" $(curl -Ls $url | md5sum | cut -d\  -f1))
 	files=( "${files[@]}" $file)
 	echo -ne " \\\\\\n          $url"
