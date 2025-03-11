81958106ec00376c1034eef6cfd2381096ed168e
Jefferson Carneiro <lrcjefferson@gmail.com>
2025-03-10 23:12:46 -0300

    New version and fixed some issues.

--- network/dokuwiki/doinst.sh
+++ network/dokuwiki/doinst.sh
@@ -1,7 +1,7 @@
-# Remove comment
-sed -i "s:#Include /etc/httpd/mod_php.conf:Include /etc/httpd/mod_php.conf:g" etc/httpd/httpd.conf
+# Remove comment and enable php
+sed -i "s:#Include /etc/httpd/mod_php.conf:Include /etc/httpd/mod_php.conf:g" /etc/httpd/httpd.conf
 
-# If not exist line, send...
-grep -q -e 'Dokuwiki' etc/httpd/httpd.conf || cat >> etc/httpd/httpd.conf <<'EOF'
-Include /etc/httpd/extra/httpd-dokuwiki.conf
-EOF
+# not exist? send...
+if ! grep -q -e 'dokuwiki' /etc/httpd/httpd.conf; then
+    echo "Include /etc/httpd/extra/httpd-dokuwiki.conf" >> /etc/httpd/httpd.conf
+fi
