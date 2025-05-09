3deb177d7d156d8ba3297c4a966c67abe0efd630
Vijay Marcel <vijaymarcel@outlook.com>
2025-05-10 01:45:28 +0700

    system/fonts-japanese-extra-II: Added (Japanese fonts).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/fonts-japanese-extra-II/doinst.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Update the X font indexes:
+
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/OTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+
+[ "$DISPLAY" != "" ] && xset fp rehash 2>/dev/null
+
+if [ -x /usr/bin/fc-cache ]; then
+  /usr/bin/fc-cache -f
+fi
