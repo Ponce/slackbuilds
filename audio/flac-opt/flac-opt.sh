2b0508e27bd08d2cb5ecd44059430af62f5dfbf6
B. Watson <urchlay@slackware.uk>
2025-05-12 17:46:40 -0400

    audio/flac-opt: Fix MANPATH breakage.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- audio/flac-opt/flac-opt.sh
+++ audio/flac-opt/flac-opt.sh
@@ -1,19 +1,6 @@
 # profile.d script for SBo flac-opt build, WTFPL, B. Watson.
 
-# don't know if MANPATH might be set already, so cover both cases.
-
-Mtmp=@PREFIX@/man
-
-if [ -z "$MANPATH" ]; then
-  MANPATH="$Mtmp"
-else
-  MANPATH="$Mtmp:$MANPATH"
-fi
-
-unset Mtmp
-
-# PATH will already be set (in /etc/profile) so don't worry about it.
-
+MANPATH="@PREFIX@/man:$MANPATH"
 PATH=@PREFIX@/bin:$PATH
 
 export PATH
