56533f8d7942aaab3e1352c1c57247614580c902
Dan-Simon Myrland <dansimon@radiotube.org>
2025-06-13 07:58:58 +0700

    system/roxterm: Added (GTK Terminal).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ system/roxterm/doinst.sh
@@ -0,0 +1,7 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+  /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+fi
