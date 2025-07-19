228c59aafa3c4d4f23a72377d634cf1e870d5b4b
B. Watson <urchlay@slackware.uk>
2025-07-19 03:06:41 -0400

    network/sslh: Updated for version 2.2.4+20250630_11da63c, new maint.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/sslh/doinst.sh
+++ network/sslh/doinst.sh
@@ -1,14 +1,11 @@
 config() {
   NEW="$1"
   OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
     rm $NEW
   fi
-  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 preserve_perms() {
@@ -24,3 +21,4 @@ preserve_perms() {
 
 preserve_perms etc/rc.d/rc.sslh.new
 config etc/sslh/sslh.cfg.new
+config etc/default/sslh.new
