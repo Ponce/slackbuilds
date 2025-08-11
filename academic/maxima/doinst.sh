95f5d0ae8750dac3ed006e9999450e725eeeaeec
Judah Milgram <milgram cgpp com>
2025-08-11 10:34:30 +0700

    academic/maxima: Updated for version 5.48.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- academic/maxima/doinst.sh
+++ academic/maxima/doinst.sh
@@ -1,4 +1,22 @@
 
+# As featured in many Slackbuilds:
+
+if [ -x /usr/bin/install-info -a -d usr/info ]; then
+  ( cd usr/info
+    rm -f dir
+    for i in *.info*; do /usr/bin/install-info $i dir 2>/dev/null; done
+  )
+fi
+
+echo << END
+Note: don't compress the info files because maxima uses them as
+internal help files and currently isn't able to decompress them on
+the fly. "info" can handle uncompressed files.
+END
+
+echo << END
+Note: lisp files were added to /usr/info because maxima needs them
+END
 
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
