a4445db17c4fb587c6bec1bc43a48b3ee211f65e
Samuel Young <samyoung12788@gmail.com>
2025-08-17 16:00:32 +0700

    network/copyparty: Updated for version 1.19.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/copyparty/doinst.sh
+++ network/copyparty/doinst.sh
@@ -13,10 +13,3 @@ config() {
 
 config etc/rc.d/rc.copyparty.new
 config etc/copyparty.d/copyparty.conf.new
-
-if [ -x /usr/bin/install-info -a -d usr/info ]; then
-  ( cd usr/info
-    rm -f dir
-    for i in *.info*; do /usr/bin/install-info $i dir 2>/dev/null; done
-  )
-fi
