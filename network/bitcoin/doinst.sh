39f42e17851e7840acb6eeadb83279e6600e0d28
nomnombtc <nomnombtc@arcor.de>
2025-09-04 22:11:50 +0200

    network/bitcoin: Updated for version 29.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/bitcoin/doinst.sh
+++ network/bitcoin/doinst.sh
@@ -1,3 +1,8 @@
 if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
 fi
