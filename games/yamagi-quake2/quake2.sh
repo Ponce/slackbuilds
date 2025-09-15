bf6bb1de900a4e96eb2096558ba8ea4b9be14f48
Ruoh-Shoei LIN <lin.ruohshoei+sbo@gmail.com>
2025-09-15 17:12:11 +0000

    games/yamagi-quake2: Updated for version 8.60.
    - upstream release
    - build system: cmake -> make
    - default game data dir: /usr/share/quake2 -> /usr/share/games/quake2
    
    Signed-off-by: Ruoh-Shoei LIN <lin.ruohshoei+sbo@gmail.com>
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/yamagi-quake2/quake2.sh
+++ games/yamagi-quake2/quake2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/share/quake2/quake2 +set basedir /usr/share/quake2 $*
+exec /usr/share/games/quake2/quake2 +set basedir /usr/share/games/quake2 $*
--- games/yamagi-quake2/xatrix.sh
+++ games/yamagi-quake2/xatrix.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/share/quake2/quake2 +set basedir /usr/share/quake2 +set game xatrix $*
+exec /usr/share/games/quake2/quake2 +set basedir /usr/share/games/quake2 +set game xatrix $*
