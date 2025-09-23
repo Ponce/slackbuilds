632252c118e5ca81a19402c0ca823d0ceb056f3a
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2025-09-23 22:21:45 +0700

    games/ut99: Fix symlink.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- games/ut99/doinst.sh
+++ games/ut99/doinst.sh
@@ -7,5 +7,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
-ln -sf /opt/ut99/UnrealTournament/System64/ut-bin-amd64 /opt/ut99/unreal
