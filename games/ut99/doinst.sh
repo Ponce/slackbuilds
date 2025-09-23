bc43476bdacd1083291bd9a665a19deff47637d4
Eduardo Castillo <hellocodelinux@gmail.com>
2025-09-23 21:53:42 +0700

    games/ut99: Added (classic fps game).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ games/ut99/doinst.sh
@@ -0,0 +1,11 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+ln -sf /opt/ut99/UnrealTournament/System64/ut-bin-amd64 /opt/ut99/unreal
