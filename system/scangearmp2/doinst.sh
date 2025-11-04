4ccd4b4890947f5d65f6e87902b3c96d9f829c92
Kenneth Chan <kenneth.t.chan@gmail.com>
2025-11-04 08:41:32 +0700

    system/scangearmp2: Updated for version 4.90.1.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/scangearmp2/doinst.sh
+++ system/scangearmp2/doinst.sh
@@ -1,5 +1,9 @@
 # reload udev rules
 if [ -x /sbin/udevadm ]; then
-	/sbin/udevadm control --reload-rules 2> /dev/null
-	/sbin/udevadm trigger --action=add --subsystem-match=usb 2> /dev/null
+  /sbin/udevadm control --reload-rules 2> /dev/null
+  /sbin/udevadm trigger --action=add --subsystem-match=usb 2> /dev/null
+fi
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
