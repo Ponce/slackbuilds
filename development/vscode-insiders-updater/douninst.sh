82472807a36cf9bef288099f5da100c3382a47b5
banovic <branislav.anovic+sbo@gmail.com>
2025-11-21 23:00:27 +0700

    development/vscode-insiders-updater: Added (VS Code Insider Updater).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ development/vscode-insiders-updater/douninst.sh
@@ -0,0 +1,6 @@
+# Remove vscode-inisiders package that was installed via vscode-insiders-updater:
+INSTALLED_VERSION=$(ls -v /var/log/packages/vscode-insiders-* 2>/dev/null | cut -d- -f3 | grep '^.[0-9]\+$' | sort -r | head -1)
+if [ -n "$INSTALLED_VERSION" ]; then
+    echo "Found previously installed Visual Studio Code Insiders package version $INSTALLED_VERSION..."
+    /sbin/removepkg vscode-insiders-$INSTALLED_VERSION-*
+fi
