5b7c25251e568e1c95d64f4fcd9d3addd8c4d020
Athanasios Silis <athanasios.silis@gmail.com>
2026-01-12 17:35:22 +0200

    added new slackbuilds for sssd 2.11.1 and its dependency on ding-libs 0.6.2

---
+++ network/sssd/doinst.sh
@@ -0,0 +1,17 @@
+ldconfig
+# intentionally redirect output to avoid noise in the terminal
+libtool --finish /usr/local/lib > /dev/null
+libtool --finish /usr/local/lib/sssd > /dev/null
+libtool --finish /usr/local/lib64/python3.12/site-packages > /dev/null
+libtool --finish /usr/local/lib/sssd/modules > /dev/null
+libtool --finish /usr/local/lib/cifs-utils > /dev/null
+libtool --finish /usr/local/lib/sssd/modules > /dev/null
+libtool --finish /usr/local/lib/sssd/modules > /dev/null
+libtool --finish /usr/local/lib/krb5/plugins/libkrb5 > /dev/null
+libtool --finish /usr/lib64/ldb > /dev/null
+libtool --finish /usr/local/lib/libnfsidmap > /dev/null
+libtool --finish /usr/local/lib > /dev/null
+libtool --finish /usr/local/lib/security > /dev/null
+libtool --finish /usr/local/lib/sssd > /dev/null
+libtool --finish /usr/local/lib > /dev/null
+libtool --finish /usr/local/lib/samba/idmap > /dev/null
