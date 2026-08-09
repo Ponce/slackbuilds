a4bcd2117eb6c118b3b70248d83b5f9c4172d3c6
Vijay Marcel <vijaymarcel@gmail.com>
2026-08-09 21:43:00 +0530

    desktop/alacarte: Updated for version 3.44.3 + new maintainer
    Signed-off-by: Christoph Willing <chris.willing@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- desktop/alacarte/doinst.sh
+++ desktop/alacarte/doinst.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
@@ -7,4 +13,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
