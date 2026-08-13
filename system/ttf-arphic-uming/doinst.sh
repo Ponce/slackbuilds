8a7499532e045a876174eea92f3f780761cbd3c2
Vijay Marcel <vijaymarcel@gmail.com>
2026-08-13 21:44:59 +0530

    system/ttf-arphic-uming: New maintainer
    Signed-off-by: Christoph Willing <chris.willing@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/ttf-arphic-uming/doinst.sh
+++ system/ttf-arphic-uming/doinst.sh
@@ -2,11 +2,28 @@
 # install, since there is a post-install script that
 # does the same thing, saving time.
 # Update X font indexes and the font cache:
+
+#!/bin/sh
+
+# Update the X font indexes:
+
 if [ -x /usr/bin/mkfontdir ]; then
-  /usr/bin/mkfontscale /usr/share/fonts/TTF
-  /usr/bin/mkfontdir /usr/share/fonts/TTF
+  ( cd /usr/share/fonts/TTF
+    mkfontscale .
+    mkfontdir .
+  )
 fi
 
+if [ -x /usr/bin/mkfontdir ]; then
+  ( cd /usr/share/fonts/OTF
+    mkfontscale .
+    mkfontdir .
+  )
+fi
+
+[ "$DISPLAY" != "" ] && xset fp rehash 2>/dev/null
+
 if [ -x /usr/bin/fc-cache ]; then
-  /usr/bin/fc-cache /usr/share/fonts/TTF
+  /usr/bin/fc-cache -f
+fi
 fi
