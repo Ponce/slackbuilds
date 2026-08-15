0e0434dca84805a08e930218ca9ebc50b64f32fd
A. Tomasini <>
2026-08-15 08:03:00 +0700

    academic/ngspice-wave: Added (analog waveform viewer).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

---
+++ academic/ngspice-wave/doinst.sh
@@ -0,0 +1,4 @@
+# No post-install actions are required.  Both binaries are self-contained:
+# the font atlas is compiled into wave-render and the X11 client speaks the
+# wire protocol, so there is nothing to register, cache or relink.
+:
