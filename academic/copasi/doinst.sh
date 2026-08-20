3235a81cc1f7f0888b16b3a069e66b5de00020b1
Pedro Mendes <pedro@gepasi.org>
2026-08-20 08:00:40 +0700

    academic/copasi: Updated for version 4.47.309.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- academic/copasi/doinst.sh
+++ academic/copasi/doinst.sh
@@ -1,3 +1,8 @@
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications
 fi
+
+if [ -x /usr/bin/xdg-mime ]; then
+  /usr/bin/xdg-mime install usr/share/copasi/COPASI-mime.xml
+fi
+
