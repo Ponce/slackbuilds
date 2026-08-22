9d467c8460400fa450dfe08f90f5e69bc3faf036
Emmanuel N. Millán <emmanueln@gmail.com>
2026-08-22 12:02:04 -0300

    system/openmpi: Updated for version 5.0.10
    Signed-off-by: Christoph Willing <chris.willing@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/openmpi/doinst.sh
+++ system/openmpi/doinst.sh
@@ -11,7 +11,13 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
 config etc/pmix-mca-params.conf.new
-config etc/openmpi-default-hostfile.new
 config etc/openmpi-mca-params.conf.new
 config etc/openmpi-totalview.tcl.new
+config etc/prte-default-hostfile.new
+config etc/prte-mca-params.conf.new 
+config etc/prte.conf.new 
