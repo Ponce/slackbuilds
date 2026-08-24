4383f15e8a4771bf119ee3c2efd63ba3e213947b
Vijay Marcel <vijaymarcel@gmail.com>
2026-08-24 21:59:58 +0530

    graphics/qcad: Updated for version 3.32.9.0 + new maintainer
    Signed-off-by: Christoph Willing <chris.willing@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- graphics/qcad/qcad.sh
+++ graphics/qcad/qcad.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-cd /usr/share/qcad
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}"/usr/share/qcad" exec /usr/share/qcad/qcad-bin "$@"
+#!/bin/bash
+
+# Move to the QCAD directory
+cd /opt/qcad
+
+# Set the library path safely and export it
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}/opt/qcad:/opt/qcad/plugins:/usr/share/qcad"
+
+# Launch the application and replace the shell process
+exec /opt/qcad/qcad-bin "$@"
