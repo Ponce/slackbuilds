bf917b3a99525d9151f2a74a81b903dcc28cfa8e
Vijay Marcel <vijaymarcel@gmail.com>
2026-09-04 22:26:13 +0530

    desktop/maitreya: Updated for version 8.2 + new maintainer
    Signed-off-by: Christoph Willing <chris.willing@slackbuilds.org>

--- desktop/maitreya/doinst.sh
+++ desktop/maitreya/doinst.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
 fi
 
 # Update the X font indexes:
@@ -9,7 +21,9 @@ if [ -x /usr/bin/mkfontdir ]; then
     mkfontdir .
   )
 fi
+
+[ "$DISPLAY" != "" ] && xset fp rehash 2>/dev/null
+
 if [ -x /usr/bin/fc-cache ]; then
   /usr/bin/fc-cache -f
 fi
-
